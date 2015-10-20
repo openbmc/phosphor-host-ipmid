dfc8d7775f51dc4927e6c458654545d50cb023bf
Adriana Kobylak <anoo@us.ibm.com>
2015-10-20 09:34:48 -0500

    Add App Set BMC Global Enables ipmi cmd
    Add support for the IPMI cmd Set BMC Global Enables

--- apphandler.h
+++ apphandler.h
@@ -1,16 +1,17 @@
 #ifndef __HOST_IPMI_APP_HANDLER_H__
 #define __HOST_IPMI_APP_HANDLER_H__
 
-// IPMI commands for net functions.
+// IPMI commands for App net functions.
 enum ipmi_netfn_app_cmds
 {
     // Get capability bits
-    IPMI_CMD_RESET_WD       = 0x22,
-    IPMI_CMD_SET_WD         = 0x24,
-    IPMI_CMD_GET_CAP_BIT    = 0x36,
-    IPMI_CMD_GET_DEVICE_ID  = 0x00,
-    IPMI_CMD_SET_ACPI       = 0x06,
-    IPMI_CMD_READ_EVENT     = 0x35,
+    IPMI_CMD_GET_DEVICE_ID          = 0x01,
+    IPMI_CMD_SET_ACPI               = 0x06,
+    IPMI_CMD_RESET_WD               = 0x22,
+    IPMI_CMD_SET_WD                 = 0x24,
+    IPMI_CMD_SET_BMC_GLOBAL_ENABLES = 0x2E,
+    IPMI_CMD_READ_EVENT             = 0x35,
+    IPMI_CMD_GET_CAP_BIT            = 0x36,
 
 };
 
