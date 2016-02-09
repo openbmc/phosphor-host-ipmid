e08fbc6965178c54677112ce5e69a90092b27384
Adriana Kobylak <anoo@us.ibm.com>
2016-02-09 16:17:23 -0600

    Address review comments for IPMI transport functions
    Call get bus interface instead of using extern.
    Define the IPMI request parameters.
    Use snprintf.
    Initialize dbus variables at the beginning of the function.

--- transporthandler.h
+++ transporthandler.h
@@ -15,4 +15,13 @@ enum ipmi_transport_return_codes
     IPMI_CC_PARM_NOT_SUPPORTED = 0x80,
 };
 
+// Parameters
+static const int LAN_PARM_INPROGRESS  = 0;
+static const int LAN_PARM_AUTHSUPPORT = 1;
+static const int LAN_PARM_AUTHENABLES = 2;
+static const int LAN_PARM_IP          = 3;
+static const int LAN_PARM_MAC         = 5;
+static const int LAN_PARM_SUBNET      = 6;
+static const int LAN_PARM_GATEWAY     = 12;
+
 #endif
