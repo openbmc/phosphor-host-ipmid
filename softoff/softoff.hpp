6c761bf98af2b460cde53ac555669044e64b3a75
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-01-24 18:48:40 +0530

    Add function to send SMS_ATTN to host
    This patch overrides one of the base API and sends a message that
    enables the current HOST IPMI BT provider to actually set the SMS_ATTN.
    
    Change-Id: I250ea6bed99e569e493251f6fc298aa7ed7c776f
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- softoff/softoff.hpp
+++ softoff/softoff.hpp
@@ -34,10 +34,44 @@ class SoftPowerOff : public sdbusplus::server::object::object<
             sdbusplus::server::object::object<
                 sdbusplus::xyz::openbmc_project::Ipmi::Internal
                                                ::server::SoftPowerOff>(
-                                               bus, objPath)
+                                               bus, objPath),
+            bus(bus)
         {
             // Nothing to do here
         }
+
+        /** @brief Sends SMS_ATN to host to initiate soft power off process.
+         *         After sending the SMS_ATN, starts a watchdog timer for 30
+         *         seconds and expects a initial response from the host.
+         *         After receiving the initial response, starts another watchdog
+         *         timer for 30 minutes to let host do a clean shutdown of
+         *         partitions. When the second response is received from the
+         *         host, it indicates that BMC can do a power off.
+         *         If BMC fails to get any response, then a hard power off would
+         *         be foced.
+         *
+         *  @param[in] value     - If true, do the action mentioned above
+         */
+        bool initiate(bool value) override;
+
+        /** @brief Called by IPMI provider as part of response received to
+         *         SoftPowerOff process.
+         *
+         *  @param[in] value - Type of response received within soft power
+         *                     off protocol. This could be either of below:
+         *                     'SoftOffReceived' or 'PartitionsShutdown'.
+         *                     A 'SoftOffReceived' indicates that host has
+         *                     received the SMS_ATN that is sent by the above
+         *                     'initiate' call.
+         *                     A 'PartitionsShutdown' indicates that host has
+         *                     shutdown all the partitions and BMC can do a
+         *                     power off operation.
+         */
+        HostResponse responseReceived(HostResponse value) override;
+
+    private:
+        /* @brief sdbusplus handle */
+        sdbusplus::bus::bus& bus;
 };
 } // namespace ipmi
 } // namespace phosphor
