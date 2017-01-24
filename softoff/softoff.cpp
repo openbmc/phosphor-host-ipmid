87ecdf62d825ba6274a5dcc2966cd4ba002778b4
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-01-24 18:48:40 +0530

    Add function to send SMS_ATTN to host
    This patch overrides one of the base API and sends a message that
    enables the current HOST IPMI BT provider to actually set the SMS_ATTN.
    
    Change-Id: I250ea6bed99e569e493251f6fc298aa7ed7c776f
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- softoff/softoff.cpp
+++ softoff/softoff.cpp
@@ -13,10 +13,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "softoff.hpp"
 namespace phosphor
 {
 namespace ipmi
 {
-    // Will be populated in the next patchset.
+
+// Need this to send SMS_ATTN
+constexpr auto HOST_IPMI_BUS  = "org.openbmc.HostIpmi";
+constexpr auto HOST_IPMI_OBJ  = "/org/openbmc/HostIpmi/1";
+constexpr auto HOST_IPMI_INTF = "org.openbmc.HostIpmi";
+
+/** @brief Send the SMS_ATN to host if value is set */
+int64_t SoftPowerOff::sendSmsAttn()
+{
+    // Response from BT
+    int64_t btResp {};
+
+    auto method = bus.new_method_call(HOST_IPMI_BUS,
+                                      HOST_IPMI_OBJ,
+                                      HOST_IPMI_INTF,
+                                      "setAttention");
+
+    // If there is any exception, would be thrown here.
+    auto reply = bus.call(method);
+
+    // BT returns '0' on success and bus_error on failure.
+    // Catching the rc anyway.
+    reply.read(btResp);
+
+    return btResp;
+}
 } // namespace ipmi
 } // namespace phosphor
