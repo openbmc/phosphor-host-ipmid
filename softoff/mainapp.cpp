2d9b4cc1521a6c928ef8c271f1eead71a75c69b6
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-01-25 16:29:43 +0530

    Add watchdog timer skeleton
    This patchset creates the watchdog timer infrastructure that is then
    used by soft power off object on user requests.
    
    Change-Id: I6f7a5c161999fda89471f453c24725efddac65b9
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- softoff/mainapp.cpp
+++ softoff/mainapp.cpp
@@ -13,26 +13,54 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <systemd/sd-event.h>
 #include <log.hpp>
 #include "softoff.hpp"
 #include "../config.h"
+#include "timer.hpp"
 
 using namespace phosphor::logging;
 
 int main(int argc, char** argv)
 {
+    // systemd event handler
+    sd_event* events = nullptr;
+    sd_event_source* eventSource = nullptr;
+
     // Get a handle to system dbus.
     auto bus = sdbusplus::bus::new_default();
 
     // Add systemd object manager.
     sdbusplus::server::manager::manager(bus, OBJPATH);
 
+    // sd_event object
+    auto r = sd_event_default(&events);
+    if (r < 0)
+    {
+        log<level::ERR>("Failure to create sd_event handler",
+                entry("ERROR=%s", strerror(-r)));
+        return -1;
+    }
+
+    // Create the Timer object
+    phosphor::ipmi::Timer timer(bus, events, eventSource,
+                phosphor::ipmi::SoftPowerOff::timeoutHandler);
+
+    // Initialize the timer object
+    r = timer.initialize();
+    if (r < 0)
+    {
+        log<level::ERR>("Failure initializing the timer object",
+                entry("ERROR=%s", strerror(-r)));
+        return -1;
+    }
+
     // Create the SoftPowerOff object.
-    phosphor::ipmi::SoftPowerOff object(bus, OBJPATH);
+    phosphor::ipmi::SoftPowerOff powerObj(bus, OBJPATH, timer);
 
     // The whole purpose of this application is to send SMS_ATTN
     // and watch for the soft power off to go through.
-    int64_t resp = object.sendSmsAttn();
+    int64_t resp = powerObj.sendSmsAttn();
     if (resp)
     {
         log<level::ERR>("Failure to send SMS_ATN.",
@@ -43,12 +71,19 @@ int main(int argc, char** argv)
     /** @brief Claim the bus */
     bus.request_name(BUSNAME);
 
-    /** @brief Wait for client requests */
-    while(true)
+    /** @brief Wait for client requests until this application has processed
+     *         at least one successful SoftPowerOff
+     */
+    while(!powerObj.isCompleted() && !timer.expired)
     {
-        // Handle dbus message / signals discarding unhandled
-        bus.process_discard();
-        bus.wait();
+        // -1 denotes wait for ever
+        r = sd_event_run(events, (uint64_t)-1);
+        if (r < 0)
+        {
+            log<level::ERR>("Failure in processing request",
+                    entry("ERROR=%s", strerror(-r)));
+            return -1;
+        }
     }
     return 0;
 }
---
+++ softoff/timer.cpp
@@ -0,0 +1,38 @@
+#include "timer.hpp"
+#include <log.hpp>
+namespace phosphor
+{
+namespace ipmi
+{
+
+using namespace phosphor::logging;
+
+// Initializes the timer object
+int Timer::initialize()
+{
+    // Add infinite expiration time
+    auto r = sd_event_add_time(timeEvent, &eventSource,
+                               CLOCK_MONOTONIC, // Time base
+                               UINT_MAX,        // Expire time - never
+                               0,               // Use default event accuracy
+                               timeoutHandler,  // Callback handler on timeout
+                               this);           // User data
+    if (r < 0)
+    {
+        log<level::ERR>("Failure to set initial expiration time value",
+                entry("ERROR=%s", strerror(-r)));
+        return r;
+    }
+
+    // Disable the timer for now
+    r = sd_event_source_set_enabled(eventSource, SD_EVENT_OFF);
+    if (r < 0)
+    {
+        log<level::ERR>("Failure to disable timer",
+                entry("ERROR=%s", strerror(-r)));
+    }
+    return r;
+}
+
+} // namespace ipmi
+} // namespace phosphor
