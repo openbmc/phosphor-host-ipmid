608dbf8e2a02c41222048e9ec78bdb2a28f727db
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-01-25 16:29:43 +0530

    Add watchdog timeout handler
    This application starts 2 watchdog timers. One after sending SMS_ATN and
    other after receiving the response from host for the initial SMS_ATN.
    If everything goes through, then the host will send appropriate response
    after the partition shutdown and thus no further actions are needed by
    this application and it terminates. However, if the host is not sending
    the response, then any of the 2 watchdog timers may timeout and this
    will then trigger a hard power off and terminates itself.
    
    Change-Id: I6f7a5c161999fda89471f453c24725efddac65b9
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- softoff/softoff.cpp
+++ softoff/softoff.cpp
@@ -19,7 +19,11 @@ namespace phosphor
 namespace ipmi
 {
 
+// Initialize the static member
+bool SoftPowerOff::completed = false;
+
 // Need this to send SMS_ATTN
+// TODO : Switch over to using mapper service in a different patch
 constexpr auto HOST_IPMI_BUS  = "org.openbmc.HostIpmi";
 constexpr auto HOST_IPMI_OBJ  = "/org/openbmc/HostIpmi/1";
 constexpr auto HOST_IPMI_INTF = "org.openbmc.HostIpmi";
@@ -28,12 +32,41 @@ constexpr auto HOST_IPMI_INTF = "org.openbmc.HostIpmi";
 void SoftPowerOff::sendSmsAttn()
 {
     auto method = bus.new_method_call(HOST_IPMI_BUS,
-                                    HOST_IPMI_OBJ,
-                                    HOST_IPMI_INTF,
-                                    "setAttention");
+                                      HOST_IPMI_OBJ,
+                                      HOST_IPMI_INTF,
+                                      "setAttention");
 
     // If there is any exception, would be thrown here.
     bus.call(method);
 }
+
+/** @brief Gets called into on receiving watchdog 'Timeout' signal */
+int SoftPowerOff::handleTimeOut(sd_bus_message* msg, void* userData,
+                                sd_bus_error* retError)
+{
+    // Get the handle to this object that was bound during registration
+    auto thisObject = static_cast<SoftPowerOff*>(userData);
+
+    // The only thing that is to be done here is to make a call to Chassis
+    // object to do Hard Power Off.
+    auto method = thisObject->bus.new_method_call(wdog::CHASSIS_SERVICE,
+                                                  wdog::CHASSIS_OBJ,
+                                                  wdog::PROPERTY_INTF,
+                                                  "Set");
+
+    // Fill the Interface, property name and its value.
+    method.append(wdog::CHASSIS_INTF,
+                  "RequestedPowerTransition",
+                  wdog::CHASSIS_OFF);
+
+    // Set the property.
+    thisObject->bus.call(method);
+
+    // This marks the END of life for this application.
+    completed = true;
+
+    return 0;
+}
+
 } // namespace ipmi
 } // namespace phosphor
