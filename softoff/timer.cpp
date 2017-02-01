f45d3b525a3ea934fc942e3ae7dc0258784c79e6
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-02-01 18:53:14 +0530

    Handle response from host for soft power off request
    Change-Id: Ia923eff57f855dc88cb04db13590ae1a58a502fd
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- softoff/timer.cpp
+++ softoff/timer.cpp
@@ -8,7 +8,7 @@ namespace ipmi
 
 using namespace phosphor::logging;
 
-// Initializes the timer object
+/** @brief Initializes the timer object */
 int Timer::initialize()
 {
     // Add infinite expiration time
@@ -35,7 +35,7 @@ int Timer::initialize()
     return r;
 }
 
-// Timeout callback handler
+/** @brief Timeout callback handler */
 int Timer::timeoutHandler(sd_event_source* eventSource,
                           uint64_t usec, void* userData)
 {
@@ -63,8 +63,9 @@ int Timer::timeoutHandler(sd_event_source* eventSource,
     return 0;
 }
 
-// Gets the time from specified type, could be
-// CLOCK_MONOTONIC, CLOCK_REALTIME and other allowed ones
+/** @brief Gets the time from specified type, could be
+ *  CLOCK_MONOTONIC, CLOCK_REALTIME and other allowed ones
+ */
 uint64_t Timer::getTime(clockid_t clockId)
 {
     using namespace std::chrono;
@@ -87,13 +88,13 @@ uint64_t Timer::getTime(clockid_t clockId)
     return time.count();
 }
 
-// Enables or disables the timer
+/** @brief Enables or disables the timer */
 int Timer::armTimer(int action)
 {
     return sd_event_source_set_enabled(eventSource, action);
 }
 
-// Sets the time and arms the timer
+/** @brief Sets the time and arms the timer */
 int Timer::startTimer(uint64_t timeValue)
 {
     // Disable the timer
