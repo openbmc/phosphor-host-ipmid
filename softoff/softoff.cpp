5f9b0ac8b5d0ba05c6e41735393d1dcf4bdc139a
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-02-01 16:08:52 +0530

    Add watchdog timeout handler
    This application starts 2 watchdog timers. One after sending SMS_ATN and
    other after receiving the response from host for the initial SMS_ATN.
    If everything goes through, then the host will send appropriate response
    after quiescing and thus no further actions are needed by
    this application and it terminates. However, if the host is not sending
    the response, then any of the 2 watchdog timers may timeout and this
    will then trigger a hard power off and terminates itself.
    
    Change-Id: I874e22b1402834afe5c032593126dd98068361d8
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- softoff/softoff.cpp
+++ softoff/softoff.cpp
@@ -13,11 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <log.hpp>
 #include "softoff.hpp"
 namespace phosphor
 {
 namespace ipmi
 {
+
+/** @brief Chassis dbus constructs to do a hard power off */
+// TODO : Need to move over to using mapper to get service name
+static constexpr auto CHASSIS_SERVICE = "xyz.openbmc_project.State.Chassis";
+static constexpr auto CHASSIS_OBJ     = "/xyz/openbmc_project/state/chassis0";
+static constexpr auto CHASSIS_INTF    = "xyz.openbmc_project.State.Chassis";
+
+// Property and the desired value
+static constexpr auto CHASSIS_OFF   = "xyz.openbmc_project.State.Chassis.\
+                                       Transition.Off";
+static constexpr auto PROPERTY_INTF = "org.freedesktop.DBus.Properties";
 
 /** @brief Send the SMS_ATN to host if value is set */
 int64_t SoftPowerOff::sendSmsAttn()
@@ -44,7 +56,30 @@ int64_t SoftPowerOff::sendSmsAttn()
 int SoftPowerOff::timeoutHandler(sd_event_source* eventSource,
                                  uint64_t usec, void* userData)
 {
-    // Will be filled with actual code
+    using namespace phosphor::logging;
+
+    log<level::INFO>("Watchdog timer expired !. Forcing Hard power off");
+
+    // Get the handle to this object that was bound during registration
+    auto timer = static_cast<Timer*>(userData);
+
+    // The only thing that is to be done here is to make a call
+    // to Chassis object to do Hard Power Off.
+    auto method = timer->bus.new_method_call(CHASSIS_SERVICE,
+                                             CHASSIS_OBJ,
+                                             PROPERTY_INTF,
+                                             "Set");
+    // Fill the Interface, property name and its value.
+    method.append(CHASSIS_INTF,
+                  "RequestedPowerTransition",
+                  CHASSIS_OFF);
+
+    // Set the property.
+    timer->bus.call(method);
+
+    // The timer is now considerd 'expired` !!
+    timer->expired = true;
+
     return 0;
 }
 
