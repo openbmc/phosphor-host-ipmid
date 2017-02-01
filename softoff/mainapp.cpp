e104440beb7503c8e5f822d5f1e72d4869caa9a8
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-02-01 18:53:14 +0530

    Handle response from host for soft power off request
    Change-Id: Ia923eff57f855dc88cb04db13590ae1a58a502fd
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- softoff/mainapp.cpp
+++ softoff/mainapp.cpp
@@ -13,16 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <chrono>
 #include <systemd/sd-event.h>
 #include <log.hpp>
 #include "softoff.hpp"
 #include "config.h"
 #include "timer.hpp"
 
-using namespace phosphor::logging;
-
 int main(int argc, char** argv)
 {
+    using namespace phosphor::logging;
+    using namespace std::chrono;
+
     // systemd event handler
     sd_event* events = nullptr;
     sd_event_source* eventSource = nullptr;
@@ -33,13 +35,14 @@ int main(int argc, char** argv)
     // Add systemd object manager.
     sdbusplus::server::manager::manager(bus, OBJPATH);
 
-    // sd_event object
+    // sd_event object. StateManager wants that this applicatin return '0'
+    // always.
     auto r = sd_event_default(&events);
     if (r < 0)
     {
         log<level::ERR>("Failure to create sd_event handler",
                 entry("ERROR=%s", strerror(-r)));
-        return -1;
+        return 0;
     }
 
     // Create the Timer object
@@ -52,11 +55,17 @@ int main(int argc, char** argv)
     {
         log<level::ERR>("Failure initializing the timer object",
                 entry("ERROR=%s", strerror(-r)));
-        return -1;
+        return 0;
     }
 
     // Create the SoftPowerOff object.
     phosphor::ipmi::SoftPowerOff powerObj(bus, OBJPATH, timer);
+
+    /** @brief Claim the bus. Delaying it until sending SMS_ATN may result
+     *  in a race condition between this available and IPMI trying to send
+     *  message as a reponse to ack from host.
+     */
+    bus.request_name(BUSNAME);
 
     // The whole purpose of this application is to send SMS_ATTN
     // and watch for the soft power off to go through.
@@ -65,11 +74,21 @@ int main(int argc, char** argv)
     {
         log<level::ERR>("Failure to send SMS_ATN.",
                 entry("ERROR=%s", strerror(resp)));
-        return -1;
+        return 0;
     }
 
-    /** @brief Claim the bus */
-    bus.request_name(BUSNAME);
+    // Start the initial timer for host to ack the SMS_ATN
+    auto time = duration_cast<microseconds>(
+            seconds(IPMI_SMS_ATN_ACK_TIMEOUT_SECS));
+    r = timer.startTimer(time.count());
+    if (r < 0)
+    {
+        log<level::ERR>("Failure to start the SMS_ATN response timer",
+                entry("ERROR=%s", strerror(-r)));
+        // This application always needs to return success so that the
+        // remaining part of power off can continue
+        return 0;
+    }
 
     /** @brief Wait for client requests until this application has processed
      *         at least one successful SoftPowerOff
@@ -82,7 +101,6 @@ int main(int argc, char** argv)
         {
             log<level::ERR>("Failure in processing request",
                     entry("ERROR=%s", strerror(-r)));
-            return -1;
         }
     }
     return 0;
