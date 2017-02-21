c9ea7ce5280beda6205f676682cddc8bd53cb633
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-02-21 12:20:25 +0530

    TEST
    Change-Id: Iadd170c67177f1420cd96e5fa53e97a1e1c86483
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ softoff/mainapp.cpp
@@ -0,0 +1,107 @@
+/**
+ * Copyright © 2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <chrono>
+#include <systemd/sd-event.h>
+#include <log.hpp>
+#include "softoff.hpp"
+#include "config.h"
+#include "timer.hpp"
+
+int main(int argc, char** argv)
+{
+    using namespace phosphor::logging;
+    using namespace std::chrono;
+
+    // systemd event handler
+    sd_event* events = nullptr;
+    sd_event_source* eventSource = nullptr;
+
+    // Get a handle to system dbus.
+    auto bus = sdbusplus::bus::new_default();
+
+    // Add systemd object manager.
+    sdbusplus::server::manager::manager(bus, OBJPATH);
+
+    // sd_event object. StateManager wants that this applicatin return '0'
+    // always.
+    auto r = sd_event_default(&events);
+    if (r < 0)
+    {
+        log<level::ERR>("Failure to create sd_event handler",
+                entry("ERROR=%s", strerror(-r)));
+        return 0;
+    }
+
+    // Create the Timer object
+    phosphor::ipmi::Timer timer(events, eventSource,
+                phosphor::ipmi::SoftPowerOff::timeoutHandler);
+
+    // Initialize the timer object
+    r = timer.initialize();
+    if (r < 0)
+    {
+        log<level::ERR>("Failure initializing the timer object",
+                entry("ERROR=%s", strerror(-r)));
+        return 0;
+    }
+
+    // Create the SoftPowerOff object.
+    phosphor::ipmi::SoftPowerOff powerObj(bus, OBJPATH, timer);
+
+    /** @brief Claim the bus. Delaying it until sending SMS_ATN may result
+     *  in a race condition between this available and IPMI trying to send
+     *  message as a reponse to ack from host.
+     */
+    bus.request_name(BUSNAME);
+
+    // The whole purpose of this application is to send SMS_ATTN
+    // and watch for the soft power off to go through.
+    int64_t resp = powerObj.sendSmsAttn();
+    if (resp)
+    {
+        log<level::ERR>("Failure to send SMS_ATN.",
+                entry("ERROR=%s", strerror(resp)));
+        return 0;
+    }
+
+    // Start the initial timer for host to ack the SMS_ATN
+    auto time = duration_cast<microseconds>(
+            seconds(IPMI_SMS_ATN_ACK_TIMEOUT_SECS));
+    r = timer.startTimer(time.count());
+    if (r < 0)
+    {
+        log<level::ERR>("Failure to start the SMS_ATN response timer",
+                entry("ERROR=%s", strerror(-r)));
+        // This application always needs to return success so that the
+        // remaining part of power off can continue
+        return 0;
+    }
+
+    /** @brief Wait for client requests until this application has processed
+     *         at least one successful SoftPowerOff
+     */
+    while(!powerObj.isCompleted() && !timer.expired)
+    {
+        // -1 denotes wait for ever
+        r = sd_event_run(events, (uint64_t)-1);
+        if (r < 0)
+        {
+            log<level::ERR>("Failure in processing request",
+                    entry("ERROR=%s", strerror(-r)));
+        }
+    }
+    return 0;
+}
---
+++ softoff/softoff.cpp
@@ -0,0 +1,109 @@
+/**
+ * Copyright © 2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <chrono>
+#include <log.hpp>
+#include "softoff.hpp"
+#include "config.h"
+
+namespace phosphor
+{
+namespace ipmi
+{
+
+/** @brief Sends the SMS_ATN to host if value is set */
+int64_t SoftPowerOff::sendSmsAttn()
+{
+    // Response from BT
+    int64_t btResp {};
+
+    auto method = bus.new_method_call(HOST_IPMI_BUS,
+                                      HOST_IPMI_OBJ,
+                                      HOST_IPMI_INTF,
+                                      "setAttention");
+
+    // If there is any exception, would be thrown here.
+    auto reply = bus.call(method);
+
+    // BT returns '0' on success and bus_error on failure.
+    // Catching the rc anyway.
+    reply.read(btResp);
+
+    return btResp;
+}
+
+/** @brief callback handler on timeout */
+int SoftPowerOff::timeoutHandler(sd_event_source* eventSource,
+                                 uint64_t usec, void* userData)
+{
+    using namespace phosphor::logging;
+
+    // Get the handle to this object that was bound during registration
+    auto timer = static_cast<Timer*>(userData);
+
+    log<level::ERR>("SoftOff Watchdog timer expired");
+
+    // The timer is now considerd 'expired` !!
+    timer->expired = true;
+
+    // Nothing more needs to be done here.
+    return 0;
+}
+
+/** @brief Host Response handler */
+auto SoftPowerOff::responseReceived(HostResponse response) -> HostResponse
+{
+    using namespace std::chrono;
+    using namespace phosphor::logging;
+
+    if (response == HostResponse::SoftOffReceived)
+    {
+        // Need to stop the running timer and then start a new timer
+        auto time = duration_cast<microseconds>(
+                seconds(IPMI_HOST_SHUTDOWN_COMPLETE_TIMEOUT_SECS));
+        auto r = timer.startTimer(time.count());
+        if (r < 0)
+        {
+            log<level::ERR>("Failure to start HostQuiesce wait timer",
+                    entry("ERROR=%s", strerror(-r)));
+
+            return sdbusplus::xyz::openbmc_project::Ipmi::Internal
+                    ::server::SoftPowerOff::responseReceived();
+        }
+    }
+    else if (response == HostResponse::HostShutdown)
+    {
+        // Disable the timer since Host has quiesced and we are
+        // done with soft power off part
+        auto r = timer.setTimer(SD_EVENT_OFF);
+        if (r < 0)
+        {
+            log<level::ERR>("Failure to STOP the timer",
+                    entry("ERROR=%s", strerror(-r)));
+
+            return sdbusplus::xyz::openbmc_project::Ipmi::Internal
+                    ::server::SoftPowerOff::responseReceived();
+        }
+
+        // This marks the completion of soft power off sequence.
+        completed = true;
+    }
+
+    return sdbusplus::xyz::openbmc_project::Ipmi::Internal
+              ::server::SoftPowerOff::responseReceived(response);
+}
+
+} // namespace ipmi
+} // namespace phosphor
