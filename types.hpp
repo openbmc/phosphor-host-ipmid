a65fee44cb3321f3b3e61a74df24c06def6b3ca5
Ratan Gupta <ratagupt@in.ibm.com>
2017-02-23 17:29:08 +0530

    inventory: write sensor yaml parser
    Write python-based parser for sensor.yaml. The parser generates
    sensor-gen.cpp,which is having a map of sensorid(key) and
    sensorinfo(value).
    
    Change-Id: I5aa2d2bc871ced06e5e4c164a67eeb5974031d5d
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ types.hpp
@@ -0,0 +1,47 @@
+#include <stdint.h>
+
+#include <map>
+#include <string>
+
+#include <sdbusplus/server.hpp>
+
+namespace ipmi
+{
+namespace sensor
+{
+
+using Offset = uint8_t;
+using ValueOnOffset = sdbusplus::message::variant<bool, int64_t, std::string>;
+
+struct Value
+{
+   ValueOnOffset assert;
+   ValueOnOffset deassert;
+};
+
+using OffsetValueMap = std::map<Offset,Value>;
+
+using DbusProperty = std::string;
+using DbusPropertyMap = std::map<DbusProperty,OffsetValueMap>;
+
+using DbusInterface = std::string;
+using DbusInterfaceMap = std::map<DbusInterface,DbusPropertyMap>;
+
+using InstancePath = std::string;
+using Type = uint8_t;
+using ReadingType = uint8_t;
+
+struct Info
+{
+   Type sensorType;
+   InstancePath sensorPath;
+   ReadingType sensorReadingType;
+   DbusInterfaceMap sensorInterfaces;
+};
+
+
+using ID = uint8_t;
+using IDInfoMap = std::map<ID,Info>;
+
+}//namespce sensor
+}//namespace ipmi
