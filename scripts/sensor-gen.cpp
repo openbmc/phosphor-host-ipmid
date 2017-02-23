2faf77c7a06549ac6d2962c2d66e1cc6078e2515
Ratan Gupta <ratagupt@in.ibm.com>
2017-02-23 17:37:10 +0530

    DO NOT MERGE: output cpp file.
    Will abandon later.
    
    Change-Id: Ie0da3d544c869926e8ff5c53b1e0660b1f955d50
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ scripts/sensor-gen.cpp
@@ -0,0 +1,508 @@
+
+// !!! WARNING: This is a GENERATED Code..Please do NOT Edit !!!
+
+#include "types.hpp"
+
+extern const sensorMap sensors = {
+{128,{
+
+        7,"/system/chassis/motherboard/cpu1/core12",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{131,{
+
+        7,"/system/chassis/motherboard/cpu1/core13",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{134,{
+
+        7,"/system/chassis/motherboard/cpu1/core14",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{137,{
+
+        7,"/system/chassis/motherboard/cpu1/core15",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{140,{
+
+        7,"/system/chassis/motherboard/cpu1/core16",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{143,{
+
+        7,"/system/chassis/motherboard/cpu1/core17",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{24,{
+
+        7,"/system/chassis/motherboard/cpu0/core2",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{18,{
+
+        7,"/system/chassis/motherboard/cpu0/core0",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{21,{
+
+        7,"/system/chassis/motherboard/cpu0/core1",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{152,{
+
+        7,"/system/chassis/motherboard/cpu1/core20",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{27,{
+
+        7,"/system/chassis/motherboard/cpu0/core3",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{30,{
+
+        7,"/system/chassis/motherboard/cpu0/core4",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{33,{
+
+        7,"/system/chassis/motherboard/cpu0/core5",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{155,{
+
+        7,"/system/chassis/motherboard/cpu1/core21",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{36,{
+
+        7,"/system/chassis/motherboard/cpu0/core6",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{39,{
+
+        7,"/system/chassis/motherboard/cpu0/core7",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{42,{
+
+        7,"/system/chassis/motherboard/cpu0/core8",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{45,{
+
+        7,"/system/chassis/motherboard/cpu0/core9",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{48,{
+
+        7,"/system/chassis/motherboard/cpu0/core10",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{51,{
+
+        7,"/system/chassis/motherboard/cpu0/core11",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{158,{
+
+        7,"/system/chassis/motherboard/cpu1/core22",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{54,{
+
+        7,"/system/chassis/motherboard/cpu0/core12",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{57,{
+
+        7,"/system/chassis/motherboard/cpu0/core13",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{60,{
+
+        7,"/system/chassis/motherboard/cpu0/core14",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{63,{
+
+        7,"/system/chassis/motherboard/cpu0/core15",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{66,{
+
+        7,"/system/chassis/motherboard/cpu0/core16",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{69,{
+
+        7,"/system/chassis/motherboard/cpu0/core17",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{161,{
+
+        7,"/system/chassis/motherboard/cpu1/core23",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{72,{
+
+        7,"/system/chassis/motherboard/cpu0/core18",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{75,{
+
+        7,"/system/chassis/motherboard/cpu0/core19",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{78,{
+
+        7,"/system/chassis/motherboard/cpu0/core20",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{81,{
+
+        7,"/system/chassis/motherboard/cpu0/core21",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{84,{
+
+        7,"/system/chassis/motherboard/cpu0/core22",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{87,{
+
+        7,"/system/chassis/motherboard/cpu0/core23",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{164,{
+
+        7,"/system/chassis/motherboard/cpu1",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{90,{
+
+        7,"/system/chassis/motherboard/cpu0",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{92,{
+
+        7,"/system/chassis/motherboard/cpu1/core0",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{95,{
+
+        7,"/system/chassis/motherboard/cpu1/core1",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{98,{
+
+        7,"/system/chassis/motherboard/cpu1/core2",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{101,{
+
+        7,"/system/chassis/motherboard/cpu1/core3",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{104,{
+
+        7,"/system/chassis/motherboard/cpu1/core4",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{107,{
+
+        7,"/system/chassis/motherboard/cpu1/core5",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{146,{
+
+        7,"/system/chassis/motherboard/cpu1/core18",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{110,{
+
+        7,"/system/chassis/motherboard/cpu1/core6",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{113,{
+
+        7,"/system/chassis/motherboard/cpu1/core7",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{116,{
+
+        7,"/system/chassis/motherboard/cpu1/core8",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{119,{
+
+        7,"/system/chassis/motherboard/cpu1/core9",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{122,{
+
+        7,"/system/chassis/motherboard/cpu1/core10",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{125,{
+
+        7,"/system/chassis/motherboard/cpu1/core11",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+{149,{
+
+        7,"/system/chassis/motherboard/cpu1/core19",111,{
+            {"xyz.openbmc_project.Inventory.Item",{
+                {"Present",{
+                    {"offset","7"},
+                }},
+            }},}
+     }
+},
+};
+
