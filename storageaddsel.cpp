b9ac6a425beec03d2b65085cec6b5e6d5272b241
Tom Joseph <tomjoseph@in.ibm.com>
2017-02-28 19:56:33 +0530

    Add null termination for eSEL data
    Change-Id: If55367f35cfcf35f0dff8da2d1e15865b702a5b2
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- storageaddsel.cpp
+++ storageaddsel.cpp
@@ -182,12 +182,14 @@ int send_esel_to_dbus(const char *desc, const char *sev, const char *details, ui
 
     // Allocate enough space to represent the data in hex separated by spaces,
     // to mimic how IPMI would display the data.
-    unique_ptr<char[]> selData(new char[debuglen*3]());
+    unique_ptr<char[]> selData(new char[(debuglen*3) + 1]());
     uint32_t i = 0;
     for(i = 0; i < debuglen; i++)
     {
         sprintf(&selData[i*3], "%02x ", 0xFF & ((char*)debug)[i]);
     }
+    selData[debuglen*3] = '\0';
+
     log<level::INFO>("Received Host Event", entry("ESEL=%s", selData.get()));
 
     try
