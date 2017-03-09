2fcf5fe094152f1810235d3bd72e852c49eece4b
Tom Joseph <tomjoseph@in.ibm.com>
2017-03-09 12:34:35 +0530

    Modify Set Sensor Handling Command
    The Set Sensor handling code is modified to use generated code.
    
    Change-Id: I3b7fa4da870b745873da4732d457d793f5549ada
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- types.hpp
+++ types.hpp
@@ -44,5 +44,17 @@ struct Info
 using Id = uint8_t;
 using IdInfoMap = std::map<Id,Info>;
 
+using Path = std::string;
+
+using Property = std::string;
+using Value = sdbusplus::message::variant<bool, int64_t, std::string>;
+using PropertyMap = std::map<Property, Value>;
+
+using Interface = std::string;
+using InterfaceMap = std::map<Interface, PropertyMap>;
+
+using Object = sdbusplus::message::object_path;
+using ObjectMap = std::map<Object, InterfaceMap>;
+
 }//namespce sensor
 }//namespace ipmi
