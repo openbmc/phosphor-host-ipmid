cbe9ff6ae233c1aafcf54c28bb14dfe7a4aa1bf5
Andrew Geissler <andrewg@us.ibm.com>
2017-03-16 15:53:20 -0500

    Compile new control host code
    Change-Id: I0136467cd3f258fbed3e40e4b7269ed4d8a23e46
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

---
+++ host-interface.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/Control/Host/server.hpp>
+
+namespace phosphor
+{
+namespace host
+{
+
+/** @class Host
+ *  @brief OpenBMC control host interface implementation.
+ *  @details A concrete implementation for xyz.openbmc_project.Control.Host
+ *  DBus API.
+ */
+class Host : public sdbusplus::server::object::object<
+                sdbusplus::xyz::openbmc_project::Control::server::Host>
+{
+    public:
+        /** @brief Constructs Host Control Interface
+         *
+         * @param[in] bus       - The Dbus bus object
+         * @param[in] objPath   - The Dbus object path
+         */
+        Host(sdbusplus::bus::bus& bus,
+             const char* objPath) :
+             sdbusplus::server::object::object<
+                sdbusplus::xyz::openbmc_project::Control::server::Host>(
+                        bus, objPath)
+        {}
+};
+
+} // namespace host
+} // namespace phosphor
