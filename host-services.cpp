fb3a3049c31ddf15a5e81640cbc5058eb7eb50f0
Andrew Geissler <andrewg@us.ibm.com>
2017-03-20 21:17:04 -0500

    Support for putting entries onto queue
    Change-Id: I766cdfcaef7d5d000a9e216bc3307ea12c9ce2f8
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- host-services.cpp
+++ host-services.cpp
@@ -9,19 +9,6 @@
 
 void register_host_services() __attribute__((constructor));
 
-// OpenBMC Host IPMI dbus framework
-const char  *object_name   =  "/org/openbmc/HostIpmi/1";
-const char  *intf_name     =  "org.openbmc.HostIpmi";
-
-//------------------------------------------------------
-// Called by IPMID as part of the start up
-// -----------------------------------------------------
-int start_host_service(sd_bus *bus, sd_bus_slot *slot)
-{
-    int rc = 0;
-    return rc < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
-}
-
 //------------------------------------------------------
 // Callback register function
 // -----------------------------------------------------
@@ -29,12 +16,6 @@ void register_host_services()
 {
     // Gets a hook onto SYSTEM bus used by host-ipmid
     sd_bus *bus = ipmid_get_sd_bus_connection();
-
-    // Gets a hook onto SYSTEM bus slot used by host-ipmid
-    sd_bus_slot *ipmid_slot = ipmid_get_sd_bus_slot();
-
-    //start_host_service(bus, ipmid_slot);
-    start_host_service(bus, ipmid_slot);
 
     static auto sdbus = sdbusplus::bus::bus(sd_bus_ref(bus));
 
@@ -44,7 +25,8 @@ void register_host_services()
     // Add sdbusplus ObjectManager.
     sdbusplus::server::manager::manager objManager(sdbus, objPathInst.c_str());
 
-    phosphor::host::Host host(sdbus, objPathInst.c_str());
+    static phosphor::host::Host host(sdbus, objPathInst.c_str());
 
     sdbus.request_name(CONTROL_HOST_BUSNAME);
+
 }
