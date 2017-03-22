a40685bba63a60f4a46a58fb57d60ea2457794ff
Andrew Geissler <andrewg@us.ibm.com>
2017-03-21 22:58:28 -0500

    Basic queue working for sending multiple commands
    Sends signal upon completion of command
    
    Change-Id: Ic507f35af0b38305eecd5558c55738f2d283aac5
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- systemintfcmds.h
+++ systemintfcmds.h
@@ -13,6 +13,8 @@
 #define SOFT_OFF            0x00
 // Major command for Any kind of power ops
 #define CMD_POWER           0x04
+// Major command for the heartbeat operation (verify host is alive)
+#define CMD_HEARTBEAT       0xFF
 
 // IPMI commands used via System Interface functions.
 enum ipmi_netfn_system_intf_cmds
