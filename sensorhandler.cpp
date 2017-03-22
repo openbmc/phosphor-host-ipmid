79c201aa56212f4be1da25f004fed735734cf4aa
Jayanth Othayoth <ojayanth@in.ibm.com>
2017-03-22 06:00:58 -0500

    Sensor Reading Support for  Turbo Allowed and PS derating factor
    Change-Id: Ib2e89383fa1da4d3216ad5c50d257ec973547b71
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- sensorhandler.cpp
+++ sensorhandler.cpp
@@ -40,6 +40,8 @@ sensorTypemap_t g_SensorTypeMap[] = {
     {0xC7, 0x03, "SYSTEM"},
     {0xC7, 0x03, "MAIN_PLANAR"},
     {0xC2, 0x6F, "PowerCap"},
+    {0xDA, 0x03, "TurboAllowed"},
+    {0xB4, 0x6F, "PSDerating"},
     {0xFF, 0x00, ""},
 };
 
