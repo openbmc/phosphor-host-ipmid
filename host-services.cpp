50c8fbdaf878a29f63b4a63000a2beb32f9509a1
Andrew Geissler <andrewg@us.ibm.com>
2017-03-21 22:58:28 -0500

    Implement basic queue for sending multiple commands
    Sends dbus signal upon completion of command
    
    Change-Id: Ic507f35af0b38305eecd5558c55738f2d283aac5
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- host-services.cpp
+++ host-services.cpp
@@ -10,12 +10,37 @@
 void register_host_services() __attribute__((constructor));
 
 //------------------------------------------------------
+// Called by IPMID as part of the start up
+// -----------------------------------------------------
+int start_host_service(sd_bus *bus, sd_bus_slot *slot)
+{
+    int rc = 0;
+
+    /* TODO - Remove as last commit once org.openbmc.HostServices.service is
+     *        renamed to new interface name
+     */
+    rc = sd_bus_request_name(bus, "org.openbmc.HostServices", 0);
+    if (rc < 0)
+    {
+        fprintf(stderr, "Failed to acquire service name: %s\n", strerror(-rc));
+    }
+    return rc < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+//------------------------------------------------------
 // Callback register function
 // -----------------------------------------------------
+phosphor::host::Host *host = nullptr;
+
 void register_host_services()
 {
     // Gets a hook onto SYSTEM bus used by host-ipmid
     sd_bus *bus = ipmid_get_sd_bus_connection();
+
+    // Gets a hook onto SYSTEM bus slot used by host-ipmid
+    sd_bus_slot *ipmid_slot = ipmid_get_sd_bus_slot();
+
+    start_host_service(bus, ipmid_slot);
 
     static auto sdbus = sdbusplus::bus::bus(sd_bus_ref(bus));
 
@@ -25,8 +50,13 @@ void register_host_services()
     // Add sdbusplus ObjectManager.
     sdbusplus::server::manager::manager objManager(sdbus, objPathInst.c_str());
 
-    static phosphor::host::Host host(sdbus, objPathInst.c_str());
+    host = new phosphor::host::Host(sdbus, objPathInst.c_str());
 
     sdbus.request_name(CONTROL_HOST_BUSNAME);
 
+}
+
+phosphor::host::Host::Command getNextCmd()
+{
+    return(host->getNextCommand());
 }
