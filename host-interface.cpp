a56d4d0d3a48bced8ba4ae95c14c12594f25b6ec
Andrew Geissler <andrewg@us.ibm.com>
2017-03-22 14:02:30 -0500

    Enable multiple commands to be queued at once
    Change-Id: I76908d988608ef6dcc1d7f945c6c4a1c5c29e44b
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- host-interface.cpp
+++ host-interface.cpp
@@ -16,15 +16,14 @@ using namespace phosphor::logging;
 // When you see base:: you know we're referencing our base class
 namespace base = sdbusplus::xyz::openbmc_project::Control::server;
 
-void Host::execute(base::Host::Command command)
-{
-    log<level::INFO>("Pushing cmd on to queue",
-            entry("CONTROL_HOST_CMD=%s",
-                  convertForMessage(command)));
-    this->workQueue.push(command);
+// TODO - Add timeout function?
+//          - If host does not respond to SMS, need to signal a failure
+//      - Flush queue on power off?  - Timeout would do this for us for free
+//      - Ignore requests when host state not running? - Timeout handles too
 
-    // If this was the only entry then send the SMS attention
-    if(this->workQueue.size() == 1)
+void Host::checkQueue()
+{
+    if (this->workQueue.size() >= 1)
     {
         log<level::INFO>("Asserting SMS Attention");
 
@@ -45,6 +44,26 @@ void Host::execute(base::Host::Command command)
             return;
         }
         log<level::INFO>("SMS Attention asserted");
+    }
+}
+
+void Host::execute(base::Host::Command command)
+{
+    log<level::INFO>("Pushing cmd on to queue",
+            entry("CONTROL_HOST_CMD=%s",
+                  convertForMessage(command)));
+
+    this->workQueue.push(command);
+
+    // Alert host if this is only command in queue otherwise host will
+    // be notified of next message after processing the current one
+    if (this->workQueue.size() == 1)
+    {
+        this->checkQueue();
+    }
+    else
+    {
+        log<level::INFO>("Command in process, no attention");
     }
 
     return;
