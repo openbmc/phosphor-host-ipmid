8ea9246fbe7a657fb6c59b5880084bd8ccf2ac6a
Jayanth Othayoth <ojayanth@in.ibm.com>
2017-03-27 08:19:18 -0500

    Sensor Reading Support for Power Supply Derating
    Change-Id: I0eb106c37c33db01937ec61dd5553b5799651633
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- sensorhandler.cpp
+++ sensorhandler.cpp
@@ -40,6 +40,7 @@ sensorTypemap_t g_SensorTypeMap[] = {
     {0xC7, 0x03, "SYSTEM"},
     {0xC7, 0x03, "MAIN_PLANAR"},
     {0xC2, 0x6F, "PowerCap"},
+    {0xB4, 0x6F, "PowerSupplyDerating"},
     {0xFF, 0x00, ""},
 };
 
