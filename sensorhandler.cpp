cc0f74a8ca03c3173b8a55596472d470110384f4
Emily Shaffer <emilyshaffer@google.com>
2017-04-05 08:53:21 -0700

    sensorhandler: modify Get Sensor Reading to use dbus interface
    Change-Id: Ice16bda6c151b0865f3fa6e8234b8e2456d5b887
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>

--- sensorhandler.cpp
+++ sensorhandler.cpp
@@ -1,4 +1,5 @@
 #include <mapper.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <bitset>
@@ -553,6 +554,8 @@ ipmi_ret_t ipmi_sen_get_sensor_reading(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
 
     *data_len=0;
 
+    int raw_value, scale;
+
     switch(type) {
         case 0xC3:
         case 0xC2:
@@ -579,6 +582,58 @@ ipmi_ret_t ipmi_sen_get_sensor_reading(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
             resp->operation     = 0;
             resp->indication[0] = 0;
             resp->indication[1] = 0;
+            break;
+
+        case 0x01:
+        case 0x02:
+        case 0x03:
+        case 0x04:
+            // Get reading for /xyz/openbmc_project/Sensor/Value.interface
+
+            // Get value
+            r = sd_bus_get_property_trivial(bus,
+                                            a.bus,
+                                            a.path,
+                                            a.interface,
+                                            "Value",
+                                            NULL,
+                                            'x',
+                                            &raw_value);
+            if (r < 0) {
+                fprintf(stderr,
+                        "Failed to call sd_bus_get_property:%d,  %s, 'value'\n",
+                        r,
+                        strerror(-r));
+                fprintf(stderr, "Bus: %s, Path: %s, Interface: %s\n",
+                        a.bus, a.path, a.interface);
+                break;
+            }
+
+            // Get scale
+            r = sd_bus_get_property_trivial(bus,
+                                            a.bus,
+                                            a.path,
+                                            a.interface,
+                                            "Scale",
+                                            NULL,
+                                            'x',
+                                            &scale);
+            if (r < 0) {
+                fprintf(stderr,
+                        "Failed to call sd_bus_get_property:%d,  %s (scale)\n",
+                        r,
+                        strerror(-r));
+                fprintf(stderr, "Bus: %s, Path: %s, Interface: %s\n",
+                        a.bus, a.path, a.interface);
+                break;
+            }
+
+            resp->value = raw_value * pow(10,scale);
+            resp->operation = 0;
+            resp->indication[0] = 0;
+            resp->indication[1] = 0;
+            rc = IPMI_CC_OK;
+            *data_len=sizeof(sensorreadingresp_t);
             break;
 
         default:
