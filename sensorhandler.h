0d37e704fcb033b7f822d9573c55d00841655ffe
Emily Shaffer <emilyshaffer@google.com>
2017-05-08 16:36:17 -0700

    sensorhandler: implement get SDR
    Change-Id: I79dc59585c1f3008fc42e95a5a4792afd7268255
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>

--- sensorhandler.h
+++ sensorhandler.h
@@ -7,6 +7,7 @@
 enum ipmi_netfn_sen_cmds
 {
     IPMI_CMD_GET_SDR_INFO       = 0x20,
+    IPMI_CMD_GET_SDR            = 0x21,
     IPMI_CMD_RESERVE_SDR_REPO   = 0x22,
     IPMI_CMD_GET_SENSOR_READING = 0x2D,
     IPMI_CMD_GET_SENSOR_TYPE    = 0x2F,
@@ -76,4 +77,184 @@ struct GetSdrInfoResp
     uint8_t count;
     uint8_t luns_and_dynamic_popl;
 };
+
+/**
+ * Get SDR
+ */
+
+// Request
+#define GET_SDR_REQ_GET_RESERVATION_ID(r) (r->reservation_id_lsb + (r->reservation_id_msb << 8))
+#define GET_SDR_REQ_GET_RECORD_ID(r) (r->record_id_lsb + (r->record_id_msb << 8))
+struct GetSdrReq
+{
+    uint8_t reservation_id_lsb;
+    uint8_t reservation_id_msb;
+    uint8_t record_id_lsb;
+    uint8_t record_id_msb;
+    uint8_t offset;
+    uint8_t bytes_to_read;
+} __attribute__((packed));
+
+// Response
+#define GET_SDR_RESP_SET_NEXT_RECORD_ID(n,r) (r->next_record_id_lsb = n & 0xFF,\
+                                              r->next_record_id_msb = (n >> 8) & 0xFF)
+struct GetSdrResp
+{
+    uint8_t next_record_id_lsb;
+    uint8_t next_record_id_msb;
+    uint8_t record_data[64];
+} __attribute__((packed));
+
+#define SDR_HEADER_SET_RECORD_ID(n,h) ((h)->record_id_lsb = (n & 0xFF),\
+                                       (h)->record_id_msb = (n >> 8) & 0xFF)
+struct SensorDataRecordHeader
+{
+    uint8_t record_id_lsb;
+    uint8_t record_id_msb;
+    uint8_t sdr_version;
+    uint8_t record_type;
+    uint8_t record_length; // Length not counting the header
+} __attribute__((packed));
+
+enum SensorDataRecordType
+{
+    SENSOR_DATA_FULL_RECORD = 1,
+};
+
+#define SDR_KEY_SET_OWNER_ID_IPMB(k) ((k)->owner_id &= ~0x01)
+#define SDR_KEY_SET_OWNER_ID_SYSTEM_SW(k) ((k)->owner_id |= 0x01)
+#define SDR_KEY_SET_OWNER_ID_ADDRESS(a,k) ((k)->owner_id &= 0x01,\
+                                           (k)->owner_id |= a<<1)
+#define SDR_KEY_SET_OWNER_LUN(l,k) ((k)->owner_lun &= ~0x03,\
+                                    (k)->owner_lun |= (l&0x03))
+#define SDR_KEY_SET_OWNER_LUN_CHANNEL(c,k) ((k)->owner_lun &= 0x0F,\
+                                            (k)->owner_lun |= ((c & 0xF)<<4))
+struct SensorDataRecordKey
+{
+    uint8_t owner_id;
+    uint8_t owner_lun;
+    uint8_t sensor_number;
+} __attribute__((packed));
+
+#define SDR_FULL_BODY_SET_ENTITY_INSTANCE_NUMBER(n,b) ((b)->entity_instance &= 1<<7,\
+                                                       (b)->entity_instance |= (n & ~(1<<7))
+#define SDR_FULL_BODY_SET_ENTITY_PHYSICAL_ENTITY(b) ((b)->entity_instance &= ~(1<<7))
+#define SDR_FULL_BODY_SET_ENTITY_LOGICAL_CONTAINER(b) ((b)->entity_instance |= 1<<7)
+
+#define SDR_FULL_BODY_SENSOR_SCANNING_ENABLED(b) ((b)->sensor_initialization |= 1<<0)
+#define SDR_FULL_BODY_SENSOR_SCANNING_DISABLED(b) ((b)->sensor_initialization &= ~(1<<0))
+#define SDR_FULL_BODY_EVENT_GENERATION_ENABLED(b) ((b)->sensor_initialization |= 1<<1)
+#define SDR_FULL_BODY_EVENT_GENERATION_DISABLED(b) ((b)->sensor_initialization &= ~(1<<1))
+#define SDR_FULL_BODY_INIT_TYPES_ENABLED(b) ((b)->sensor_initialization |= 1<<2)
+#define SDR_FULL_BODY_INIT_TYPES_DISABLED(b) ((b)->sensor_initialization &= ~(1<<2))
+#define SDR_FULL_BODY_INIT_HYST_ENABLED(b) ((b)->sensor_initialization |= 1<<3)
+#define SDR_FULL_BODY_INIT_HYST_DISABLED(b) ((b)->sensor_initialization &= ~(1<<3))
+#define SDR_FULL_BODY_INIT_THRESH_ENABLED(b) ((b)->sensor_initialization |= 1<<4)
+#define SDR_FULL_BODY_INIT_THRESH_DISABLED(b) ((b)->sensor_initialization &= ~(1<<4))
+#define SDR_FULL_BODY_INIT_EVENTS_ENABLED(b) ((b)->sensor_initialization |= 1<<5)
+#define SDR_FULL_BODY_INIT_EVENTS_DISABLED(b) ((b)->sensor_initialization &= ~(1<<5))
+#define SDR_FULL_BODY_INIT_SCANNING_ENABLED(b) ((b)->sensor_initialization |= 1<<6)
+#define SDR_FULL_BODY_INIT_SCANNING_DISABLED(b) ((b)->sensor_initialization &= ~(1<<6))
+#define SDR_FULL_BODY_INIT_SETTABLE_ENABLED(b) ((b)->sensor_initialization |= 1<<7)
+#define SDR_FULL_BODY_INIT_SETTABLE_DISABLED(b) ((b)->sensor_initialization &= ~(1<<7))
+
+#define SDR_FULL_BODY_SET_PERCENTAGE(b) ((b)->sensor_units_1 |= 1<<0)
+#define SDR_FULL_BODY_UNSET_PERCENTAGE(b) ((b)->sensor_units_1 &= ~(1<<0))
+#define SDR_FULL_BODY_SET_MODIFIER_OPERATION(o,b) ((b)->sensor_units_1 &= ~(3<<1),\
+                                                   (b)->sensor_units_1 |= (o & 0x3)<<1)
+#define SDR_FULL_BODY_SET_RATE_UNIT(u,b) ((b)->sensor_units_1 &= ~(7<<3),\
+                                          (b)->sensor_units_1 |= (u & 0x7)<<3)
+#define SDR_FULL_BODY_SET_ANALOG_DATA_FORMAT(f,b) ((b)->sensor_units_1 &= ~(3<<6),\
+                                                   (b)->sensor_units_1 |= (f & 0x3)<<6)
+
+#define SDR_FULL_BODY_SET_M(m,b) ((b)->m_lsb = m & 0xFF,\
+                                  (b)->m_msb_and_tolerance &= ~(3<<6),\
+                                  (b)->m_msb_and_tolerance |= (m & (3<<8) >> 2))
+#define SDR_FULL_BODY_SET_TOLERANCE(t,b) ((b)->m_msb_and_tolerance &= ~0x3F,\
+                                          (b)->m_msb_and_tolerance |= t & 0x3F)
+
+#define SDR_FULL_BODY_SET_B(b,p) (p->b_lsb = b & 0xFF,\
+                                  p->b_msb_and_accuracy_lsb &= ~(3<<6),\
+                                  p->b_msb_and_accuracy_lsb |= (b & (3<<8) >> 2))
+#define SDR_FULL_BODY_SET_ACCURACY(t,b) ((b)->b_msb_and_accuracy_lsb &= ~0x3F,\
+                                         (b)->b_msb_and_accuracy_lsb |= t & 0x3F,\
+                                         (b)->accuracy_and_sensor_direction &= 0x0F,\
+                                         (b)->accuracy_and_sensor_direction |= (t & 0xF) << 4)
+#define SDR_FULL_BODY_SET_ACCURACY_EXP(e,b) ((b)->accuracy_and_sensor_direction &= ~(3<<2),\
+                                             (b)->accuracy_and_sensor_direction |= (e & 3)<<2)
+#define SDR_FULL_BODY_SET_SENSOR_DIR(d,b) ((b)->accuracy_and_sensor_direction &= ~(3<<0),\
+                                           (b)->accuracy_and_sensor_direction |= (d & 3))
+
+#define SDR_FULL_BODY_SET_B_EXP(e,b) ((b)->r_b_exponents &= 0xF0,\
+                                      (b)->r_b_exponents |= e & 0x0F)
+#define SDR_FULL_BODY_SET_R_EXP(e,b) ((b)->r_b_exponents &= 0x0F,\
+                                      (b)->r_b_exponents |= (e & 0x0F)<<4)
+
+#define SDR_FULL_BODY_SET_ID_STRLEN(l,b) ((b)->id_string_info &= ~(0x1F),\
+                                          (b)->id_string_info |= l & 0x1F)
+#define SDR_FULL_BODY_GET_ID_STRLEN(b) ((b)->id_string_info & 0x1F)
+#define SDR_FULL_BODY_SET_ID_TYPE(t,b) ((b)->id_string_info &= ~(3<<6),\
+                                        (b)->id_string_info |= (t & 0x3)<<6)
+#define FULL_RECORD_ID_STR_MAX_LENGTH 16
+
+struct SensorDataFullRecordBody
+{
+    uint8_t entity_id;
+    uint8_t entity_instance;
+    uint8_t sensor_initialization;
+    uint8_t sensor_capabilities; // no macro support
+    uint8_t sensor_type;
+    uint8_t event_reading_type;
+    uint8_t supported_assertions[2]; // no macro support
+    uint8_t supported_deassertions[2]; // no macro support
+    uint8_t discrete_reading_setting_mask[2]; // no macro support
+    uint8_t sensor_units_1;
+    uint8_t sensor_units_2_base;
+    uint8_t sensor_units_3_modifier;
+    uint8_t linearization;
+    uint8_t m_lsb;
+    uint8_t m_msb_and_tolerance;
+    uint8_t b_lsb;
+    uint8_t b_msb_and_accuracy_lsb;
+    uint8_t accuracy_and_sensor_direction;
+    uint8_t r_b_exponents;
+    uint8_t analog_characteristic_flags; //no macro support
+    uint8_t nominal_reading;
+    uint8_t normal_max;
+    uint8_t normal_min;
+    uint8_t sensor_max;
+    uint8_t sensor_min;
+    uint8_t upper_nonrecoverable_threshold;
+    uint8_t upper_critical_threshold;
+    uint8_t upper_noncritical_threshold;
+    uint8_t lower_nonrecoverable_threshold;
+    uint8_t lower_critical_threshold;
+    uint8_t lower_noncritical_threshold;
+    uint8_t positive_threshold_hysteresis;
+    uint8_t negative_threshold_hysteresis;
+    uint16_t reserved;
+    uint8_t oem_reserved;
+    uint8_t id_string_info;
+    char id_string[16];
+} __attribute__((packed));
+
+// More types contained in section 43.17 Sensor Unit Type Codes,
+// IPMI spec v2 rev 1.1
+enum SensorUnitTypeCodes
+{
+    SENSOR_UNIT_UNSPECIFIED = 0,
+    SENSOR_UNIT_DEGREES_C = 1,
+    SENSOR_UNIT_VOLTS = 4,
+    SENSOR_UNIT_AMPERES = 5,
+    SENSOR_UNIT_JOULES = 7,
+    SENSOR_UNIT_METERS = 34,
+    SENSOR_UNIT_REVOLUTIONS = 41,
+};
+
+struct SensorDataFullRecord
+{
+    SensorDataRecordHeader header;
+    SensorDataRecordKey key;
+    SensorDataFullRecordBody body;
+} __attribute__((packed));
 #endif
