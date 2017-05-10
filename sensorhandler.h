4d80b096fb404058730109ae7ea8b6bec351196f
Emily Shaffer <emilyshaffer@google.com>
2017-05-10 12:01:10 -0700

    sensorhandler: Add functionality to use m, b, b_exp from config.yaml
    Change-Id: I1b5b8a5e0e5abd74eadde494728f363cf1ca4ec6
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>

--- sensorhandler.h
+++ sensorhandler.h
@@ -235,7 +235,7 @@ struct SensorDataFullRecordBody
     uint16_t reserved;
     uint8_t oem_reserved;
     uint8_t id_string_info;
-    char id_string[16];
+    char id_string[FULL_RECORD_ID_STR_MAX_LENGTH];
 } __attribute__((packed));
 
 // More types contained in section 43.17 Sensor Unit Type Codes,
