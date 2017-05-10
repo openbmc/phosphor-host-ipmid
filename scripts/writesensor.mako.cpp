71e58c1338e1c7d72cfe3d5a4df27c72663be1a5
Emily Shaffer <emilyshaffer@google.com>
2017-05-10 12:01:10 -0700

    sensorhandler: Add functionality to use m, b, b_exp from config.yaml
    Change-Id: I1b5b8a5e0e5abd74eadde494728f363cf1ca4ec6
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>

--- scripts/writesensor.mako.cpp
+++ scripts/writesensor.mako.cpp
@@ -16,8 +16,12 @@ extern const IdInfoMap sensors = {
        path = sensor["path"]
        sensorType = sensor["sensorType"]
        readingType = sensor["sensorReadingType"]
+       multiplier = sensor["multiplierM"]
+       offset = sensor["offsetB"]
+       exp = sensor["bExp"]
 %>
-        ${sensorType},"${path}",${readingType},{
+        ${sensorType},"${path}",${readingType},${multiplier},${offset},${exp},
+        ${offset * pow(10,exp)},{
     % for interface,properties in interfaces.iteritems():
             {"${interface}",{
             % for dbus_property,property_value in properties.iteritems():
