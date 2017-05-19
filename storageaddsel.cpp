9aa426446a11c67f955b2e4e67bc8d8ff5028d26
Marri Devender Rao <devenrao@in.ibm.com>
2017-05-19 04:33:29 -0500

    storageaddsel: fix error-log usage
    Log sbusplus exceptions instead of phosphor error exceptions.
    Pick metadata structures from the phosphor-logging generated
    types
    
    Change-Id: I923676cb419c6e22c0586e9ceea5382e71143e4d
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- storageaddsel.cpp
+++ storageaddsel.cpp
@@ -9,7 +9,8 @@
 #include <systemd/sd-bus.h>
 #include <mapper.h>
 #include <phosphor-logging/elog.hpp>
-#include <phosphor-logging/elog-errors-HostEvent.hpp>
+#include <org/open_power/Host/Event/error.hpp>
+#include <phosphor-logging/elog-errors.hpp>
 #include "host-ipmid/ipmid-api.h"
 #include "sensorhandler.h"
 #include "storagehandler.h"
@@ -190,8 +191,9 @@ int send_esel_to_dbus(const char *desc, const char *sev, const char *details, ui
     }
     selData[debuglen*3] = '\0';
 
-    using error = org::open_power::Error::Host::Event;
-    report<error>(error::ESEL(selData.get()));
+    using error = sdbusplus::org::open_power::Host::Event::Error::Event;
+    using metadata = org::open_power::Host::Event::Event;
+    report<error>(metadata::ESEL(selData.get()));
 
     return 0;
 }
