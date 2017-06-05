d57f4f5c7f08f1117e9038dd48e35258fe2a119f
Marri Devender Rao <devenrao@in.ibm.com>
2017-06-04 23:40:16 -0500

    generate map of fru properties
    Reusing frup.hpp, example.yaml, fru-gen.py, writefru.mako.cpp from
    ipmi-fru-gen repository to generate map of frup properties.
    
    Generated map will be used to support read the data from the
    inventory
    
    Change-Id: I6d6dbc55a340dd06b4639f8bded8cc09df815a59
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

---
+++ scripts/readfru.mako.cpp
@@ -0,0 +1,35 @@
+// !!! WARNING: This is a GENERATED Code..Please do NOT Edit !!!
+#include <iostream>
+#include "fruread.hpp"
+
+extern const FruMap frus = {
+% for key in fruDict.keys():
+   {${key},{
+<%
+    fru = fruDict[key]
+%>
+    % for object,interfaces in fru.items():
+         {"${object}",{
+         % for interface,properties in interfaces.items():
+             {"${interface}",{
+            % for dbus_property,property_value in properties.items():
+                 {"${dbus_property}",{
+                     "${property_value.get("IPMIFruSection", "")}",
+                     "${property_value.get("IPMIFruProperty", "")}", \
+<%
+    delimiter = property_value.get("IPMIFruValueDelimiter")
+    if not delimiter:
+        delimiter = ""
+    else:
+        delimiter = '\\' + hex(delimiter)[1:]
+%>
+                     "${delimiter}"
+                 }},
+            % endfor
+             }},
+         % endfor
+        }},
+    % endfor
+   }},
+% endfor
+};
