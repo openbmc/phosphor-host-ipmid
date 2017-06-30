fbea03c7c61044ca483ccd67887dd80f24aea35c
Tom Joseph <tomjoseph@in.ibm.com>
2017-06-30 18:40:30 +0530

    Get the timestamp from the log entry
    Change-Id: Id00f6e1e6f65c17babc5d06abe1b9fc39a4de8b8
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- selutility.hpp
+++ selutility.hpp
@@ -51,6 +51,13 @@ struct GetSELEntryResponse
  */
 GetSELEntryResponse convertLogEntrytoSEL(const std::string& objPath);
 
+/** @brief Get the timestamp of the log entry
+ *
+ *  @param[in] objPath - DBUS object path of the logging entry.
+ *
+ *  @return On success return the timestamp of the log entry.
+ */
+uint32_t getEntryTimeStamp(const std::string& objPath);
 
 namespace internal
 {
@@ -68,7 +75,7 @@ GetSELEntryResponse prepareSELEntry(
         const std::string& objPath,
         ipmi::sensor::InvObjectIDMap::const_iterator iter);
 
-} //internal
+}
 
 } // namespace sel
 
