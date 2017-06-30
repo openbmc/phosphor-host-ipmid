1b1507f38348e06909696c96f68b3024e6c18a93
Tom Joseph <tomjoseph@in.ibm.com>
2017-06-30 19:14:49 +0530

    Add Clear SEL command
    Resolves openbmc/openbmc#1515
    
    Change-Id: I35b07e0bf1012d4b660d10cf17a47318f3c4e6e2
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- selutility.hpp
+++ selutility.hpp
@@ -105,6 +105,23 @@ struct DeleteSELEntryRequest
     uint16_t selRecordID;           //!< SEL Record ID.
 } __attribute__((packed));
 
+static constexpr auto initiateErase = 0xAA;
+static constexpr auto getEraseStatus = 0x00;
+static constexpr auto eraseComplete = 0x01;
+
+/** @struct ClearSELRequest
+ *
+ *  IPMI payload for Clear SEL command request.
+ */
+struct ClearSELRequest
+{
+    uint16_t reservationID;         //!< Reservation ID.
+    uint8_t charC;                  //!< Char 'C'(0x43h).
+    uint8_t charL;                  //!< Char 'L'(0x4Ch).
+    uint8_t charR;                  //!< Char 'R'(0x52h).
+    uint8_t eraseOperation;         //!< Erase operation.
+} __attribute__((packed));
+
 /** @brief Convert logging entry to SEL
  *
  *  @param[in] objPath - DBUS object path of the logging entry.
