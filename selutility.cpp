a54c7fa6b084cadb6ffea7f4797f43a1ffdc7c75
Tom Joseph <tomjoseph@in.ibm.com>
2017-06-30 18:49:09 +0530

    Add support to prepare SEL record from a DBUS log object
    Change-Id: Ifff206aea19f16606451441ddfbb91d83c34e841
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- selutility.cpp
+++ selutility.cpp
@@ -40,6 +40,73 @@ uint32_t getEntryTimeStamp(const std::string& service,
     return static_cast<uint32_t>(sdbusplus::message::variant_ns::get<uint64_t>(timestamp)/1000);
 }
 
+extern const ipmi::sensor::InvObjectIDMap invSensors;
+
+GetSELEntryResponse prepareSELEntry(
+        const std::string& objPath,
+        ipmi::sensor::InvObjectIDMap::const_iterator iter)
+{
+    struct ipmi::sel::GetSELEntryResponse record {};
+    using namespace std::string_literals;
+
+    sdbusplus::bus::bus bus{ipmid_get_sd_bus_connection()};
+    static const auto intf = "org.freedesktop.DBus.Properties"s;
+
+    std::string service;
+
+    service = ipmi::getService(bus, logEntryIface, objPath);
+
+    auto methodCall = bus.new_method_call(service.c_str(),
+                                         objPath.c_str(),
+                                         intf.c_str(),
+                                         "Get");
+
+    static const auto idProperty = "Id"s;
+
+    methodCall.append(logEntryIface);
+    methodCall.append(idProperty.c_str());
+
+    auto reply = bus.call(methodCall);
+    if (reply.is_method_error())
+    {
+        throw std::runtime_error("ERROR in reading entry interface for ID");
+    }
+
+    sdbusplus::message::variant<uint32_t> logID;
+    reply.read(logID);
+
+    methodCall = bus.new_method_call(service.c_str(),
+                                     objPath.c_str(),
+                                     intf.c_str(),
+                                     "Get");
+
+    static const auto timeProperty = "Timestamp"s;
+
+    methodCall.append(logEntryIface);
+    methodCall.append(timeProperty.c_str());
+
+    reply = bus.call(methodCall);
+    if (reply.is_method_error())
+    {
+        throw std::runtime_error("ERROR in reading entry interface for time");
+    }
+
+    sdbusplus::message::variant<uint64_t> timestamp;
+    reply.read(timestamp);
+
+    record.recordID = static_cast<uint16_t>(sdbusplus::message::variant_ns::get<uint32_t>(logID));
+    record.recordType = systemRecord;
+    record.timeStamp = static_cast<uint32_t>(sdbusplus::message::variant_ns::get<uint64_t>(timestamp)/1000);
+    record.generatorID = generator;
+    record.evmRev = evmRev;
+    record.sensorType = iter->second.sensorType;
+    record.sensorNum = iter->second.sensorID;
+    record.eventType = iter->second.eventReadingType;
+    record.eventData1 = iter->second.eventOffset;
+
+    return record;
+}
+
 } // namespace sel
 
 } // namespace ipmi
--- selutility.hpp
+++ selutility.hpp
@@ -18,6 +18,31 @@ static constexpr auto logBasePath = "/xyz/openbmc_project/logging/entry";
 static constexpr auto logEntryIface = "xyz.openbmc_project.Logging.Entry";
 static constexpr auto logDeleteIface = "xyz.openbmc_project.Object.Delete";
 
+static constexpr auto systemRecord = 0x02;
+static constexpr auto generator = 0x2000;
+static constexpr auto evmRev = 0x04;
+
+/** @struct GetSELEntryResponse
+ *
+ *  IPMI payload for Get SEL Entry command response.
+ */
+struct GetSELEntryResponse
+{
+    uint16_t nextRecordID;          //!< Next RecordID.
+    uint16_t recordID;              //!< Record ID.
+    uint8_t recordType;             //!< Record Type.
+    uint32_t timeStamp;             //!< Timestamp.
+    uint16_t generatorID;           //!< Generator ID.
+    uint8_t evmRev;                 //!< EvM Rev.
+    uint8_t sensorType;             //!< Sensor Type.
+    uint8_t sensorNum;              //!< Sensor Number.
+    uint8_t eventType;              //!< Event Dir | Event Type.
+    uint8_t eventData1;             //!< Event Data 1.
+    uint8_t eventData2;             //!< Event Data 2.
+    uint8_t eventData3;             //!< Event Data 3.
+} __attribute__((packed));
+
+
 uint32_t getEntryTimeStamp(const std::string& service,
                            const std::string& objPath);
 
