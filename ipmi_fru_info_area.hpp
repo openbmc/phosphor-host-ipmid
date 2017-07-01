b27ccbd9b4df7bbdaf925551271f4476a4edc099
Marri Devender Rao <devenrao@in.ibm.com>
2017-07-01 16:11:40 -0500

    construct IPMI FRU Info data based on inventory data
    Change-Id: If8ee75b48323d29b568ef807d381a3e5600527d5
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

---
+++ ipmi_fru_info_area.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+
+namespace ipmi
+{
+namespace fru
+{
+using FruAreaData = std::vector<uint8_t>;
+using Section = std::string;
+using Value = std::string;
+using Property = std::string;
+using PropertyMap = std::map<Property, Value>;
+using FruInventoryData = std::map<Section, PropertyMap>;
+
+/**
+ * @brief Builds Fru area data from inventory data
+ *
+ * @param[in] invData FRU properties values read from inventory
+ *
+ * @return FruAreaData FRU area data as per IPMI specification
+ */
+FruAreaData buildFruAreaData(const FruInventoryData& inventory);
+
+} //fru
+} //ipmi
+
