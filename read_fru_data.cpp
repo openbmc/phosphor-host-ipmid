af9c039ac9128f424e29984a6556ba5b33909556
Marri Devender Rao <devenrao@in.ibm.com>
2017-07-10 01:49:54 -0500

    cater for property change signal to invalidate fru data
    Change-Id: I90897959dab76181a6006c372c4b60cc1bdd0c0c
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- read_fru_data.cpp
+++ read_fru_data.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <phosphor-logging/elog-errors.hpp>
+#include <iostream>
 #include "xyz/openbmc_project/Common/error.hpp"
 #include "read_fru_data.hpp"
 #include "fruread.hpp"
@@ -12,7 +13,9 @@ extern phosphor::hostipmi::FrusAreaMap gfrusMap;
 using namespace phosphor::logging;
 using InternalFailure =
         sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+using namespace sdbusplus::bus::match::rules;
 
+bool g_registerPropChgCallback = false;
 namespace phosphor
 {
 namespace hostipmi
@@ -57,6 +60,47 @@ std::string readProperty(
     return value;
 }
 
+int processFruPropChange(sd_bus_message* m, void* userdata, sd_bus_error* ret)
+{
+    std::cout  << "processFruPropChange " << std::endl;
+    return 1;
+}
+
+//Rule for property change
+std::string propChangeMatch(const std::string& spath)
+{
+    return std::string(
+        type::signal() +
+        interface(PROP_INTF) +
+        member("PropertiesChanged") +
+        path(spath));
+}
+
+int registerFruPropertyChangeHandlers()
+{
+    int r(0);
+    if (!g_registerPropChgCallback)
+    {
+        g_registerPropChgCallback = true;
+        int r(0);
+        for (auto& fru : frus)
+        {
+            auto& instanceList = fru.second;
+            for (auto& instance : instanceList)
+            {
+                auto bus = ipmid_get_sd_bus_connection();
+                std::string propchg = propChangeMatch(OBJ_PATH+instance.first);
+                std::cout  << "Property change rule " << propchg << std::endl;
+                r = sd_bus_add_match(bus, NULL, propchg.c_str(), processFruPropChange, NULL);
+                if (r < 0)
+                {
+                    log<level::ERR>("Failure in adding property change listener");
+                }
+            }
+        }
+    }
+    return r;
+}
 /**
  * @brief Read the property value from Inventory
  *
@@ -109,6 +153,8 @@ FruAreaData getFruAreaData(sdbusplus::bus::bus& bus, const uint8_t& fruNum)
     FruAreaData data = buildFruAreaData(invData);
     gfrusMap.insert(std::pair<uint8_t, FruAreaData>(fruNum, data));
 
+    //Register property change callback handlers, if not already registered.
+    registerFruPropertyChangeHandlers();
     return data;
 }
 } //hostipmi
