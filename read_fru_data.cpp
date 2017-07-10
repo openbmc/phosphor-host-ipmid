6bee15fd42543fe583746a0685d3b5b3e0ec7cbb
Marri Devender Rao <devenrao@in.ibm.com>
2017-07-10 01:49:54 -0500

    cater for property change signal to invalidate fru data
    Resolves openbmc/openbmc#1560
    
    Change-Id: I90897959dab76181a6006c372c4b60cc1bdd0c0c
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- read_fru_data.cpp
+++ read_fru_data.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <map>
 #include <phosphor-logging/elog-errors.hpp>
 #include "xyz/openbmc_project/Common/error.hpp"
@@ -8,7 +7,6 @@
 #include "utils.hpp"
 
 extern const FruMap frus;
-
 namespace ipmi
 {
 namespace fru
@@ -16,6 +14,7 @@ namespace fru
 using namespace phosphor::logging;
 using InternalFailure =
         sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+std::unique_ptr<sdbusplus::bus::match_t> matchPtr(nullptr);
 
 static constexpr auto INV_INTF  = "xyz.openbmc_project.Inventory.Manager";
 static constexpr auto OBJ_PATH  = "/xyz/openbmc_project/inventory";
@@ -65,6 +64,58 @@ std::string readProperty(const std::string& intf,
     return value;
 }
 
+void processFruPropChange(sdbusplus::message::message& msg)
+{
+    if(cache::frusMap.empty())
+    {
+        return;
+    }
+    std::string path = msg.get_path();
+    //trim the object base path
+    std::size_t found = path.find(OBJ_PATH);
+    if (found != std::string::npos)
+    {
+        path.erase(found, strlen(OBJ_PATH));
+    }
+    for (auto& fru : frus)
+    {
+        bool found = false;
+        auto& fruId = fru.first;
+        auto& instanceList = fru.second;
+        for (auto& instance : instanceList)
+        {
+            if(instance.first == path)
+            {
+                found = true;
+                break;
+            }
+        }
+        if (found)
+        {
+            cache::frusMap.erase(fruId);
+            break;
+        }
+    }
+}
+
+//register for fru property change
+int registerCallbackHandler()
+{
+    if(matchPtr == nullptr)
+    {
+        using namespace sdbusplus::bus::match::rules;
+        sdbusplus::bus::bus bus{ipmid_get_sd_bus_connection()};
+        matchPtr = std::make_unique<sdbusplus::bus::match_t>(
+            bus,
+            path_namespace(OBJ_PATH) +
+            type::signal() +
+            member("PropertiesChanged") +
+            interface(PROP_INTF),
+            std::bind(processFruPropChange, std::placeholders::_1));
+    }
+    return 0;
+}
+
 /**
  * @brief Read FRU property values from Inventory
  *
@@ -97,7 +148,7 @@ FruInventoryData readDataFromInventory(const FRUId& fruNum)
             }
         }
     }
-    return std::move(data);
+    return data;
 }
 
 const FruAreaData& getFruAreaData(const FRUId& fruNum)
