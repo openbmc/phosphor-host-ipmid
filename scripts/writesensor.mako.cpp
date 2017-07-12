325392256a5375d008aa97a2eab6f834dd27dba6
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2017-07-12 06:35:20 -0500

    IPMI:OCC: Update the IPMI host code to update different types of services.
    Change-Id: Id76446061fd0fa6dc3dead702538e424293af7ce
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- scripts/writesensor.mako.cpp
+++ scripts/writesensor.mako.cpp
@@ -19,9 +19,13 @@ extern const IdInfoMap sensors = {
        multiplier = sensor.get("multiplierM", 1)
        offset = sensor.get("offsetB", 0)
        exp = sensor.get("bExp", 0)
+       updatePath = sensor["updatePath"]
+       updateInterface = sensor["updateInterface"]
+       updateCommand = sensor["updateCommand"]
 %>
         ${sensorType},"${path}",${readingType},${multiplier},${offset},${exp},
-        ${offset * pow(10,exp)},{
+        ${offset * pow(10,exp)},"${updatePath}","${updateInterface}",
+        "${updateCommand}",{
     % for interface,properties in interfaces.iteritems():
             {"${interface}",{
             % for dbus_property,property_value in properties.iteritems():
