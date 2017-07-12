6d785caad498e9a41ec477916d3c2d26812fb769
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-07-12 16:13:49 +0530

    Use Host Command Manager in host interface implementation
    Change-Id: Icefce510a3a0022bf0288fa99518459b732a2e04
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ ipmid-host-cmd.hpp
@@ -0,0 +1,10 @@
+#include <memory>
+#include <sdbusplus/bus.hpp>
+#include <host-cmd-utils.hpp>
+
+// Need this to use new sdbusplus compatible interfaces
+using sdbusPtr =  std::unique_ptr<sdbusplus::bus::bus>;
+extern sdbusPtr& ipmid_get_sdbus_plus_handler();
+
+// Global Host Bound Command manager
+extern void ipmid_push_cmd_to_host(phosphor::host::command::CommandHandler);
