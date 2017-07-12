d65b4e85462c0e9cbbd40f54a497b3b6f0f9438d
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2017-07-12 06:35:20 -0500

    IPMI: Virtual sensor support in host ipmid
    Resolves openbmc/openbmc#1608
    
    Change-Id: Id76446061fd0fa6dc3dead702538e424293af7ce
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- types.hpp
+++ types.hpp
@@ -6,6 +6,7 @@
 #include <string>
 
 #include <sdbusplus/server.hpp>
+#include "sensorhandler.h"
 
 namespace ipmi
 {
@@ -21,6 +22,19 @@ using ObjectTree = std::map<DbusObjectPath,
                             std::map<DbusService, std::vector<DbusInterface>>>;
 namespace sensor
 {
+
+/**
+ * @enum ValueReadingType
+ *
+ * IPMI data types in request
+ */
+enum ValueReadingType{
+   IPMI_TYPE_ASSERTION,
+   IPMI_TYPE_READING,
+   IPMI_TYPE_EVENT1,
+   IPMI_TYPE_EVENT2,
+   IPMI_TYPE_EVENT3,
+};
 
 using Offset = uint8_t;
 using Value = ipmi::Value;
@@ -47,6 +61,8 @@ using OffsetB = uint16_t;
 using Exponent = uint8_t;
 using ScaledOffset = int64_t;
 
+using UpdateInterface = std::string;
+
 struct Info
 {
    Type sensorType;
@@ -56,6 +72,9 @@ struct Info
    OffsetB coefficientB;
    Exponent exponentB;
    ScaledOffset scaledOffset;
+   ValueReadingType valueReadingType;
+   std::function<uint8_t(SetSensorReadingReq*,Info)> updateFunc;
+   std::function<uint8_t(SetSensorReadingReq*)> getSensorValue;
    DbusInterfaceMap sensorInterfaces;
 };
 
