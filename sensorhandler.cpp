dd919889733d5f953cdec5c9fcc55e264880bb99
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2017-07-12 06:35:20 -0500

    Support set sensor reading command for occ active
    Change-Id: Id76446061fd0fa6dc3dead702538e424293af7ce
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- sensorhandler.cpp
+++ sensorhandler.cpp
@@ -410,17 +410,6 @@ ipmi_ret_t setSensorReading(void *request)
 {
     auto cmdData = static_cast<SetSensorReadingReq *>(request);
 
-    auto assertionStates =
-            (static_cast<uint16_t>(cmdData->assertOffset8_14)) << 8 |
-            cmdData->assertOffset0_7;
-
-    auto deassertionStates =
-            (static_cast<uint16_t>(cmdData->deassertOffset8_14)) << 8 |
-            cmdData->deassertOffset0_7;
-
-    std::bitset<16> assertionSet(assertionStates);
-    std::bitset<16> deassertionSet(deassertionStates);
-
     // Check if the Sensor Number is present
     auto iter = sensors.find(cmdData->number);
     if (iter == sensors.end())
@@ -428,73 +417,8 @@ ipmi_ret_t setSensorReading(void *request)
         return IPMI_CC_SENSOR_INVALID;
     }
 
-    auto& interfaceList = iter->second.sensorInterfaces;
-    if (interfaceList.empty())
-    {
-        log<level::ERR>("Interface List empty for the sensor",
-                entry("Sensor Number = %d", cmdData->number));
-        return IPMI_CC_UNSPECIFIED_ERROR;
-    }
-
-    ipmi::sensor::ObjectMap objects;
-    ipmi::sensor::InterfaceMap interfaces;
-    for (const auto& interface : interfaceList)
-    {
-        for (const auto& property : interface.second)
-        {
-            ipmi::sensor::PropertyMap props;
-            bool valid = false;
-            for (const auto& value : property.second)
-            {
-                if (assertionSet.test(value.first))
-                {
-                    props.emplace(property.first, value.second.assert);
-                    valid = true;
-                }
-                else if (deassertionSet.test(value.first))
-                {
-                    props.emplace(property.first, value.second.deassert);
-                    valid = true;
-                }
-            }
-            if (valid)
-            {
-                interfaces.emplace(interface.first, std::move(props));
-            }
-        }
-    }
-    objects.emplace(iter->second.sensorPath, std::move(interfaces));
-
-    sdbusplus::bus::bus bus{ipmid_get_sd_bus_connection()};
-    using namespace std::string_literals;
-    static const auto intf = "xyz.openbmc_project.Inventory.Manager"s;
-    static const auto path = "/xyz/openbmc_project/inventory"s;
-    std::string service;
-
-    try
-    {
-        service = ipmi::getService(bus, intf, path);
-
-        // Update the inventory manager
-        auto pimMsg = bus.new_method_call(service.c_str(),
-                                          path.c_str(),
-                                          intf.c_str(),
-                                          "Notify");
-        pimMsg.append(std::move(objects));
-        auto inventoryMgrResponseMsg = bus.call(pimMsg);
-        if (inventoryMgrResponseMsg.is_method_error())
-        {
-            log<level::ERR>("Error in notify call");
-            return IPMI_CC_UNSPECIFIED_ERROR;
-        }
-    }
-    catch (const std::runtime_error& e)
-    {
-        log<level::ERR>(e.what());
-        return IPMI_CC_UNSPECIFIED_ERROR;
-    }
-
-    return IPMI_CC_OK;
+    auto& sensorInf = iter->second;
+    return iter->second.updateFunc(cmdData,sensorInf);
 }
 
 ipmi_ret_t ipmi_sen_set_sensor(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
