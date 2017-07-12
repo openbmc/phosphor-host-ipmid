3e2db1661247436de3fade6dd7d20c9598deb4df
Tom Joseph <tomjoseph@in.ibm.com>
2017-07-12 20:07:11 +0530

    Implement the Get Asset Tag command
    Change-Id: Ic2fb1e707174ece63fbb758bf1422c2c4d95723b
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- dcmihandler.h
+++ dcmihandler.h
@@ -10,6 +10,7 @@ enum ipmi_netfn_sen_cmds
 {
     // Get capability bits
     IPMI_CMD_DCMI_GET_POWER = 0x03,
+    IPMI_CMD_DCMI_GET_ASSET_TAG = 0x06,
 };
 
 namespace dcmi
@@ -27,6 +28,33 @@ static constexpr auto assetTagProp = "AssetTag";
 
 using ObjectTree = std::map<std::string, std::map<std::string,
                             std::vector<std::string>>>;
+
+static constexpr auto groupExtId = 0xDC;
+
+static constexpr auto assetTagMaxOffset = 62;
+static constexpr auto assetTagMaxSize = 63;
+static constexpr auto maxBytes = 16;
+
+/** @struct GetAssetTagRequest
+ *
+ *  DCMI payload for Get Asset Tag command request.
+ */
+struct GetAssetTagRequest
+{
+    uint8_t groupID;            //!< Group extension identification.
+    uint8_t offset;             //!< Offset to read.
+    uint8_t bytes;              //!< Number of bytes to read.
+} __attribute__((packed));
+
+/** @struct GetAssetTagResponse
+ *
+ *  DCMI payload for Get Asset Tag command response.
+ */
+struct GetAssetTagResponse
+{
+    uint8_t groupID;            //!< Group extension identification.
+    uint8_t tagLength;          //!< Total asset tag length.
+} __attribute__((packed));
 
 /** @brief Read the asset tag of the server
  *
