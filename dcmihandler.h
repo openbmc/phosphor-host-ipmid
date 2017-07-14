ce3abdc52eb601aba4164602f9e931936594bee8
Tom Joseph <tomjoseph@in.ibm.com>
2017-07-15 00:55:23 +0530

    Add API to write asset tag to the DBUS object
    Change-Id: Ia4bf3f99a865617535c65a023c8e697792cd43b4
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- dcmihandler.h
+++ dcmihandler.h
@@ -64,6 +64,12 @@ struct GetAssetTagResponse
  */
 std::string readAssetTag();
 
+/** @brief Write the asset tag to the asset tag DBUS property
+ *
+ *  @param[in] assetTag - Asset Tag to be written to the property.
+ */
+void writeAssetTag(const std::string& assetTag);
+
 } // namespace dcmi
 
 #endif
