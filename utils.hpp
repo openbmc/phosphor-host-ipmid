b9b0d491270ec20b4489fea307acca4a75ad2ba7
Ratan Gupta <ratagupt@in.ibm.com>
2017-07-25 21:52:10 +0530

    Move dbus utility function to utils
    Change-Id: Ief3d9ab2311a597670767cf9931913e5afef4fee
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- utils.hpp
+++ utils.hpp
@@ -1,9 +1,26 @@
 #pragma once
 
+#include "types.hpp"
 #include <sdbusplus/server.hpp>
 
 namespace ipmi
 {
+
+constexpr auto MAPPER_BUS_NAME = "xyz.openbmc_project.ObjectMapper";
+constexpr auto MAPPER_OBJ = "/xyz/openbmc_project/object_mapper";
+constexpr auto MAPPER_INTF = "xyz.openbmc_project.ObjectMapper";
+
+constexpr auto SETTINGS_ROOT = "/";
+constexpr auto SETTINGS_MATCH = "host0";
+constexpr auto PROP_INTF = "org.freedesktop.DBus.Properties";
+
+constexpr auto IP_INTERFACE = "xyz.openbmc_project.Network.IP";
+constexpr auto MAC_INTERFACE = "xyz.openbmc_project.Network.MACAddress";
+
+constexpr auto METHOD_GET = "Get";
+constexpr auto METHOD_GET_ALL = "GetAll";
+constexpr auto METHOD_SET = "Set";
+
 
 /**
  * @brief Get the DBUS Service name for the input dbus path
@@ -16,6 +33,53 @@ namespace ipmi
 std::string getService(sdbusplus::bus::bus& bus,
                        const std::string& intf,
                        const std::string& path);
+/** @brief Gets the dbus object info implementing the given interface
+ *         from the given subtree.
+ *  @param[in] interface - Dbus interface.
+ *  @param[in] serviceRoot - subtree from where the search should start.
+ *  @param[in] match - identifier for object.
+ *  @return On success returns the object having objectpath and servicename.
+ */
+ipmi::DbusObjectInfo getDbusObject(const std::string& interface,
+                                   const std::string& serviceRoot = SETTINGS_ROOT,
+                                   const std::string& match = "");
+
+/** @brief Gets the value associated with the given object
+ *         and the interface.
+ *  @param[in] service - Dbus service name.
+ *  @param[in] objPath - Dbus object path.
+ *  @param[in] interface - Dbus interface.
+ *  @param[in] property - name of the property.
+ *  @return On success returns the value of the property.
+ */
+std::string getDbusProperty(const std::string& service,
+                            const std::string& objPath,
+                            const std::string& interface,
+                            const std::string& property);
+
+/** @brief Gets all the properties associated with the given object
+ *         and the interface.
+ *  @param[in] service - Dbus service name.
+ *  @param[in] objPath - Dbus object path.
+ *  @param[in] interface - Dbus interface.
+ *  @return On success returns the map of name value pair.
+ */
+ipmi::PropertyMap getAllDbusProperties(const std::string& service,
+                                       const std::string& objPath,
+                                       const std::string& interface);
+/** @brief Sets the property value of the given object.
+ *  @param[in] service - Dbus service name.
+ *  @param[in] objPath - Dbus object path.
+ *  @param[in] interface - Dbus interface.
+ *  @param[in] property - name of the property.
+ *  @param[in] value - value which needs to be set.
+ */
+void setDbusProperty(const std::string& service,
+                     const std::string& objPath,
+                     const std::string& interface,
+                     const std::string& property,
+                     const ipmi::Value& value);
+
 } // namespace ipmi
 
 
