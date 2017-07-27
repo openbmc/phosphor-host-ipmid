b92ca1f9e1e3ea5374a55a66dee1569de13943b9
Ratan Gupta <ratagupt@in.ibm.com>
2017-07-27 07:07:48 +0530

    Move LAN handling commands to the new xyz interfaces
    Change-Id: I4631c79c171883e5f0d531cfb5957a09496e4b3a
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- types.hpp
+++ types.hpp
@@ -21,6 +21,7 @@ using Value = sdbusplus::message::variant<bool, uint8_t, int16_t,
                                           int64_t, uint64_t, std::string>;
 
 using PropertyMap = std::map<DbusProperty, Value>;
+
 using ObjectTree = std::map<DbusObjectPath,
                             std::map<DbusService, std::vector<DbusInterface>>>;
 namespace sensor
