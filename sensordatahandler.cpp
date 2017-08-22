2dd3141f892953a2f69b5402effbc49f81495a31
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2017-08-22 07:40:27 -0500

    IPMI changes to skip updating non present cores
    Cores which are not present need not to be updated
    to inventory. This change is for skipping several
    updates to inventory based on skipOn value.
    
    Change-Id: I29e005a715ccae1df6eeaf35561a20896ecde0ac
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- sensordatahandler.cpp
+++ sensordatahandler.cpp
@@ -321,11 +321,21 @@ ipmi_ret_t assertion(const SetSensorReadingReq& cmdData,
             {
                 if (assertionSet.test(value.first))
                 {
+                    //Skip update if skipOn is ASSERT
+                    if (SkipAssertion::ASSERT == value.second.skip)
+                    {
+                        return IPMI_CC_OK;
+                    }
                     props.emplace(property.first, value.second.assert);
                     valid = true;
                 }
                 else if (deassertionSet.test(value.first))
                 {
+                    //Skip update if skipOn is DEASSERT
+                    if (SkipAssertion::DEASSERT == value.second.skip)
+                    {
+                        return IPMI_CC_OK;
+                    }
                     props.emplace(property.first, value.second.deassert);
                     valid = true;
                 }
@@ -336,6 +346,7 @@ ipmi_ret_t assertion(const SetSensorReadingReq& cmdData,
             }
         }
     }
+
     objects.emplace(sensorInfo.sensorPath, std::move(interfaces));
     msg.append(std::move(objects));
     return updateToDbus(msg);
