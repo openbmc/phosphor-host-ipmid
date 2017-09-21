1488a35b38b39851078802e96ecdf60783d3266c
Nagaraju Goruganti <ngorugan@in.ibm.com>
2017-09-21 07:44:17 -0500

    Don't set/get zeroconfig address if other IP address is present
    Resolves openbmc/openbmc#2265
    
    Change-Id: I65b54358f13231c78b3c6488a59b94bc9323484c
    Signed-off-by: Nagaraju Goruganti <ngorugan@in.ibm.com>

--- utils.hpp
+++ utils.hpp
@@ -45,6 +45,18 @@ DbusObjectInfo getDbusObject(sdbusplus::bus::bus& bus,
                              const std::string& subtreePath = ROOT,
                              const std::string& match = {});
 
+/** @brief Gets the ipAddres of first dbus IP object of Non-LinkLocalIPAddress
+ *         type from the given subtree, if not avalable gets IP object of
+ *         LinkLocalIPAddress type.
+ *  @param[in] bus - DBUS Bus Object.
+ *  @param[in] subtreePath - subtree from where the search should start.
+ *  @param[in] match - identifier for object.
+ *  @return On success returns the ipAddress.
+ */
+std::string getIPAddress(sdbusplus::bus::bus& bus,
+                         const std::string& subtreePath = ROOT,
+                         const std::string& match = "ipv4");
+
 /** @brief Gets the value associated with the given object
  *         and the interface.
  *  @param[in] bus - DBUS Bus Object.
@@ -197,6 +209,11 @@ void createVLAN(sdbusplus::bus::bus& bus,
  *  @param[in] path - Dbus object path.
  */
 uint32_t getVLAN(const std::string& path);
+
+/** @brief checks if the given ip is Link Local Ip or not.
+ *  @param[in] ipaddress - IPAddress.
+ */
+bool isLinkLocalIP(const std::string& ipaddress);
 
 } //namespace network
 } // namespace ipmi
