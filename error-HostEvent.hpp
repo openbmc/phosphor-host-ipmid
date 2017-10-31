5283aef29acc4b522c01bea9e6e86ce61a9a36b3
Tom Joseph <tomjoseph@in.ibm.com>
2017-10-31 17:25:33 +0530

    Commit a MaintenanceProcedure log entry on a 0xDE SEL record
    In the case of a procedure callout, HB sends a eSEL of 0xDF type.
    It is followed by a Add SEL record with OEM record type 0xDE
    and byte 11 in the record indicate the procedure associated
    with the eSEL.
    
    Resolves openbmc/openbmc#2368
    
    Change-Id: Ia57f423c9d533cd8968b613d7522b409a9820198
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- error-HostEvent.hpp
+++ error-HostEvent.hpp
@@ -26,6 +26,19 @@ struct Event final : public sdbusplus::exception_t
     const char* what() const noexcept override;
 };
 
+struct MaintenanceProcedure final : public sdbusplus::exception_t
+{
+    static constexpr auto errName = "org.open_power.Host.Error.MaintenanceProcedure";
+    static constexpr auto errDesc =
+            "A host system event with a procedure callout";
+    static constexpr auto errWhat =
+            "org.open_power.Host.Error.MaintenanceProcedure: A host system event with a procedure callout";
+
+    const char* name() const noexcept override;
+    const char* description() const noexcept override;
+    const char* what() const noexcept override;
+};
+
 } // namespace Error
 } // namespace Host
 } // namespace open_power
