55eafdc302fceb2c3cdf85e506ae6e6bee6e0b75
Patrick Venture <venture@google.com>
2017-11-09 14:56:45 -0800

    watchdog: implement Get watchdog command
    The basic get watchdog command.  Implemented using sdbusplus, however,
    leaving the previous code using the sd-bus library.
    
    The set watchdog command is also updated in this commit such that
    the information sent to set is stored and retrieved for get.
    
    Change-Id: If192ebbb21062e1dbe5cf90c66ff957aa26192e1
    Signed-off-by: Patrick Venture <venture@google.com>

--- app/watchdog.hpp
+++ app/watchdog.hpp
@@ -19,6 +19,25 @@ ipmi_ret_t ipmi_app_set_watchdog(
         ipmi_data_len_t data_len,
         ipmi_context_t context);
 
+/** @brief The GET watchdog IPMI command.
+ *
+ *  @param[in] netfn
+ *  @param[in] cmd
+ *  @param[in] request
+ *  @param[in,out] response
+ *  @param[out] data_len
+ *  @param[in] context
+ *
+ *  @return IPMI_CC_OK on success, -1 otherwise.
+ */
+ipmi_ret_t ipmi_app_get_watchdog(
+        ipmi_netfn_t netfn,
+        ipmi_cmd_t cmd,
+        ipmi_request_t request,
+        ipmi_response_t response,
+        ipmi_data_len_t data_len,
+        ipmi_context_t context);
+
 /** @brief The RESET watchdog IPMI command.
  *
  *  @param[in] netfn
@@ -37,4 +56,3 @@ ipmi_ret_t ipmi_app_reset_watchdog(
         ipmi_response_t response,
         ipmi_data_len_t data_len,
         ipmi_context_t context);
-
