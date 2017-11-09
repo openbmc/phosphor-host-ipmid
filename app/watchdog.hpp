894571d9a2a7b0ab1504c6f66e5e0b1a72a5d702
Patrick Venture <venture@google.com>
2017-11-09 14:46:54 -0800

    watchdog: cleanup style and similar fixes
    Some style cleanup.
    
    Change-Id: I4f8ce595294797cc015a55cc6c9477aee7b6938a
    Signed-off-by: Patrick Venture <venture@google.com>

--- app/watchdog.hpp
+++ app/watchdog.hpp
@@ -11,9 +11,13 @@
  *
  *  @return IPMI_CC_OK on success, -1 otherwise.
  */
-ipmi_ret_t ipmi_app_set_watchdog(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
-    ipmi_request_t request, ipmi_response_t response,
-    ipmi_data_len_t data_len, ipmi_context_t context);
+ipmi_ret_t ipmi_app_set_watchdog(
+        ipmi_netfn_t netfn,
+        ipmi_cmd_t cmd,
+        ipmi_request_t request,
+        ipmi_response_t response,
+        ipmi_data_len_t data_len,
+        ipmi_context_t context);
 
 /** @brief The RESET watchdog IPMI command.
  *
@@ -26,7 +30,11 @@ ipmi_ret_t ipmi_app_set_watchdog(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
  *
  *  @return IPMI_CC_OK on success, -1 otherwise.
  */
-ipmi_ret_t ipmi_app_reset_watchdog(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
-    ipmi_request_t request, ipmi_response_t response,
-    ipmi_data_len_t data_len, ipmi_context_t context);
+ipmi_ret_t ipmi_app_reset_watchdog(
+        ipmi_netfn_t netfn,
+        ipmi_cmd_t cmd,
+        ipmi_request_t request,
+        ipmi_response_t response,
+        ipmi_data_len_t data_len,
+        ipmi_context_t context);
 
