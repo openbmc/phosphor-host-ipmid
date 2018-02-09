c54382a5f0cb223af37ccf675291ff429361aff0
William A. Kennington III <wak@google.com>
2018-02-09 15:28:46 -0800

    watchdog: Implements the Watchdog Get Command
    Change-Id: I2ba9fd0eeba4e828cafd1bcf6fe30e2322eaf99e
    Signed-off-by: William A. Kennington III <wak@google.com>

--- app/watchdog.hpp
+++ app/watchdog.hpp
@@ -39,3 +39,21 @@ ipmi_ret_t ipmi_app_watchdog_set(
         ipmi_response_t response,
         ipmi_data_len_t data_len,
         ipmi_context_t context);
+
+/** @brief The GET watchdog IPMI command.
+ *  @param[in] netfn
+ *  @param[in] cmd
+ *  @param[in] request
+ *  @param[in,out] response
+ *  @param[out] data_len
+ *  @param[in] context
+ *
+ *  @return IPMI_CC_OK on success, an IPMI error code otherwise.
+ */
+ipmi_ret_t ipmi_app_watchdog_get(
+        ipmi_netfn_t netfn,
+        ipmi_cmd_t cmd,
+        ipmi_request_t request,
+        ipmi_response_t response,
+        ipmi_data_len_t data_len,
+        ipmi_context_t context);
