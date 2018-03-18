3f137e2ed6112aae069e4dc5edf4e54628dbf3cb
Ratan Gupta <ratagupt@in.ibm.com>
2018-03-18 23:13:39 +0530

    SetLan: Fix the IP address if partial data given
    Presently when partial lan data is given the code
    was not getting the correct IP object,it was getting
    the linklocal address.
    
    This defect was introduced due to this commit c7c1c3c.
    This commit fixes this problem.
    
    Change-Id: I8f6d915cd3e1757ba28394cd93f16396e087d4f7
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- transporthandler.cpp
+++ transporthandler.cpp
@@ -699,7 +699,7 @@ void applyChanges(int channel)
 
                 // if the system is having ip object,then
                 // get the IP object.
-                ipObject = ipmi::getDbusObject(bus,
+                ipObject = ipmi::getIPObject(bus,
                                                ipmi::network::IP_INTERFACE,
                                                ipmi::network::ROOT,
                                                ethIp);
