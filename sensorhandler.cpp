0b413b5436568baeb94544ae84732b6b864ba77b
Jayanth Othayoth <ojayanth@in.ibm.com>
2018-04-02 07:59:34 -0500

    sensorhandler: Enable mutability based set function
    Resolves openbmc/openbmc#2980
    
    Change-Id: I67afd4e84ec96e5cfc2dd315604d70f41c67a438
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- sensorhandler.cpp
+++ sensorhandler.cpp
@@ -15,6 +15,7 @@
 #include "types.hpp"
 #include "utils.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
+#include <iostream>
 
 static constexpr uint8_t fruInventoryDevice = 0x10;
 static constexpr uint8_t IPMIFruInventory = 0x02;
@@ -372,6 +373,13 @@ ipmi_ret_t setSensorReading(void *request)
 
     try
     {
+        if (ipmi::sensor::Mutability::Write !=
+              (iter->second.mutability & ipmi::sensor::Mutability::Write))
+        {
+            log<level::ERR>("Sensor Set operation is not allowed",
+                            entry("SENSOR_NUM=%d", cmdData.number));
+            return IPMI_CC_ILLEGAL_COMMAND;
+        }
         return iter->second.updateFunc(cmdData, iter->second);
     }
     catch (InternalFailure& e)
