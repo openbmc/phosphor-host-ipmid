80d03c33bf5ca561d2d174c9bc3444edb9c2622b
Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
2018-04-19 05:38:37 +0530

    Squashed commit - IPMI Password management lib
    commit 2bf9747659fc99abc58878848077c1826c22f592
    Author: AppaRao Puli <apparao.puli@intel.com>
    Date:   Thu May 17 10:28:48 2018 +0530
    
        Handling delete password entry from ipmi-pass
    
        API to Handle the delete password entry from
        ipmi-pass encrypted file when user gets deleted
        by any interface
    
        Change-Id: I692a81b166b53d6fc981fdb85ce5d6980887560b
        Signed-off-by: AppaRao Puli <apparao.puli@intel.com>
        Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
    
    commit 4d4d6e67d1908ab594405e75856758386e9a0e29
    Author: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
    Date:   Thu Apr 19 05:38:37 2018 +0530
    
        Add first user layer support.
    
        Exposes user layer library which will be used by both
        apphandler and netipmid for RAKP. Password is decrypted
        from the special file which is used by pam-ipmi.
    
        Change-Id: Ia6b99d4359a03843f19cd0e18f7269ad2e3cdb44
        Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
    
    Change-Id: Ifd77fca75bc4f6f7b057be6f1ad8791212130f5b
    Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>

---
+++ user/passwd_mgr.hpp
@@ -0,0 +1,102 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#pragma once
+#include <openssl/evp.h>
+
+#include <ctime>
+#include <unordered_map>
+#include <vector>
+
+namespace ipmi
+{
+
+class PasswdMgr
+{
+  public:
+    ~PasswdMgr() = default;
+    PasswdMgr(const PasswdMgr&) = delete;
+    PasswdMgr& operator=(const PasswdMgr&) = delete;
+    PasswdMgr(PasswdMgr&&) = delete;
+    PasswdMgr& operator=(PasswdMgr&&) = delete;
+
+    /** @brief Constructs user password list
+     *
+     */
+    PasswdMgr();
+
+    /** @brief Get password for the user
+     *
+     *  @param[in] userName - user name
+     */
+    std::string getPasswdByUserName(const std::string& userName);
+
+    /** @brief Clear username and password entry for the specified user
+     *
+     *  @param[in] userName - username
+     */
+    int clearUserEntry(const std::string& userName);
+
+  private:
+    std::unordered_map<std::string, std::string> passwdMapList;
+    std::time_t fileLastUpdatedTime;
+    /** @brief check timestamp and reload password map if required
+     *
+     */
+    void checkAndReload(void);
+    /** @brief initializes passwdMapList by reading the encrypted file
+     *
+     */
+    void initPasswordMap(void);
+    /** @brief Function to read the password file data
+     *
+     *  @param[out] outBytes - vector to hold decrypted password file data
+     *
+     * @return error response
+     */
+    int readPasswdFileData(std::vector<uint8_t>& outBytes);
+    /** @brief  Updates special password file by clearing the password entry
+     *  for the user specified.
+     *
+     *  @param[in] userName - user name entry that has to be removed.
+     *
+     * @return error response
+     */
+    int updatePasswdSpecialFile(const std::string& userName);
+    /** @brief encrypts or decrypt the data provided
+     *
+     *  @param[in] isEncrypt - do encrypt if set to 1, else do decrypt.
+     *  @param[in] cipher - cipher to be used
+     *  @param[in] key - pointer to the key
+     *  @param[in] keyLen - Length of the key to be used
+     *  @param[in] iv - pointer to initialization vector
+     *  @param[in] ivLen - Length of the iv
+     *  @param[in] inBytesLen - input data to be encrypted / decrypted
+     *  @param[in] inBytesLen - input size to be encrypted / decrypted
+     *  @param[in] mac - message authentication code - to figure out corruption
+     *  @param[in] macLen - size of MAC
+     *  @param[in] outBytes - ptr to store output bytes
+     *  @param[in] outBytesLen - outbut data length.
+     *
+     * @return error response
+     */
+    int encryptDecryptData(uint8_t isEncrypt, const EVP_CIPHER* cipher,
+                           uint8_t* key, size_t keyLen, uint8_t* iv,
+                           size_t ivLen, uint8_t* inBytes, size_t inBytesLen,
+                           uint8_t* mac, size_t* macLen, uint8_t* outBytes,
+                           size_t* outBytesLen);
+};
+
+} // namespace ipmi
---
+++ user/user_layer.cpp
@@ -0,0 +1,33 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "user_layer.hpp"
+
+#include "passwd_mgr.hpp"
+static ipmi::PasswdMgr passwdMgr;
+
+ipmi_ret_t ipmiUserGetPassword(const std::string& userName,
+                               std::string& password)
+{
+    password = passwdMgr.getPasswdByUserName(userName);
+    return IPMI_CC_OK;
+}
+
+ipmi_ret_t ipmiUserClearPassword(const std::string& userName)
+{
+    passwdMgr.clearUserEntry(userName);
+    return IPMI_CC_OK;
+}
