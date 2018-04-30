49030757d093ec5418d08fc01d8d689197bd17fe
Nagaraju Goruganti <ngorugan@in.ibm.com>
2018-04-29 22:38:40 -0500

    Fix Host time is not getting printed to journal
    log<level::DEBUG> is not putting Host time into journal, instead it is
    putting it in the metadata. Made fix for it.
    
    Change-Id: I0ba72a871469a30c605ee3ec91a07787959d5ae3
    Signed-off-by: Nagaraju Goruganti <ngorugan@in.ibm.com>

--- storagehandler.cpp
+++ storagehandler.cpp
@@ -433,6 +433,7 @@ ipmi_ret_t ipmi_storage_get_sel_time(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
     using namespace std::chrono;
     uint64_t host_time_usec = 0;
     uint32_t resp = 0;
+    std::stringstream hostTime;
 
     try
     {
@@ -469,8 +470,11 @@ ipmi_ret_t ipmi_storage_get_sel_time(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         return IPMI_CC_UNSPECIFIED_ERROR;
     }
 
-    log<level::DEBUG>("Host time:",
-                      entry("HOST_TIME=%s", getTimeString(host_time_usec)));
+    hostTime << "Host time:" << host_time_usec << " , " << getTimeString(
+                host_time_usec);
+    std::string s = hostTime.str();
+    log<level::DEBUG>(hostTime.str().c_str());
+
 
     // Time is really long int but IPMI wants just uint32. This works okay until
     // the number of seconds since 1970 overflows uint32 size.. Still a whole
