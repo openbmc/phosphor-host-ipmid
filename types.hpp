006d33c4a7e18abcba8cbbe9c24a10fb08c9b745
James Feist <james.feist@linux.intel.com>
2018-06-15 11:36:27 -0700

    Add double to ipmi::Value type
    ipmi::Value is used as the type in setDbusProperty. As
    properties are allowed to be doubles, add it to the type.
    
    Change-Id: I3d2dd5063100b8e73732740ce056fc476a7e1e44
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- types.hpp
+++ types.hpp
@@ -16,9 +16,9 @@ using DbusInterface = std::string;
 using DbusObjectInfo = std::pair<DbusObjectPath, DbusService>;
 using DbusProperty = std::string;
 
-using Value = sdbusplus::message::variant<bool, uint8_t, int16_t,
-                                          uint16_t, int32_t, uint32_t,
-                                          int64_t, uint64_t, std::string>;
+using Value = sdbusplus::message::variant<bool, uint8_t, int16_t, uint16_t,
+                                          int32_t, uint32_t, int64_t, uint64_t,
+                                          double, std::string>;
 
 using PropertyMap = std::map<DbusProperty, Value>;
 
