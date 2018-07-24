1f357a2b8d991593a396f512e7350647829e17ad
Patrick Venture <venture@google.com>
2018-07-24 13:20:26 -0700

    oem: add header for Google OEM Codes in use
    The following codes are in use by Google downstream.
    
    Change-Id: Ib3aaa24287b4d5b116266210b0d00de098cf136f
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ host-ipmid/oemgoog.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "host-ipmid/ipmid-api.h"
+#include "host-ipmid/oemrouter.hpp"
+
+namespace oem
+{
+constexpr Number googOemNumber = 11129;
+
+/* Google OEM commands. */
+enum GoogOemCmd
+{
+    ethStatsCmd = 48,
+    gsysCmd = 50,
+    nemoraSettingsCmd = 52,
+    flashOverBTCmd = 127,
+    blobTransferCmd = 128,
+};
+
+}  // namespace oem
+
