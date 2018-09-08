a06a68d55768636c374d690bbe02549828ab6e75
Patrick Venture <venture@google.com>
2018-09-07 19:56:18 -0700

    replaced c headers with cpp where applicable
    Change-Id: I23a70eb540ccde5d2aba467426769feffb07b516
    Signed-off-by: Patrick Venture <venture@google.com>

--- sensorhandler.cpp
+++ sensorhandler.cpp
@@ -7,12 +7,10 @@
 
 #include <host-ipmid/ipmid-api.h>
 #include <mapper.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
 #include <systemd/sd-bus.h>
 
 #include <bitset>
+#include <cmath>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
 #include <set>
@@ -665,7 +663,7 @@ void getSensorThresholds(uint8_t sensorNum,
 
     if (warnLow != 0)
     {
-        warnLow *= pow(10, info.scale - info.exponentR);
+        warnLow *= std::pow(10, info.scale - info.exponentR);
         response->lowerNonCritical = static_cast<uint8_t>(
             (warnLow - info.scaledOffset) / info.coefficientM);
         response->validMask |= static_cast<uint8_t>(
@@ -674,7 +672,7 @@ void getSensorThresholds(uint8_t sensorNum,
 
     if (warnHigh != 0)
     {
-        warnHigh *= pow(10, info.scale - info.exponentR);
+        warnHigh *= std::pow(10, info.scale - info.exponentR);
         response->upperNonCritical = static_cast<uint8_t>(
             (warnHigh - info.scaledOffset) / info.coefficientM);
         response->validMask |= static_cast<uint8_t>(
@@ -690,7 +688,7 @@ void getSensorThresholds(uint8_t sensorNum,
 
     if (critLow != 0)
     {
-        critLow *= pow(10, info.scale - info.exponentR);
+        critLow *= std::pow(10, info.scale - info.exponentR);
         response->lowerCritical = static_cast<uint8_t>(
             (critLow - info.scaledOffset) / info.coefficientM);
         response->validMask |= static_cast<uint8_t>(
@@ -699,7 +697,7 @@ void getSensorThresholds(uint8_t sensorNum,
 
     if (critHigh != 0)
     {
-        critHigh *= pow(10, info.scale - info.exponentR);
+        critHigh *= std::pow(10, info.scale - info.exponentR);
         response->upperCritical = static_cast<uint8_t>(
             (critHigh - info.scaledOffset) / info.coefficientM);
         response->validMask |= static_cast<uint8_t>(
