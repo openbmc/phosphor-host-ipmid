eeb2e89752979da521126e9541defa063eb53b4e
Patrick Venture <venture@google.com>
2018-09-08 09:27:16 -0700

    add iana header to store OEM Numbers
    The iana.hpp header is installed to identify OEM IPMI numbers used, to
    avoid namespace conflicts.
    
    Change-Id: Ifd629817e3b921f86a5ea4cf2c2965a44a2d8201
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ host-ipmid/iana.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "host-ipmid/oemrouter.hpp"
+
+namespace oem
+{
+/*
+ * This is the OpenBMC IANA OEM Number
+ */
+constexpr Number obmcOemNumber = 49871;
+
+/*
+ * This is the Google IANA OEM Number
+ */
+constexpr Number googOemNumber = 11129;
+
+}  // namespace oem
--- host-ipmid/oemopenbmc.hpp
+++ host-ipmid/oemopenbmc.hpp
@@ -1,17 +1,10 @@
 #pragma once
 
 #include "host-ipmid/oemrouter.hpp"
-
 #include "host-ipmid/ipmid-api.h"
 
 namespace oem
 {
-
-/*
- * This is the OpenBMC IANA OEM Number
- */
-constexpr Number obmcOemNumber = 49871;
-
 /*
  * OpenBMC OEM Extension IPMI Command codes.
  */
