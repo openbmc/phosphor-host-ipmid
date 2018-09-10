832012e29090b3b066bb52a025954a79146d3a2f
Patrick Venture <venture@google.com>
2018-09-10 15:53:14 -0700

    add std namespace prefix to common methods
    Standard clib methods should be explicitly grabbed from the standard
    namespace.
    
    Change-Id: I8bcfcb260a99180d535ec8102c1a24c318cbc209
    Signed-off-by: Patrick Venture <venture@google.com>

--- apphandler.cpp
+++ apphandler.cpp
@@ -15,7 +15,20 @@
 #include <systemd/sd-bus.h>
 #include <unistd.h>
 
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <fstream>
+#include <memory>
 #include <nlohmann/json.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
+#include <string>
+#include <tuple>
+#include <vector>
+#include <xyz/openbmc_project/Common/error.hpp>
+#include <xyz/openbmc_project/Software/Activation/server.hpp>
+#include <xyz/openbmc_project/Software/Version/server.hpp>
 
 #if __has_include(<filesystem>)
 #include <filesystem>
@@ -29,20 +42,6 @@ namespace filesystem = std::experimental::filesystem;
 #else
 #error filesystem not available
 #endif
-
-#include <algorithm>
-#include <array>
-#include <cstddef>
-#include <fstream>
-#include <memory>
-#include <phosphor-logging/elog-errors.hpp>
-#include <phosphor-logging/log.hpp>
-#include <string>
-#include <tuple>
-#include <vector>
-#include <xyz/openbmc_project/Common/error.hpp>
-#include <xyz/openbmc_project/Software/Activation/server.hpp>
-#include <xyz/openbmc_project/Software/Version/server.hpp>
 
 extern sd_bus* bus;
 
@@ -303,7 +302,7 @@ ipmi_ret_t ipmi_app_get_device_id(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
 
             rev.minor = (rev.minor > 99 ? 99 : rev.minor);
             dev_id.fw[1] = rev.minor % 10 + (rev.minor / 10) * 16;
-            memcpy(&dev_id.aux, rev.d, 4);
+            std::memcpy(&dev_id.aux, rev.d, 4);
         }
 
         // IPMI Spec version 2.0
@@ -345,7 +344,7 @@ ipmi_ret_t ipmi_app_get_device_id(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
     }
 
     // Pack the actual response
-    memcpy(response, &dev_id, *data_len);
+    std::memcpy(response, &dev_id, *data_len);
 
     return rc;
 }
@@ -386,7 +385,7 @@ ipmi_ret_t ipmi_app_get_self_test_results(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
     selftestresults[0] = 0x56;
     selftestresults[1] = 0;
 
-    memcpy(response, selftestresults, *data_len);
+    std::memcpy(response, selftestresults, *data_len);
 
     return rc;
 }
@@ -474,7 +473,7 @@ ipmi_ret_t ipmi_app_get_device_guid(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
 
             int resp_byte = strtoul(tmp_array, NULL, 16); // Convert to hex byte
             // Copy end to first
-            memcpy((void*)&resp_uuid[resp_loc], &resp_byte, 1);
+            std::memcpy((void*)&resp_uuid[resp_loc], &resp_byte, 1);
             resp_loc--;
             id_octet += 2; // Finished with the 2 chars, advance
         }
@@ -485,7 +484,7 @@ ipmi_ret_t ipmi_app_get_device_guid(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
     *data_len = resp_size;
 
     // Pack the actual response
-    memcpy(response, &resp_uuid, *data_len);
+    std::memcpy(response, &resp_uuid, *data_len);
 
 finish:
     sd_bus_error_free(&error);
@@ -514,7 +513,7 @@ ipmi_ret_t ipmi_app_get_bt_capabilities(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
     *data_len = sizeof(str);
 
     // Pack the actual response
-    memcpy(response, &str, *data_len);
+    std::memcpy(response, &str, *data_len);
 
     return rc;
 }
@@ -531,7 +530,7 @@ ipmi_ret_t ipmi_app_wildcard_handler(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
     *data_len = strlen("THIS IS WILDCARD");
 
     // Now pack actual response
-    memcpy(response, "THIS IS WILDCARD", *data_len);
+    std::memcpy(response, "THIS IS WILDCARD", *data_len);
 
     return rc;
 }
@@ -590,7 +589,7 @@ ipmi_ret_t ipmi_app_get_sys_guid(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         }
 
         *data_len = bmc_guid_len;
-        memcpy(response, &respGuid, bmc_guid_len);
+        std::memcpy(response, &respGuid, bmc_guid_len);
     }
     catch (const InternalFailure& e)
     {
@@ -622,9 +621,9 @@ ipmi_ret_t ipmi_app_set_system_info(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
                                     ipmi_data_len_t dataLen,
                                     ipmi_context_t context)
 {
-    printf("Handling IPMI_CMD_SET_SYSTEM_INFO Netfn:[0x%X], Cmd:[0x%X]\n",
-           netfn, cmd);
-    printf("Setting parameters is not implemented.\n");
+    std::printf("Handling IPMI_CMD_SET_SYSTEM_INFO Netfn:[0x%X], Cmd:[0x%X]\n",
+                netfn, cmd);
+    std::printf("Setting parameters is not implemented.\n");
     return IPMI_CC_INVALID;
 }
 
@@ -700,8 +699,8 @@ ipmi_ret_t ipmi_app_get_system_info(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
     ipmi_sys_info_resp_t resp = {};
     size_t respLen = 0;
     uint8_t* const reqData = static_cast<uint8_t*>(request);
-    printf("Handling IPMI_CMD_GET_SYSTEM_INFO Netfn:[0x%X], Cmd:[0x%X]\n",
-           netfn, cmd);
+    std::printf("Handling IPMI_CMD_GET_SYSTEM_INFO Netfn:[0x%X], Cmd:[0x%X]\n",
+                netfn, cmd);
     const uint8_t paramRequested = reqData[1];
     std::string paramString;
     bool found;
@@ -709,7 +708,7 @@ ipmi_ret_t ipmi_app_get_system_info(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
 
     if (*dataLen < 4)
     {
-        fprintf(stderr, "command is too short (length %zu)\n", *dataLen);
+        std::fprintf(stderr, "command is too short (length %zu)\n", *dataLen);
         return IPMI_CC_INVALID;
     }
 
@@ -721,8 +720,8 @@ ipmi_ret_t ipmi_app_get_system_info(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         goto writeResponse;
     }
 
-    printf("Get System Info Parameter %x, set %u\n", paramRequested,
-           resp.setSelector);
+    std::printf("Get System Info Parameter %x, set %u\n", paramRequested,
+                resp.setSelector);
 
     // The "Set In Progress" parameter can be used for rollback of parameter
     // data and is not implemented.
@@ -757,7 +756,7 @@ ipmi_ret_t ipmi_app_get_system_info(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
     respLen = sizeof(resp); // Write entire string data chunk in response.
 
 writeResponse:
-    memcpy(response, &resp, sizeof(resp));
+    std::memcpy(response, &resp, sizeof(resp));
     *dataLen = respLen;
     return IPMI_CC_OK;
 }
