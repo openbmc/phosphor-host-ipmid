c8f573c0b8ca0fd34fb28346a188ec6548944654
Jason M. Bills <jason.m.bills@linux.intel.com>
2018-09-10 14:12:16 -0700

    Fix IPMI SEL reservations and cancellations
    Per the IPMI Spec, the SEL must be reserved to
       Delete an entry
       Clear the SEL
       Get a partial entry
       Add a partial entry
    
    The current SEL reservation must be cancelled when
       A SEL entry is added
       A SEL entry is deleted
       The SEL is cleared
       The device is reset
       A new reservation is requested
    
    This change adds a reservation status to track when a reservation
    is active and a method to cancel the current reservation, and it
    uses that to cancel the reservation in the Delete, Clear, and Add
    SEL methods.
    
    Change-Id: Ifd72e6d06ecc622855bd9ce8cc3928cbd0f2c34b
    Signed-off-by: Jason M. Bills <jason.m.bills@linux.intel.com>

--- ipmid.cpp
+++ ipmid.cpp
@@ -79,11 +79,29 @@ typedef std::pair<ipmid_callback_t, ipmi_context_t> ipmi_fn_context_t;
 std::map<ipmi_fn_cmd_t, ipmi_fn_context_t> g_ipmid_router_map;
 
 // IPMI Spec, shared Reservation ID.
-unsigned short g_sel_reserve = 0xFFFF;
+static unsigned short selReservationID = 0xFFFF;
+static bool selReservationValid = false;
 
-unsigned short get_sel_reserve_id(void)
+unsigned short reserveSel(void)
 {
-    return g_sel_reserve;
+    // IPMI spec, Reservation ID, the value simply increases against each
+    // execution of the Reserve SEL command.
+    if (++selReservationID == 0)
+    {
+        selReservationID = 1;
+    }
+    selReservationValid = true;
+    return selReservationID;
+}
+
+bool checkSELReservation(unsigned short id)
+{
+    return (selReservationValid && selReservationID == id);
+}
+
+void cancelSELReservation(void)
+{
+    selReservationValid = false;
 }
 
 namespace internal
