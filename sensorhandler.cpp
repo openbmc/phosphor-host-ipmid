openbmc/phosphor-host-ipmid

commit 0fbdbce22771dac6fe8d651e4c8155645807b83d

sensorhandler: fix buffer overrun in ipmi_fru_get_sdr
Change-Id: Ic12598027a92495e49f7cb06aa28f77c0727be44
Signed-off-by: Emily Shaffer <emilyshaffer@google.com>

sensorhandler.cpp
@@ -757,23 +757,18 @@ ipmi_ret_t ipmi_fru_get_sdr(ipmi_request_t request, ipmi_response_t response,
             (FRU_RECORD_ID_START + fru->first), resp);
     }
 
-    if (req->bytes_to_read > (sizeof(*resp) - req->offset))
+    // Check for invalid offset size
+    if (req->offset > sizeof(record))
     {
-        dataLength = (sizeof(*resp) - req->offset);
-    }
-    else
-    {
-        dataLength = req->bytes_to_read;
+        return IPMI_CC_PARM_OUT_OF_RANGE;
     }
 
-    if (dataLength <= 0)
-    {
-        return IPMI_CC_REQ_DATA_LEN_INVALID;
-    }
+    dataLength = std::min(static_cast<size_t>(req->bytes_to_read),
+                          sizeof(record) - req->offset);
 
     std::memcpy(resp->record_data,
                 reinterpret_cast<uint8_t*>(&record) + req->offset,
-                (dataLength));
+                dataLength);
 
     *data_len = dataLength;
     *data_len += 2; // additional 2 bytes for next record ID

commit 1cd85963972719312a0017b59e29d90e1baa4ce5

Fix clang-format issue in sensorhandler.cpp
A clang-format issue in sensorhandler.cpp was introduced by
commit 0fbdbce22771dac6fe8d651e4c8155645807b83d that is causing
CI to fail for changes to phosphor-host-ipmid. This commit
updates sensorhandler.cpp with the latest clang-format changes.

Change-Id: If37a25c50ee04264ce55c8c7959a4fee2aba4045
Signed-off-by: Jason M. Bills <jason.m.bills@linux.intel.com>

sensorhandler.cpp
@@ -767,8 +767,7 @@ ipmi_ret_t ipmi_fru_get_sdr(ipmi_request_t request, ipmi_response_t response,
                           sizeof(record) - req->offset);
 
     std::memcpy(resp->record_data,
-                reinterpret_cast<uint8_t*>(&record) + req->offset,
-                dataLength);
+                reinterpret_cast<uint8_t*>(&record) + req->offset, dataLength);
 
     *data_len = dataLength;
     *data_len += 2; // additional 2 bytes for next record ID
@@ -863,8 +862,8 @@ ipmi_ret_t ipmi_sen_get_sdr(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
                     *data_len);
 
         // data_len should include the LSB and MSB:
-        *data_len += sizeof(resp->next_record_id_lsb)
-                     + sizeof(resp->next_record_id_msb);
+        *data_len +=
+            sizeof(resp->next_record_id_lsb) + sizeof(resp->next_record_id_msb);
     }
 
     return ret;