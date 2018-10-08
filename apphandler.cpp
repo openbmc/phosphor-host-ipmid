0f4119c506944c21c8b72162710a0362b10eb99f
Yong Li <yong.b.li@linux.intel.com>
2018-10-09 01:59:45 +0800

    Implement IPMI ACPI set/get commands
    The IPMI ACPI set/get power state commands are provided
    to allow system software to tell a controller the present
    ACPI power state of the system.
    
    This is an independent setting that may not necessarily match the
    actual power state of the system.
    
    The commands are used to enable the reporting of the power state,
    it does not control or change the power state.
    
    The ACPI status will be saved into flash by settings manager
    
    Tested By:
    Set ACPI:
    ipmitool -H <IP>  -P 0penBmc -I lanplus raw 0x06 0x06 0x86 0x83
    Get ACPI:
    ipmitool -H <IP>  -P 0penBmc -I lanplus raw 0x06 0x07
    
    Change-Id: I6d00c0aec931b98d6691d7c4a54698e08d317aa4
    Signed-off-by: Yong Li <yong.b.li@linux.intel.com>

--- apphandler.cpp
+++ apphandler.cpp
@@ -27,6 +27,7 @@
 #include <tuple>
 #include <vector>
 #include <xyz/openbmc_project/Common/error.hpp>
+#include <xyz/openbmc_project/Control/Power/ACPIPowerState/server.hpp>
 #include <xyz/openbmc_project/Software/Activation/server.hpp>
 #include <xyz/openbmc_project/Software/Version/server.hpp>
 #include <xyz/openbmc_project/State/BMC/server.hpp>
@@ -174,16 +175,203 @@ bool getCurrentBmcState()
                BMC::BMCState::Ready;
 }
 
+namespace acpi_state
+{
+using namespace sdbusplus::xyz::openbmc_project::Control::Power::server;
+
+const static constexpr char* acpiObjPath =
+    "/xyz/openbmc_project/control/host0/acpi_power_state";
+const static constexpr char* acpiInterface =
+    "xyz.openbmc_project.Control.Power.ACPIPowerState";
+const static constexpr char* sysACPIProp = "SysACPIStatus";
+const static constexpr char* devACPIProp = "DevACPIStatus";
+
+static constexpr uint8_t stateLegacyOn = 0x20;
+static constexpr uint8_t stateLegacyOff = 0x21;
+static constexpr uint8_t stateUnknown = 0x2a;
+static constexpr uint8_t stateNoChange = 0x7f;
+static constexpr uint8_t stateMask = 0x80;
+
+static constexpr uint8_t maxDevState = 4;
+static constexpr uint8_t maxSysState = 11;
+
+typedef struct
+{
+    uint8_t sysACPIState;
+    uint8_t devACPIState;
+} __attribute__((packed)) sACPIState;
+
+std::map<ACPIPowerState::ACPI, uint8_t> dbusToIpmi = {
+    {ACPIPowerState::ACPI::S0_G0_D0, 0x00},
+    {ACPIPowerState::ACPI::S1_D1, 0x01},
+    {ACPIPowerState::ACPI::S2_D2, 0x02},
+    {ACPIPowerState::ACPI::S3_D3, 0x03},
+    {ACPIPowerState::ACPI::S4, 0x04},
+    {ACPIPowerState::ACPI::S5_G2, 0x05},
+    {ACPIPowerState::ACPI::S4_S5, 0x06},
+    {ACPIPowerState::ACPI::G3, 0x07},
+    {ACPIPowerState::ACPI::SLEEP, 0x08},
+    {ACPIPowerState::ACPI::G1_SLEEP, 0x09},
+    {ACPIPowerState::ACPI::OVERRIDE, 0x0a},
+    {ACPIPowerState::ACPI::LEGACY_ON, stateLegacyOn},
+    {ACPIPowerState::ACPI::LEGACY_OFF, stateLegacyOff},
+    {ACPIPowerState::ACPI::Unknown, stateUnknown}};
+} // namespace acpi_state
+
 ipmi_ret_t ipmi_app_set_acpi_power_state(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
                                          ipmi_request_t request,
                                          ipmi_response_t response,
                                          ipmi_data_len_t data_len,
                                          ipmi_context_t context)
 {
+    uint8_t s = acpi_state::stateUnknown;
     ipmi_ret_t rc = IPMI_CC_OK;
+
+    sdbusplus::bus::bus bus{ipmid_get_sd_bus_connection()};
+
+    acpi_state::ACPIPowerState::ACPI value =
+        acpi_state::ACPIPowerState::ACPI::Unknown;
+
+    acpi_state::sACPIState* req =
+        reinterpret_cast<acpi_state::sACPIState*>(request);
+
+    if (*data_len != sizeof(acpi_state::sACPIState))
+    {
+        log<level::ERR>("set_acpi invalid len");
+        return IPMI_CC_REQ_DATA_LEN_INVALID;
+    }
+
+    s = req->sysACPIState & ~acpi_state::stateMask;
+    if ((s < acpi_state::maxSysState) || (s == acpi_state::stateLegacyOn) ||
+        (s == acpi_state::stateLegacyOff) || (s == acpi_state::stateUnknown) ||
+        (s == acpi_state::stateNoChange))
+    {
+        if (req->sysACPIState & acpi_state::stateMask)
+        {
+            if (req->sysACPIState != acpi_state::stateNoChange)
+            {
+                for (auto const& it : acpi_state::dbusToIpmi)
+                {
+                    if (it.second == s)
+                    {
+                        value = it.first;
+                        break;
+                    }
+                }
+
+                try
+                {
+                    ipmi::DbusObjectInfo acpiObject =
+                        ipmi::getDbusObject(bus, acpi_state::acpiInterface);
+                    ipmi::setDbusProperty(
+                        bus, acpiObject.second, acpiObject.first,
+                        acpi_state::acpiInterface, acpi_state::sysACPIProp,
+                        convertForMessage(value));
+                }
+                catch (const InternalFailure& e)
+                {
+                    log<level::ERR>("Failed in set ACPI system property",
+                                    entry("EXCEPTION=%s", e.what()));
+                    *data_len = 0;
+                    return IPMI_CC_UNSPECIFIED_ERROR;
+                }
+            }
+        }
+    }
+    else
+    {
+        log<level::ERR>("set_acpi sys invalid input");
+        *data_len = 0;
+        return IPMI_CC_PARM_OUT_OF_RANGE;
+    }
+
+    s = req->devACPIState & ~acpi_state::stateMask;
+    if ((s < acpi_state::maxDevState) || (s == acpi_state::stateUnknown) ||
+        (s == acpi_state::stateNoChange))
+    {
+        if (req->devACPIState & acpi_state::stateMask)
+        {
+            if (req->devACPIState != acpi_state::stateNoChange)
+            {
+                for (auto const& it : acpi_state::dbusToIpmi)
+                {
+                    if (it.second == s)
+                    {
+                        value = it.first;
+                        break;
+                    }
+                }
+
+                try
+                {
+                    ipmi::DbusObjectInfo acpiObject =
+                        ipmi::getDbusObject(bus, acpi_state::acpiInterface);
+                    ipmi::setDbusProperty(
+                        bus, acpiObject.second, acpiObject.first,
+                        acpi_state::acpiInterface, acpi_state::devACPIProp,
+                        convertForMessage(value));
+                }
+                catch (const InternalFailure& e)
+                {
+                    log<level::ERR>("Failed in set ACPI device property",
+                                    entry("EXCEPTION=%s", e.what()));
+                    *data_len = 0;
+                    return IPMI_CC_UNSPECIFIED_ERROR;
+                }
+            }
+        }
+    }
+    else
+    {
+        log<level::ERR>("set_acpi dev invalid input");
+        *data_len = 0;
+        return IPMI_CC_PARM_OUT_OF_RANGE;
+    }
+
+    return rc;
+}
+
+ipmi_ret_t ipmi_app_get_acpi_power_state(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
+                                         ipmi_request_t request,
+                                         ipmi_response_t response,
+                                         ipmi_data_len_t data_len,
+                                         ipmi_context_t context)
+{
+    ipmi_ret_t rc = IPMI_CC_OK;
+
+    acpi_state::sACPIState* res =
+        reinterpret_cast<acpi_state::sACPIState*>(response);
+
+    sdbusplus::bus::bus bus{ipmid_get_sd_bus_connection()};
+
     *data_len = 0;
 
-    log<level::DEBUG>("IPMI SET ACPI STATE Ignoring for now\n");
+    try
+    {
+        ipmi::DbusObjectInfo acpiObject =
+            ipmi::getDbusObject(bus, acpi_state::acpiInterface);
+
+        auto sysACPIVal = ipmi::getDbusProperty(
+            bus, acpiObject.second, acpiObject.first, acpi_state::acpiInterface,
+            acpi_state::sysACPIProp);
+        auto sysACPI = acpi_state::ACPIPowerState::convertACPIFromString(
+            sysACPIVal.get<std::string>());
+        res->sysACPIState = acpi_state::dbusToIpmi.at(sysACPI);
+
+        auto devACPIVal = ipmi::getDbusProperty(
+            bus, acpiObject.second, acpiObject.first, acpi_state::acpiInterface,
+            acpi_state::devACPIProp);
+        auto devACPI = acpi_state::ACPIPowerState::convertACPIFromString(
+            devACPIVal.get<std::string>());
+        res->devACPIState = acpi_state::dbusToIpmi.at(devACPI);
+
+        *data_len = 2;
+    }
+    catch (const InternalFailure& e)
+    {
+        log<level::ERR>("Failed in get ACPI property");
+        return IPMI_CC_UNSPECIFIED_ERROR;
+    }
     return rc;
 }
 
@@ -854,6 +1042,10 @@ void register_netfn_app_functions()
     // <Set ACPI Power State>
     ipmi_register_callback(NETFUN_APP, IPMI_CMD_SET_ACPI, NULL,
                            ipmi_app_set_acpi_power_state, PRIVILEGE_ADMIN);
+
+    // <Get ACPI Power State>
+    ipmi_register_callback(NETFUN_APP, IPMI_CMD_GET_ACPI, NULL,
+                           ipmi_app_get_acpi_power_state, PRIVILEGE_ADMIN);
 
     // <Get Channel Access>
     ipmi_register_callback(NETFUN_APP, IPMI_CMD_GET_CHANNEL_ACCESS, NULL,
