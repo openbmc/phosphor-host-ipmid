a2df90350cec03840e6d43ffd6c3bc8c6bbb244a
Vernon Mauery <vernon.mauery@linux.intel.com>
2018-10-08 12:05:18 -0700

    Add in Native OEM and Group OEM support
    Add full support for the IPMI OEM NetFn and the Group OEM support as
    defined in the IPMI 2.0 specification.
    
    For now the legacy OEM router mechanism is still supported.
    
    Change-Id: I8cc999489000c6e0daf5aad3579838e6f499ba47
    Signed-off-by: Vernon Mauery <vernon.mauery@linux.intel.com>

--- ipmi/handler.hpp
+++ ipmi/handler.hpp
@@ -230,6 +230,34 @@ class IpmiHandler<ipmid_callback_t> : public HandlerBase
         return response;
     }
 };
+template <>
+class IpmiHandler<oem::Handler> : public HandlerBase
+{
+  public:
+    explicit IpmiHandler(const oem::Handler& handler) : handler_(handler)
+    {
+    }
+
+  private:
+    oem::Handler handler_;
+
+    message::Response::ptr
+        executeCallback(message::Request::ptr request) override
+    {
+        auto response = request->makeResponse();
+        size_t len = request->raw.size();
+        // (re-)allocate a big request buffer here
+        request->raw.resize(64 * 1024);
+        // allocate a big response buffer here
+        response->raw.resize(64 * 1024);
+
+        auto ccRet = handler_(request->ctx->cmd, request->raw.data(),
+                              response->raw.data(), &len);
+        response->cc = ccRet;
+        response->raw.resize(len);
+        return response;
+    }
+};
 
 inline auto makeLegacyHandler(const ipmid_callback_t& handler)
 {
@@ -237,6 +265,12 @@ inline auto makeLegacyHandler(const ipmid_callback_t& handler)
     return ptr;
 }
 
+inline auto makeLegacyHandler(oem::Handler&& handler)
+{
+    HandlerBase::ptr ptr(
+        new IpmiHandler<oem::Handler>(std::forward<oem::Handler>(handler)));
+    return ptr;
+}
 #endif // ALLOW_DEPRECATED_API
 
 template <typename Handler>
