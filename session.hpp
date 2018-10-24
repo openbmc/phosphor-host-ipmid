07e5b28c142abc0d2f1a94d6ce9ef21446226f5e
Vernon Mauery <vernon.mauery@linux.intel.com>
2018-10-24 13:11:23 -0700

    netipmid: make session a header-only situation
    The Session class is a fairly simple class, no need for an external cpp
    file; make it all header-only and allow the compiler to do its job.
    
    Change-Id: Ibbe7e963762926ec04c75ab187a5b8045de851f4
    Signed-off-by: Vernon Mauery <vernon.mauery@linux.intel.com>

--- session.hpp
+++ session.hpp
@@ -231,7 +231,31 @@ class Session
      * transaction time is compared against the session inactivity timeout.
      *
      */
-    bool isSessionActive();
+    bool isSessionActive()
+    {
+        auto currentTime = std::chrono::steady_clock::now();
+        auto elapsedSeconds = std::chrono::duration_cast<std::chrono::seconds>(
+            currentTime - lastTime);
+
+        switch (state)
+        {
+            case State::SETUP_IN_PROGRESS:
+                if (elapsedSeconds < SESSION_SETUP_TIMEOUT)
+                {
+                    return true;
+                }
+                break;
+            case State::ACTIVE:
+                if (elapsedSeconds < SESSION_INACTIVITY_TIMEOUT)
+                {
+                    return true;
+                }
+                break;
+            default:
+                return false;
+        }
+        return false;
+    }
 
     /**
      * @brief Session's Current Privilege Level
