96d1b00bc0af3da272a7f4ca45061e88cff4dc63
Vernon Mauery <vernon.mauery@linux.intel.com>
2018-11-07 09:36:22 -0800

    ipmid: Add command filter mechanism
    Every ipmi command will pass through a filter prior to execution by the
    registered handler. The filter consists of all registered filter
    handlers that all must either return an IPMI error code or allow the
    command to be executed. If any of the filter handlers return an error
    code, the remaining handlers will not get a chance to run.
    
    Each handler, executed in registered priority order, can be passed the
    full message, or just the context (metadata describing the command,
    netfn, cmd, etc.)
    
    Change-Id: I3c48f19ebae0d24344b15fbcd2b940a32f8511d7
    Signed-off-by: Vernon Mauery <vernon.mauery@linux.intel.com>

--- ipmid-new.cpp
+++ ipmid-new.cpp
@@ -150,6 +150,14 @@ static std::map<std::pair<Iana, Cmd>, /* key is Iana/Cmd (NetFn is 2Eh) */
                 HandlerTuple>
     oemHandlerMap;
 
+using FilterTuple = std::tuple<int,             /* prio */
+                               FilterBase::ptr, /* filter */
+                               std::any         /* ctx */
+                               >;
+
+/* list to hold all registered ipmi command filters */
+static std::list<FilterTuple> filterList;
+
 namespace impl
 {
 /* common function to register all standard IPMI handlers */
@@ -218,6 +226,18 @@ void registerOemHandler(int prio, Iana iana, Cmd cmd, Privilege priv,
     {
         mapCmd = item;
     }
+}
+
+/* common function to register all IPMI filter handlers */
+void registerFilter(int prio, FilterBase::ptr filter, std::any& ctx)
+{
+    FilterTuple item(prio, filter, ctx);
+
+    // walk the list and put it in the right place
+    auto i = filterList.begin();
+    for (; i != filterList.end() && std::get<int>(*i) > prio; i++)
+        ;
+    filterList.insert(i, item);
 }
 
 } // namespace impl
@@ -298,6 +318,24 @@ message::Response::ptr executeIpmiOemCommand(message::Request::ptr request)
     return errorResponse(request, ccInvalidCommand, iana);
 }
 
+message::Response::ptr filterIpmiCommand(message::Request::ptr request)
+{
+    // pass the command through the filter mechanism
+    // This can be the firmware firewall or any OEM mechanism like
+    // whitelist filtering based on operational mode
+    for (auto& item : filterList)
+    {
+        auto& filter = std::get<1>(item);
+        // auto& ctx = std::get<2>(item);
+        ipmi::Cc cc = filter->call(request);
+        if (ipmi::ccSuccess != cc)
+        {
+            return errorResponse(request, cc);
+        }
+    }
+    return message::Response::ptr();
+}
+
 message::Response::ptr executeIpmiCommand(message::Request::ptr request)
 {
     // the command has already passed through filter; execute it
@@ -348,7 +386,12 @@ auto executionEntry(boost::asio::yield_context yield, NetFn netFn, uint8_t lun,
     auto ctx = std::make_shared<ipmi::Context>(netFn, cmd, 0, 0,
                                                ipmi::privilegeAdmin, &yield);
     auto request = std::make_shared<ipmi::message::Request>(ctx, data);
-    auto response = executeIpmiCommand(request);
+    auto response = filterIpmiCommand(request);
+    // an empty response means the command was not filtered
+    if (!response)
+    {
+        response = executeIpmiCommand(request);
+    }
 
     // Responses in IPMI require a bit set.  So there ya go...
     netFn |= 0x01;
@@ -564,7 +607,12 @@ void handleLegacyIpmiCommand(sdbusplus::message::message& m)
         std::make_shared<ipmi::Context>(netFn, cmd, 0, 0, ipmi::privilegeAdmin);
     auto request = std::make_shared<ipmi::message::Request>(ctx, data);
 
-    auto response = ipmi::executeIpmiCommand(request);
+    auto response = ipmi::filterIpmiCommand(request);
+    // an empty response means the command was not filtered
+    if (!response)
+    {
+        response = ipmi::executeIpmiCommand(request);
+    }
 
     // Responses in IPMI require a bit set.  So there ya go...
     netFn |= 0x01;
