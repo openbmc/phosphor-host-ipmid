baf5333e3e0c7044fa206c03e8a1a26c7918b5b5
Vernon Mauery <vernon.mauery@linux.intel.com>
2018-11-28 11:02:43 -0800

    ipmid: channel: add max transfer size property
    Each channel can have a different maximum transfer size. This should be
    exposed so that it is easier to tell how many bytes it is possible to
    send or receive on a given channel.
    
    Change-Id: Ie277eb7a802edc1deeefe201a7560d371920f519
    Signed-off-by: Vernon Mauery <vernon.mauery@linux.intel.com>

--- user_channel/channel_layer.hpp
+++ user_channel/channel_layer.hpp
@@ -225,6 +225,14 @@ EChannelSessSupported getChannelSessionSupport(const uint8_t chNum);
  */
 int getChannelActiveSessions(const uint8_t chNum);
 
+/** @brief determines maximum transfer size for a channel
+ *
+ *  @param[in] chNum - channel number
+ *
+ *  @return maximum bytes that can be transferred on this channel
+ */
+size_t getChannelMaxTransferSize(uint8_t chNum);
+
 /** @brief initializes channel management
  *
  *  @return IPMI_CC_OK for success, others for failure.
