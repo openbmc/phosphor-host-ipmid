1d8c5a85a51d069c42bcfc358e46090d20fab71f
Johnathan Mantey <johnathanx.mantey@intel.com>
2018-12-11 13:53:33 -0800

    Move public variables to the class private space.
    Moved two variables only used by the class into it's private variable
    space.
    
    Highlighted a method prototype that was buried within the variable list.
    
    Change-Id: Ie90d6b7db85ce1a9e445de87d612c41b4337d7fc
    Signed-off-by: Johnathan Mantey <johnathanx.mantey@intel.com>

--- user_channel/channel_mgmt.hpp
+++ user_channel/channel_mgmt.hpp
@@ -215,16 +215,12 @@ class ChannelConfig
      */
     int writeChannelVolatileData();
 
-    uint32_t signalFlag = 0;
-
-    std::unique_ptr<boost::interprocess::named_recursive_mutex> channelMutex{
-        nullptr};
-
   private:
+    uint32_t signalFlag;
+    std::unique_ptr<boost::interprocess::named_recursive_mutex> channelMutex;
     std::array<ChannelProperties, maxIpmiChannels> channelData;
     std::time_t nvFileLastUpdatedTime;
     std::time_t voltFileLastUpdatedTime;
-    std::time_t getUpdatedFileTime(const std::string& fileName);
     boost::interprocess::file_lock mutexCleanupLock;
     bool signalHndlrObjectState;
     boost::interprocess::file_lock sigHndlrLock;
@@ -386,6 +382,15 @@ class ChannelConfig
 
     void processChAccessPropChange(const std::string& path,
                                    const DbusChObjProperties& chProperties);
+
+    /** @brief function to retrieve last modification time for the named file
+     *
+     *  @param[in] fileName - the name of the file for which to acquire
+     *  timestamp data
+     *
+     *  @return time the file was last modified
+     */
+    std::time_t getUpdatedFileTime(const std::string& fileName);
 };
 
 } // namespace ipmi
