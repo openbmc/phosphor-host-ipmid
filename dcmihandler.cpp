0841b27d03ce4f354f39a348cd5cd89fdc23b97a
Johnathan Mantey <johnathanx.mantey@intel.com>
2018-12-13 13:17:56 -0800

    Place IPMI channel to NIC device mapping in the IPMI JSON file
    Mapping of the generic IPMI channel name (i.e. LAN1) was hard coded
    within the channel management source.  This mapping is now present in
    the JSON configuration file instead.
    
    Change-Id: Ibc6821cae5a26f2666467aba5346d364053f2582
    Signed-off-by: Johnathan Mantey <johnathanx.mantey@intel.com>

--- dcmihandler.cpp
+++ dcmihandler.cpp
@@ -2,7 +2,7 @@
 
 #include "dcmihandler.hpp"
 
-#include "net.hpp"
+#include "user_channel/channel_layer.hpp"
 #include "utils.hpp"
 
 #include <host-ipmid/ipmid-api.h>
@@ -248,7 +248,7 @@ bool getDHCPEnabled()
     sdbusplus::bus::bus bus{ipmid_get_sd_bus_connection()};
 
     auto ethdevice =
-        ipmi::network::ChanneltoEthernet(ethernetDefaultChannelNum);
+        ipmi::getChannelIntfNameFromChannelNumber(ethernetDefaultChannelNum);
     auto ethernetObj =
         ipmi::getDbusObject(bus, ethernetIntf, networkRoot, ethdevice);
     auto service = ipmi::getService(bus, ethernetIntf, ethernetObj.first);
