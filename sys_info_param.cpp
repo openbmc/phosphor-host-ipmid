8b819d6acb99f1ffe8b9fd49f092414d64d048e3
Jia, Chunhui <chunhui.jia@linux.intel.com>
2018-12-26 16:55:07 +0800

    Set/Get System Info Parameters command
    Implementation details:
    1. encoding supports ASCII only
    2. each paramter supports 64 bytes data
    3. SetInProgress is implemented but rollback feature is not
       supported.
    4. non-volatile data will be saved in nv storage and restored
       after BMC reboot.
    5. 16 bytes chunk could be cached. Cache is available for only
       one parameter at a time.
    
    Test:
    Verified with ipmitool for UEFI shell and IPMI over LAN
    
    Change-Id: I671b1b66b3ce867b7aa1ce72437379c412d48eb1
    Signed-off-by: Jia, Chunhui <chunhui.jia@linux.intel.com>

--- sys_info_param.cpp
+++ sys_info_param.cpp
@@ -1,5 +1,8 @@
 #include "sys_info_param.hpp"
 
+#include <fstream>
+#include <phosphor-logging/log.hpp>
+
 std::tuple<bool, std::string>
     SysInfoParamStore::lookup(uint8_t paramSelector) const
 {
@@ -14,10 +17,28 @@ std::tuple<bool, std::string>
     return std::make_tuple(true, s);
 }
 
-void SysInfoParamStore::update(uint8_t paramSelector, const std::string& s)
+void SysInfoParamStore::update(uint8_t paramSelector, const std::string& s,
+                               bool isNonVolatile)
 {
     // Add a callback that captures a copy of the string passed and returns it
     // when invoked.
+    if (true == isNonVolatile)
+    {
+        try
+        {
+            std::string filename = "/var/lib/ipmi/sysinfo" +
+                                   std::to_string(paramSelector) + ".txt";
+            std::ofstream outFile(filename, std::ios::out | std::ios::trunc);
+            outFile.write(s.c_str(), s.size());
+            outFile.close();
+        }
+        catch (std::ios_base::failure& e)
+        {
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                "Exception",
+                phosphor::logging::entry("EXCEPTION=%s", e.what()));
+        }
+    }
 
     // clang-format off
     update(paramSelector, [s]() {
@@ -30,4 +51,51 @@ void SysInfoParamStore::update(uint8_t paramSelector,
                                const std::function<std::string()>& callback)
 {
     params[paramSelector] = callback;
+}
+
+int SysInfoParamStore::restore(uint8_t paramSelector)
+{
+    constexpr size_t buffLen = 64;
+    char buf[buffLen];
+    std::string filename =
+        "/var/lib/ipmi/sysinfo" + std::to_string(paramSelector) + ".txt";
+    std::string s = "";
+
+    try
+    {
+        std::ifstream infile(filename);
+        if (!infile.good())
+        {
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                "file does not exist",
+                phosphor::logging::entry("FILE=%s", filename.c_str()));
+            return -1;
+        }
+        if (infile.peek() != std::ifstream::traits_type::eof())
+        { // not empty file
+            infile.read(buf, buffLen);
+            uint8_t size = infile.gcount();
+            if (size > buffLen)
+            {
+                phosphor::logging::log<phosphor::logging::level::ERR>(
+                    "data from file excced maximum size");
+                return -1;
+            }
+            buf[size] = '\0';
+            s = buf;
+        }
+        infile.close();
+    }
+    catch (std::ios_base::failure& e)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Exception", phosphor::logging::entry("EXCEPTION=%s", e.what()));
+        return -1;
+    }
+    // clang-format off
+    update(paramSelector, [s]() {
+        return s;
+    });
+    // clang-format on
+    return 0;
 }
