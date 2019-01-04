536d55ac10f0f7efbb8c8d92940442cba3a1414a
Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
2019-01-04 23:48:02 +0530

    Use network interface name as channel name
    Use network interface name as channel name for the LAN
    channels. Mapper related to convert IPMI LAN reference to
    network interface is not needed anymore, and the same has been
    removed.
    
    Unit-test:
    verified ipmitool channel info for channel 1 & 2 and made sure
    it works fine as expected.
    
    Change-Id: Id87e2924a2a21a846507d6378b1b615000280f06
    Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>

--- user_channel/channel_mgmt.hpp
+++ user_channel/channel_mgmt.hpp
@@ -96,6 +96,14 @@ class ChannelConfig
      */
     bool isValidAuthType(const uint8_t chNum, const EAuthType& authType);
 
+    /** @brief function to get channel name from channel number
+     *
+     *  @param[in] chNum - channel number index
+     *
+     *  @return network channel interface name
+     */
+    std::string getChannelName(const uint8_t chNum);
+
     /** @brief determines supported session type of a channel
      *
      *  @param[in] chNum - channel number
@@ -387,14 +395,6 @@ class ChannelConfig
      *  @return channel protocol  type
      */
     EChannelProtocolType convertToProtocolTypeIndex(const std::string& value);
-
-    /** @brief function to convert channel name to network interface name
-     *
-     *  @param[in] value - channel interface name - ipmi centric
-     *
-     *  @return network channel interface name
-     */
-    std::string convertToNetInterface(const std::string& value);
 };
 
 } // namespace ipmi
