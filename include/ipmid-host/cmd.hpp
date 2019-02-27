ff874c6852338ef963937247c511dcfbc703c8b6
Vernon Mauery <vernon.mauery@linux.intel.com>
2019-02-27 10:05:14 -0800

    Add back an interface used by an external provider
    The move to the new API removed the ipmid_get_sdbus_plus_handler
    interface because it looked like it was self-contained in the
    executable, not realizing it was needed by an external provider.
    
    Change-Id: I75bb898294cc59cae7d291f7a6a6ed1e61d62903
    Signed-off-by: Vernon Mauery <vernon.mauery@linux.intel.com>

--- include/ipmid-host/cmd.hpp
+++ include/ipmid-host/cmd.hpp
@@ -4,3 +4,4 @@
 
 // Global Host Bound Command manager
 extern void ipmid_send_cmd_to_host(phosphor::host::command::CommandHandler&&);
+extern std::unique_ptr<sdbusplus::bus::bus>& ipmid_get_sdbus_plus_handler();
