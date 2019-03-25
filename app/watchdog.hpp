57e89419ecc069c7baa556092860e66e9992d492
Vernon Mauery <vernon.mauery@linux.intel.com>
2019-03-25 14:17:54 -0700

    rewrite Reset Watchdog Timer command to use new provider API
    Modify to use the new provider API. One by one, change calls to
    remove any legacy API constructs.
    
    Change-Id: If6cd1d921908dc7f5462e99b0098b4897e743ebb
    Signed-off-by: Vernon Mauery <vernon.mauery@linux.intel.com>

--- app/watchdog.hpp
+++ app/watchdog.hpp
@@ -2,22 +2,11 @@
 
 #include <ipmid/api.h>
 
+#include <ipmid/api.hpp>
+
 /** @brief The RESET watchdog IPMI command.
- *
- *  @param[in] netfn
- *  @param[in] cmd
- *  @param[in] request
- *  @param[in,out] response
- *  @param[out] data_len
- *  @param[in] context
- *
- *  @return IPMI_CC_OK on success, an IPMI error code otherwise.
  */
-ipmi_ret_t ipmi_app_watchdog_reset(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
-                                   ipmi_request_t request,
-                                   ipmi_response_t response,
-                                   ipmi_data_len_t data_len,
-                                   ipmi_context_t context);
+ipmi::RspType<> ipmiAppResetWatchdogTimer();
 
 /** @brief The SET watchdog IPMI command.
  *
