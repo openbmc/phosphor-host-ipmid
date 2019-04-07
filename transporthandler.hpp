f85eacf428746a1ec729d878515ab384186dbb66
William A. Kennington III <wak@google.com>
2019-04-07 05:18:04 -0700

    transporthandler: Convert netmask storage to prefix
    This is another consistency change to align the ChannelConfig to be
    closer to the format used in dbus. It also greatly simplifies the
    netmaskToPrefix code down to a few instructions.
    
    Tested:
        Bad netmasks set on the interface are correctly handled prior to
        being stored in the configuraiton. Good netmasks are set correctly
        and verified that the network information gets updated correctly.
        Printed out the netmask before and after to ensure the output is
        consistent with the system state.
    
    Change-Id: Iad76c92e282097fbda8018c03b719c3090876103
    Signed-off-by: William A. Kennington III <wak@google.com>

--- transporthandler.hpp
+++ transporthandler.hpp
@@ -89,10 +89,12 @@ constexpr uint8_t SET_IN_PROGRESS_RESERVED = 3; // Reserved
 const int CHANNEL_MASK = 0x0f;
 const int NUM_CHANNELS = 0x0f;
 
+constexpr uint8_t DEFAULT_PREFIX = 32;
+
 struct ChannelConfig_t
 {
     std::string ipaddr;
-    std::string netmask;
+    std::optional<uint8_t> prefix;
     std::optional<bool> dhcpEnabled;
     std::string gateway;
     // IPMI stores the vlan info in 16 bits,32 bits is to aligned
@@ -105,7 +107,7 @@ struct ChannelConfig_t
     void clear()
     {
         ipaddr.clear();
-        netmask.clear();
+        prefix.reset();
         dhcpEnabled.reset();
         gateway.clear();
         vlanID = ipmi::network::VLAN_ID_MASK;
