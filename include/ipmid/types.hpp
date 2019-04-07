dcde8275c5125dc6b54cf865c52cdc836634db97
William A. Kennington III <wak@google.com>
2019-04-06 18:19:38 -0700

    transporthandler: Rewrite + New Handler
    This rewrites the old transport handler to use the new ipmi handler
    registration functions. It attempts to clean up the old code, by
    refactoring any business logic out of the code that parses the IPMI
    messages. This makes the code paths easier to understand and allows for
    better code re-use.
    
    This also gets rid of the concept of the settings change timer. Clients
    expect to see their settings take effect as soon as they are set,
    regardless of the "Set In Progress" flag. This means we no longer need a
    cache for our network settings that are about to be set by the daemon,
    and a client can hold the BMC in "Set In Progress" while it verifies
    settings like other BMC implementations.
    
    Change-Id: I5406a674f087600afdfc2c0b3adeacde10986abc
    Signed-off-by: William A. Kennington III <wak@google.com>

--- include/ipmid/types.hpp
+++ include/ipmid/types.hpp
@@ -215,12 +215,7 @@ using EntityInfoMap = std::map<Id, EntityInfo>;
 
 namespace network
 {
-using ChannelEthMap = std::map<int, std::string>;
-
 constexpr auto MAC_ADDRESS_FORMAT = "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx";
-constexpr auto IP_ADDRESS_FORMAT = "%u.%u.%u.%u";
-constexpr auto PREFIX_FORMAT = "%hhd";
-constexpr auto ADDR_TYPE_FORMAT = "%hhx";
 
 constexpr auto IPV4_ADDRESS_SIZE_BYTE = 4;
 constexpr auto IPV6_ADDRESS_SIZE_BYTE = 16;
@@ -228,20 +223,6 @@ constexpr auto IPV6_ADDRESS_SIZE_BYTE = 16;
 constexpr auto DEFAULT_MAC_ADDRESS = "00:00:00:00:00:00";
 constexpr auto DEFAULT_ADDRESS = "0.0.0.0";
 
-constexpr auto MAC_ADDRESS_SIZE_BYTE = 6;
-constexpr auto VLAN_SIZE_BYTE = 2;
-constexpr auto IPSRC_SIZE_BYTE = 1;
-constexpr auto BITS_32 = 32;
-constexpr auto MASK_32_BIT = 0xFFFFFFFF;
-constexpr auto VLAN_ID_MASK = 0x00000FFF;
-constexpr auto VLAN_ENABLE_MASK = 0x8000;
-
-enum class IPOrigin : uint8_t
-{
-    UNSPECIFIED = 0,
-    STATIC = 1,
-    DHCP = 2,
-};
-
 } // namespace network
+
 } // namespace ipmi
