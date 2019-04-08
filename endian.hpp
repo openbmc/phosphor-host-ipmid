a5bc5a2886e219c9b458fd6ead42daa4980f156f
Alexander Amelkin <a.amelkin@yadro.com>
2019-04-08 17:54:26 +0300

    Add 3-byte conversion functions to endian.hpp
    Sometimes 24-bit integer values are used in IPMI.
    They can't be endian-wise converted using standard
    functions that swap bytes in two- and four-byte
    words. Special functions are needed that would
    operate on IPMI array of bytes.
    
    Partially resolves openbmc/openbmc#3391
    
    Change-Id: Ib80ad4210c826c53eef0dedc86a3a110fd480a17
    Signed-off-by: Alexander Amelkin <a.amelkin@yadro.com>

--- endian.hpp
+++ endian.hpp
@@ -3,10 +3,45 @@
 #include <endian.h>
 #include <stdint.h>
 
+#include <climits>
+
 namespace endian
 {
 namespace details
 {
+// These two template functions convert IPMI data buffer to/from an
+// arbitrary length integer. These can be used for 24-bit numbers,
+// where usual integer conversion would fail.
+
+template <typename T>
+T from_ipmi(uint8_t* ipmi, size_t bytes)
+{
+    size_t byte = 0;
+    T o;
+
+    // IPMI always has LSB first
+    for (o = byte = 0; byte < bytes && byte < sizeof(T); ++byte)
+    {
+        o |= ipmi[byte] << (CHAR_BIT * byte);
+    }
+
+    return o;
+}
+
+template <typename T>
+void to_ipmi(T i, uint8_t* ipmi, size_t bytes)
+{
+    size_t byte = 0;
+
+    // IPMI always has LSB first
+    for (byte = 0; byte < bytes && byte < sizeof(T); ++byte)
+    {
+        ipmi[byte] = static_cast<uint8_t>((i >> (CHAR_BIT * byte)) & 0xFF);
+    }
+}
+
+// These are integer conversion templates. Work only for 16- and 32-bit
+// integers (well, for 64 too, but they aren't used in IPMI)
 template <typename T>
 struct convert
 {
@@ -31,16 +66,28 @@ template<> struct convert<uint32_t>
     static uint32_t to_network(uint32_t i) { return htobe32(i); };
     static uint32_t from_network(uint32_t i) { return be32toh(i); };
 };
-}
+} // namespace details
 
 template<typename T> T to_ipmi(T i)
 {
     return details::convert<T>::to_ipmi(i);
 }
 
+template <typename T>
+void to_ipmi(T i, uint8_t* ipmi, size_t bytes)
+{
+    return details::to_ipmi<T>(i, ipmi, bytes);
+}
+
 template<typename T> T from_ipmi(T i)
 {
     return details::convert<T>::from_ipmi(i);
+}
+
+template <typename T>
+T from_ipmi(uint8_t* ipmi, size_t bytes)
+{
+    return details::from_ipmi<T>(ipmi, bytes);
 }
 
 template<typename T> T to_network(T i)
@@ -51,4 +98,4 @@ template<typename T> T from_network(T i)
 {
     return details::convert<T>::from_network(i);
 }
-}
+} // namespace endian
