f4091eebe1f4bb0f99352c4956b0b7e3ebd3fc01
jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
2019-05-02 21:13:30 +0000

    storagehandler: move get & reserve SEL to new API
    Rewrite "Get & Reserve SEL" command to use the newly
    introduced IPMI provider API.
    
    Tested:
    1. verified ipmitool sel info is same both before and
    after the changes
    2. verfied the Reserve SEL command
    
    Change-Id: I37d602293066274a3fc417ad7e59cea3c73315f7
    Signed-off-by: jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
    Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
    Signed-off-by: jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>

--- selutility.hpp
+++ selutility.hpp
@@ -35,20 +35,6 @@ static constexpr auto selVersion = 0x51;
 static constexpr auto invalidTimeStamp = 0xFFFFFFFF;
 static constexpr auto operationSupport = 0x0A;
 
-/** @struct GetSELInfoResponse
- *
- *  IPMI payload for Get SEL Info command response.
- */
-struct GetSELInfoResponse
-{
-    uint8_t selVersion;       //!< SEL revision.
-    uint16_t entries;         //!< Number of log entries in SEL.
-    uint16_t freeSpace;       //!< Free Space in bytes.
-    uint32_t addTimeStamp;    //!< Most recent addition timestamp.
-    uint32_t eraseTimeStamp;  //!< Most recent erase timestamp.
-    uint8_t operationSupport; //!< Operation support.
-} __attribute__((packed));
-
 static constexpr auto firstEntry = 0x0000;
 static constexpr auto lastEntry = 0xFFFF;
 static constexpr auto entireRecord = 0xFF;
