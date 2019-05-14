6f2b392567915ca7623a55e46a00d52d4c3360a7
Sumanth Bhat <sumanth.bhat@linux.intel.com>
2019-05-14 12:13:57 +0000

    Initialise Cipher Suite Privilege Levels configuration file
    IPMI Spec 13.17 provides a way to pick a "highest level matching
    proposed algorithms" during RMCPP Open Session Request, and effective
    privilege levels are decided taking Cipher Suite Privilege Levels into
    account. This patch initialises Cipher Suite Privielge Levels
    configurations into a JSON file. By default, admin privileges are given
    to all Cipher Suites across all channels.
    
    Verify-instruction: cs_privilege_levels.json is initialised in /var/lib/ipmi
    
    Unit-test :
    1. File is initialised only if the file doesn't exist when
    phosphor-ipmi-host service starts
    2. File data is as expected and persistant across reboots.
    3. If manually deleted the file and restart phosphor-ipmi-host; file is
    initialised again with default values
    
    Change-Id: I526b8708f63659210768c77e8e19fa5a76df1f0d
    Signed-off-by: Sumanth Bhat <sumanth.bhat@linux.intel.com>

---
+++ user_channel/cipher_mgmt.cpp
@@ -0,0 +1,134 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "cipher_mgmt.hpp"
+
+#include <experimental/filesystem>
+#include <fstream>
+#include <phosphor-logging/log.hpp>
+
+namespace ipmi
+{
+
+using namespace phosphor::logging;
+namespace fs = std::experimental::filesystem;
+
+static constexpr const char* cipherSuitePrivFileName  =
+    "/var/lib/ipmi/cs_privilege_levels.json";
+
+static constexpr const char* privAdmin = "PrivAdmin";
+
+static constexpr uint8_t maxCSRecords = 16;
+
+CipherConfig& getCipherConfigObject()
+{
+    static CipherConfig cipherConfig;
+    return cipherConfig;
+}
+
+CipherConfig::~CipherConfig()
+{
+    // Nothing to do here as of now
+}
+
+CipherConfig::CipherConfig()
+{
+    if (!fs::exists(cipherSuitePrivFileName))
+    {
+        log<level::DEBUG>(
+            "CS privilege levels file does not exist. Initialising...");
+        initPrivilegeLevelsPersistData();
+    }
+}
+
+Json CipherConfig::readJsonFile(const std::string& configFile)
+{
+    std::ifstream jsonFile(configFile);
+    if (!jsonFile.good())
+    {
+        log<level::ERR>("JSON file not found");
+        return nullptr;
+    }
+
+    Json data = nullptr;
+    try
+    {
+        data = Json::parse(jsonFile, nullptr, false);
+    }
+    catch (Json::parse_error& e)
+    {
+        log<level::DEBUG>("Corrupted channel config.",
+                          entry("MSG: %s", e.what()));
+        throw std::runtime_error("Corrupted channel config file");
+    }
+
+    return data;
+}
+
+int CipherConfig::writeJsonFile(const std::string& configFile,
+                                const Json& jsonData)
+{
+    std::ofstream jsonFile(configFile);
+    if (!jsonFile.good())
+    {
+        log<level::ERR>("JSON file not found");
+        return -EIO;
+    }
+
+    // Write JSON to file
+    jsonFile << jsonData;
+
+    jsonFile.flush();
+    return 0;
+}
+
+void CipherConfig::initPrivilegeLevelsPersistData()
+{
+    Json initData;
+    std::ofstream csPrivilegeLevelsFile;
+    uint8_t chNum, csNum;
+
+    csPrivilegeLevelsFile.open(cipherSuitePrivFileName);
+
+    for (chNum = 0; chNum < ipmi::maxIpmiChannels; chNum++)
+    {
+        Json privData;
+
+        std::string chKey = "Channel" + std::to_string(chNum);
+        for (csNum = 0; csNum < maxCSRecords; csNum++)
+        {
+            std::string csKey = "CipherID" + std::to_string(csNum);
+            // By default, provide Admin privileges for all Cipher Suites,
+            // across all channels
+            privData[csKey] = privAdmin;
+        }
+        initData[chKey] = privData;
+    }
+
+    if (writeJsonFile(cipherSuitePrivFileName, initData) != 0)
+    {
+        log<level::ERR>(
+            "Error initialising CipherSuite Privilege Levels file.");
+    }
+
+    csPrivilegeLevelsFile.close();
+
+    log<level::DEBUG>("Successfully initialised CipherSuite Privilege Levels "
+                      "data initialization.");
+    return;
+}
+
+} // namespace ipmi
---
+++ user_channel/cipher_mgmt.hpp
@@ -0,0 +1,61 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+#include "channel_layer.hpp"
+
+#include <nlohmann/json.hpp>
+
+namespace ipmi
+{
+
+using Json = nlohmann::json;
+
+class CipherConfig;
+
+CipherConfig& getCipherConfigObject();
+
+class CipherConfig
+{
+  public:
+    ~CipherConfig();
+    CipherConfig();
+
+  private:
+    /** @brief function to read json config file
+     *
+     *  @param[in] configFile - configuration file name
+     *
+     *  @return Json object
+     */
+    Json readJsonFile(const std::string& configFile);
+
+    /** @brief function to write json config file
+     *
+     *  @param[in] configFile - configuration file name
+     *  @param[in] jsonData - json object
+     *
+     *  @return 0 for success, -errno for failure.
+     */
+    int writeJsonFile(const std::string& configFile, const Json& jsonData);
+
+    /** @brief function to initialise CS Privilege Levels json config file
+     *
+     */
+    void initPrivilegeLevelsPersistData();
+};
+
+} // namespace ipmi
