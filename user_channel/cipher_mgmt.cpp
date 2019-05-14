8e179a2c641c96672f5156ebc0e727492fccc44f
Sumanth Bhat <sumanth.bhat@linux.intel.com>
2019-05-14 12:13:57 +0000

    Initialize Cipher Suite Privilege Levels cfg file
    IPMI Spec 13.17 provides a way to pick a "highest level matching
    proposed algorithms" during RMCPP Open Session Request, and effective
    privilege levels are decided taking Cipher Suite Privilege Levels into
    account. This patch ships default Cipher Suite Privilege Levels file
    from root filesystem. By default, admin privileges are
    given to all Cipher Suites across all channels.
    
    Tested:
    1. By default, cs_privilege_levels.json is present in location
       /usr/share/ipmi-providers/
    Provided support from below patch.
    https://git-amr-2.devtools.intel.com/gerrit/#/c/230725/
    2. File data is as expected and persistent across reboots.
    3. Manually deleted the json file while phosphor-ipmi-host was running
    and rebooted and the file was re-created.
    
    Change-Id: I526b8708f63659210768c77e8e19fa5a76df1f0d
    Signed-off-by: Sumanth Bhat <sumanth.bhat@linux.intel.com>
    Signed-off-by: vijayabharathix shetty <vijayabharathix.shetty@intel.com>

---
+++ user_channel/cipher_mgmt.cpp
@@ -0,0 +1,197 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "cipher_mgmt.hpp"
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <filesystem>
+#include <fstream>
+#include <include/ipmid/api-types.hpp>
+#include <phosphor-logging/log.hpp>
+
+namespace ipmi
+{
+
+using namespace phosphor::logging;
+namespace fs = std::filesystem;
+
+static std::array<std::string, PRIVILEGE_OEM + 1> privList = {
+    "priv-unspecified", "priv-callback", "priv-user",
+    "priv-operator",    "priv-admin",    "priv-oem"};
+
+CipherConfig& getCipherConfigObject(const std::string& csFileName)
+{
+    static CipherConfig cipherConfig(csFileName);
+    return cipherConfig;
+}
+
+CipherConfig::CipherConfig(const std::string& csFileName) :
+    cipherSuitePrivFileName(csFileName)
+{
+    if (!fs::exists(cipherSuitePrivFileName))
+    {
+        log<level::INFO>("CS privilege levels user settings file does not "
+                         "exist. Switching to default file...");
+        cipherSuitePrivFileName = csPrivDefaultFileName;
+    }
+    loadCSPrivilegesToMap();
+}
+
+void CipherConfig::loadCSPrivilegesToMap()
+{
+    if (!fs::exists(csPrivDefaultFileName))
+    {
+        log<level::ERR>("CS privilege levels default file does not exist...");
+    }
+    else
+    {
+
+        Json data = readCSPrivilegeLevels(csPrivDefaultFileName);
+
+        if (data != nullptr)
+        {
+            for (uint8_t chNum = 0; chNum < ipmi::maxIpmiChannels; chNum++)
+            {
+
+                std::string chKey = "Channel" + std::to_string(chNum);
+                for (uint8_t csNum = 0; csNum < maxCSRecords; csNum++)
+                {
+
+                    auto csKey = "CipherID" + std::to_string(csNum);
+
+                    csPrivilegeMap[{chNum, csNum}] = convertToPrivLimitIndex(
+                        static_cast<std::string>(data[chKey][csKey]));
+                }
+            }
+            if (fs::exists(csPrivFileName))
+            {
+                Json jsonData = readCSPrivilegeLevels(csPrivFileName);
+                if (jsonData != nullptr)
+                {
+                    updateCSPrivilegesMap(jsonData);
+                }
+            }
+        }
+    }
+}
+
+void CipherConfig::updateCSPrivilegesMap(const Json& jsonData)
+{
+
+    for (uint8_t chNum = 0; chNum < ipmi::maxIpmiChannels; chNum++)
+    {
+        std::string chKey = "Channel" + std::to_string(chNum);
+        for (uint8_t csNum = 0; csNum < maxCSRecords; csNum++)
+        {
+            auto csKey = "CipherID" + std::to_string(csNum);
+
+            if (jsonData.find(chKey) != jsonData.end())
+            {
+                csPrivilegeMap[{chNum, csNum}] = convertToPrivLimitIndex(
+                    static_cast<std::string>(jsonData[chKey][csKey]));
+            }
+        }
+    }
+}
+
+Json CipherConfig::readCSPrivilegeLevels(const std::string& csFileName)
+{
+    std::ifstream jsonFile(csFileName);
+    if (!jsonFile.good())
+    {
+        log<level::ERR>("JSON file not found");
+        return nullptr;
+    }
+
+    Json data = nullptr;
+    try
+    {
+        data = Json::parse(jsonFile, nullptr, false);
+    }
+    catch (Json::parse_error& e)
+    {
+        log<level::ERR>("Corrupted cipher suite privilege levels config file.",
+                        entry("MSG: %s", e.what()));
+
+        // TODO:Throw error which needs to be handled by the caller - Ex:
+        // Get/Set Privilege Levels. Currently don't throw to allow ipmid to
+        // continue
+    }
+
+    return data;
+}
+
+int CipherConfig::writeCSPrivilegeLevels(const Json& jsonData)
+{
+    std::string tmpFile =
+        static_cast<std::string>(cipherSuitePrivFileName) + "_tmpXXXXXX";
+
+    char tmpRandomfile[tmpFile.length() + 1];
+    strcpy(tmpRandomfile, tmpFile.c_str());
+
+    int fd = mkstemp(tmpRandomfile);
+    fchmod(fd, 0644);
+
+    if (fd < 0)
+    {
+        log<level::ERR>("Error opening CS privilege level config file",
+                        entry("FILE_NAME=%s", tmpFile.c_str()));
+        return -EIO;
+    }
+    const auto& writeData = jsonData.dump();
+    if (write(fd, writeData.c_str(), writeData.size()) !=
+        static_cast<ssize_t>(writeData.size()))
+    {
+        close(fd);
+        log<level::ERR>("Error writing CS privilege level config file",
+                        entry("FILE_NAME=%s", tmpFile.c_str()));
+        return -EIO;
+    }
+    close(fd);
+
+    if (std::rename(tmpRandomfile, cipherSuitePrivFileName.c_str()))
+    {
+        log<level::ERR>("Error renaming CS privilege level config file",
+                        entry("FILE_NAME=%s", tmpFile.c_str()));
+        return -EIO;
+    }
+
+    return 0;
+}
+
+uint8_t CipherConfig::convertToPrivLimitIndex(const std::string& value)
+{
+    auto iter = std::find(privList.begin(), privList.end(), value);
+    if (iter == privList.end())
+    {
+        log<level::ERR>("Invalid privilege.",
+                        entry("PRIV_STR=%s", value.c_str()));
+        return ccUnspecifiedError;
+    }
+
+    return static_cast<uint8_t>(std::distance(privList.begin(), iter));
+}
+
+std::string CipherConfig::convertToPrivLimitString(const uint8_t value)
+{
+    return privList.at(value);
+}
+
+} // namespace ipmi
