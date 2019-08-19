46c20b5ba23408a1856c4117629c2aaffa2fb6e7
Patrick Venture <venture@google.com>
2019-08-19 12:07:18 -0700

    sensorhandler: use entity-map from json if filled
    Step 4 of the transition from YAML to JSON will return the data from the
    JSON file if present and valid, otherwise it'll fallback and return the
    default example YAML present in the repository.
    
    Tested: This was not tested.
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: I33c773fc53660a9eb5e27a8c8c3e231c64fe079d

--- entity_map_json.hpp
+++ entity_map_json.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ipmid/types.hpp>
+#include <memory>
 #include <nlohmann/json.hpp>
 
 namespace ipmi
@@ -32,6 +33,26 @@ EntityInfoMap buildEntityMapFromFile();
  * @return the map
  */
 EntityInfoMap buildJsonEntityMap(const nlohmann::json& data);
+
+/**
+ * @brief Owner of the EntityInfoMap.
+ */
+class EntityInfoMapContainer
+{
+  public:
+    /** Get ahold of the owner. */
+    static EntityInfoMapContainer* getContainer();
+    /** Get ahold of the records. */
+    const EntityInfoMap& getIpmiEntityRecords();
+
+    static std::unique_ptr<EntityInfoMapContainer> instance;
+
+  private:
+    EntityInfoMapContainer(const EntityInfoMap& entities) : entities_(entities)
+    {
+    }
+    EntityInfoMap entities_;
+};
 
 } // namespace sensor
 } // namespace ipmi
