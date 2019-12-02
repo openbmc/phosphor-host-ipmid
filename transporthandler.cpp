4bb33e93484d74dd5b71696d7b8e113860bf66f5
jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
2019-12-02 17:29:09 +0000

    Added CipherSuite priv param-24 to set/get LanCfg
    IPMI Spec. 23.1(Set lan configuration) and 23.2(get lan configuration)
    refers to set and get (write and read) privileges through set and get
    lan configuration command using parameter 24 (0x18, RMCP+ Messaging
    Cipher Suite Privilege Levels).
    
    This patch allows us to create/update user setting Cipher Suite
    privilege levels file.
    
    This patch makes set/get Cipher Suite Privilege Level parameter data
    to be persistant. Supports set and get Cipher Suite privilege levels
    command for any channel.
    
    Tested:
    Set Lan config:
    Command: ipmitool raw 0x0c 0x01 0x03 0x18 0x00 0x34 0x43 0x44 0x33 0x44
             0x44 0x44 0x44
    Response:                       //Success
    
    Get Lan config:
    Command:  ipmitool  raw 0x0c 0x02 0x03 0x18 0x01 0x01
    Response: 11 00 34 43 44 33 44 44 44 44
    
    Signed-off-by: jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
    Change-Id: I1357af820bd1082bda208aef3918be8c31bf1af3

--- transporthandler.cpp
+++ transporthandler.cpp
@@ -1,4 +1,5 @@
 #include "app/channel.hpp"
+#include "user_channel/cipher_mgmt.hpp"
 
 #include <arpa/inet.h>
 #include <netinet/ether.h>
@@ -185,6 +186,7 @@ enum class LanParam : uint8_t
     VLANId = 20,
     CiphersuiteSupport = 22,
     CiphersuiteEntries = 23,
+    cipherSuitePrivilegeLevels = 24,
     IPFamilySupport = 50,
     IPFamilyEnables = 51,
     IPv6Status = 55,
@@ -1655,6 +1657,51 @@ RspType<> setLan(uint4_t channelBits, uint4_t, uint8_t parameter,
             // Accept any prefix value since our prefix length has to be 0
             return responseSuccess();
         }
+        case LanParam::cipherSuitePrivilegeLevels:
+        {
+            std::array<uint8_t, lanParamCipherSuitePrivilegeLevelsSize>
+                requestBytes;
+
+            if (req.unpack(requestBytes) || !req.fullyUnpacked())
+            {
+                return responseReqDataLenInvalid();
+            }
+            constexpr uint8_t reserved = 0;
+            if (requestBytes[reserved])
+            {
+                return responseInvalidFieldRequest();
+            }
+
+            constexpr uint8_t dataLowerMask = 0x0F;
+            constexpr uint8_t dataUpperMask = 0xF0;
+            constexpr uint8_t dataLowerMaxValue = 0x05;
+            constexpr uint8_t dataUpperMaxValue = 0x50;
+
+            for (auto& data : requestBytes)
+            {
+                if (((data & dataLowerMask) > dataLowerMaxValue) ||
+                    ((data & dataUpperMask) > dataUpperMaxValue))
+                {
+                    return responseInvalidFieldRequest();
+                }
+            }
+            uint8_t resp = getCipherConfigObject(csPrivFileName)
+                               .setCSPrivilegeLevels(channel, requestBytes);
+
+            if (!resp)
+            {
+                return responseSuccess();
+            }
+            else if (resp == ccInvalidFieldRequest)
+            {
+                req.trailingOk = true;
+                return responseInvalidFieldRequest();
+            }
+            else
+            {
+                return responseUnspecifiedError();
+            }
+        }
     }
 
     if ((parameter >= oemCmdStart) && (parameter <= oemCmdEnd))
@@ -1909,6 +1956,29 @@ RspType<message::Payload> getLan(uint4_t channelBits, uint3_t, bool revOnly,
             in6_addr prefix{};
             ret.pack(dataRef(prefix));
             return responseSuccess(std::move(ret));
+        }
+        case LanParam::cipherSuitePrivilegeLevels:
+        {
+            std::array<uint8_t, lanParamCipherSuitePrivilegeLevelsSize>
+                csPrivilegeLevels;
+
+            uint8_t resp =
+                getCipherConfigObject(csPrivFileName)
+                    .getCSPrivilegeLevels(channel, csPrivilegeLevels);
+
+            if (!resp)
+            {
+                ret.pack(csPrivilegeLevels);
+                return responseSuccess(std::move(ret));
+            }
+            else if (resp == ccInvalidFieldRequest)
+            {
+                return responseInvalidFieldRequest();
+            }
+            else
+            {
+                return responseUnspecifiedError();
+            }
         }
     }
 
