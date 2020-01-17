abd0e7311e84ab8211a6abc4dd944c749c2390d4
Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
2020-01-17 12:13:28 +0530

    [channel-mgmt]: Handle out of range error in path
    Function must assume that object path without any netork name
    may be advertised, and must handle the condition accordingly.
    
    Tested:
    1. Verified that ipmi user is properly enabled with channel
    based permission and it works fine.
    
    Change-Id: Id51f09bb7c19a9750cb06c2c55f7271afc6b1d2a
    Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>

--- user_channel/channel_mgmt.cpp
+++ user_channel/channel_mgmt.cpp
@@ -168,15 +168,15 @@ int ChannelConfig::convertToChannelNumberFromChannelName(
 
 std::string ChannelConfig::getChannelNameFromPath(const std::string& path)
 {
-    std::size_t pos = path.find(networkIntfObjectBasePath);
-    if (pos == std::string::npos)
+
+    static size_t length = strlen(networkIntfObjectBasePath);
+    if (path.compare(0, length, networkIntfObjectBasePath) ||
+        ((length + 1) >= path.size()))
     {
-        log<level::ERR>("Invalid interface path.",
-                        entry("PATH=%s", path.c_str()));
-        throw std::invalid_argument("Invalid interface path");
+        log<level::ERR>("Invalid object path.", entry("PATH=%s", path.c_str()));
+        throw std::invalid_argument("Invalid object path");
     }
-    std::string chName =
-        path.substr(pos + strlen(networkIntfObjectBasePath) + 1);
+    std::string chName(path, length + 1);
     return chName;
 }
 
