25a0f33ac8fee3bd8cc23a3a12719b8d4a10e095
srikanta mondal <srikantax.mondal@intel.com>
2020-04-09 23:02:41 +0000

    transporthandler: IPV6 address status as optional
    Issue: In setLanconfig, address status field is read-only parameter
           for IPv6 Static address.
    
    Fix: Make the address status field as Optional
    
    Tested:
    Before:
    Command: ipmitool raw 0x0c 0x01 0x01 0x38 0x00 0x80 0x0f 0xec 0x0d
             0xb8 0x00 0x00 0xf1 0x01 0x00 0x00 0x00 0x00 0x00 0x00 0x10
             0x52 0x40 0x06    //Set Lan Config for IPV6 static address
    Response:         // Success
    
    After:
    Command: ipmitool raw 0x0c 0x01 0x01 0x38 0x00 0x80 0x0f 0xec 0x0d
             0xb8 0x00 0x00 0xf1 0x01 0x00 0x00 0x00 0x00 0x00 0x00 0x10
             0x52 0x40 0x06    //Set Lan Config for IPV6 static address
    Response:         // Success
    
    Signed-off-by: srikanta mondal <srikantax.mondal@intel.com>
    Change-Id: I051e2835191295965ccc2c4613a8ed554e1d2b77

--- transporthandler.cpp
+++ transporthandler.cpp
@@ -1579,7 +1579,7 @@ RspType<> setLan(Context::ptr ctx, uint4_t channelBits, uint4_t reserved1,
             in6_addr ip;
             std::array<uint8_t, sizeof(ip)> ipbytes;
             uint8_t prefix;
-            uint8_t status;
+            std::optional<uint8_t> status;
             if (req.unpack(set, rsvd, enabled, ipbytes, prefix, status) != 0 ||
                 !req.fullyUnpacked())
             {
