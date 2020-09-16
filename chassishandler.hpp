03bcb65d30ec2df9c58181ddb020d74fdde33ae7
Kirill Pakhomov <k.pakhomov@yadro.com>
2020-09-16 20:06:45 +0300

    Refactor Chassis Control and State transitions
    Refactor the Chassis Control handler and state transition
    method to use the new DBus API and prepare Chassis Control
    handler for further improvements.
    
    Change-Id: Iff5d919e09ef2f7ea80042925604bc1f2bf33d41
    Signed-off-by: Kirill Pakhomov <k.pakhomov@yadro.com>

--- chassishandler.hpp
+++ chassishandler.hpp
@@ -3,6 +3,11 @@
 #include <stdint.h>
 
 #include <cstddef>
+#include <xyz/openbmc_project/State/Chassis/server.hpp>
+#include <xyz/openbmc_project/State/Host/server.hpp>
+
+// Phosphor State manager
+namespace State = sdbusplus::xyz::openbmc_project::State::server;
 
 // IPMI commands for Chassis net functions.
 enum ipmi_netfn_chassis_cmds
@@ -64,3 +69,95 @@ enum class ChassisIDState : uint8_t
     indefiniteOn = 0x2,
     reserved = 0x3
 };
+
+namespace ipmi
+{
+
+namespace softoff
+{
+static constexpr auto propRespReceived = "ResponseReceived";
+static constexpr auto valHostShutdown =
+    "xyz.openbmc_project.Ipmi.Internal."
+    "SoftPowerOff.HostResponse.HostShutdown";
+
+/** @brief Send a command to SoftPowerOff application to stop any timer
+ *
+ */
+bool stopSoftOffTimer();
+
+/** @brief Create file to indicate there is no need for
+ *         softoff notification to the host
+ *
+ */
+void indicateNoSoftoffNeeded();
+
+} // namespace softoff
+
+namespace chassis
+{
+// OpenBMC Chassis State Manager dbus framework
+static constexpr auto stateMgrRoot = "/xyz/openbmc_project/state/chassis0";
+static constexpr auto stateMgrServiceIface =
+    "xyz.openbmc_project.State.Chassis";
+static constexpr auto propReqTrans = "RequestedPowerTransition";
+static constexpr auto propCurrentState = "CurrentPowerState";
+static constexpr auto valStateOff =
+    "xyz.openbmc_project.State.Chassis.PowerState.Off";
+static constexpr auto valStateOn =
+    "xyz.openbmc_project.State.Chassis.PowerState.On";
+
+/** @brief Initialize the Chassis State transition
+ *
+ *  @param[in] bus - The bus used for calling the method
+ *  @param[in] transition - The state to perform transition to
+ *  @return On success returns True, on failure - False
+ */
+bool initStateTransition(State::Chassis::Transition transition);
+
+/** @brief Get the value of the Chassis State
+ *
+ *  @param[in] bus - The bus used for calling the method
+ *  @return On success returns the value of the property.
+ */
+std::string getChassisState();
+
+} // namespace chassis
+
+namespace host
+{
+// Since the timer is set to be periodic with 1sec interval,
+// introduce the timeout of 10 seconds
+static constexpr uint8_t TRANSITION_TIMEOUT = 10;
+static constexpr auto stateMgrRoot = "/xyz/openbmc_project/state/host0";
+static constexpr auto stateMgrServiceIface = "xyz.openbmc_project.State.Host";
+static constexpr auto propReqTrans = "RequestedHostTransition";
+static constexpr auto propCurrentState = "CurrentHostState";
+static constexpr auto valStateOff =
+    "xyz.openbmc_project.State.Host.HostState.Off";
+static constexpr auto valTransOff =
+    "xyz.openbmc_project.State.Host.Transition.Off";
+
+/** @brief Initialize the Host State transition
+ *
+ *  @param[in] bus - The bus used for calling the method
+ *  @param[in] transition - The state to perform transition to
+ *  @return On success returns True, on failure - False
+ */
+bool initStateTransition(State::Host::Transition transition);
+
+/** @brief Get the value of the Host State
+ *
+ *  @param[in] bus - The bus used for calling the method
+ *  @return On success returns the value of the property.
+ */
+std::string getHostState();
+
+/** @brief Get the value of the RequestedHostTransition
+ *
+ *  @param[in] bus - The bus used for calling the method
+ *  @return On success returns the value of the property.
+ */
+std::string getHostTransition();
+
+} // namespace host
+} // namespace ipmi
