87f9b000c76bf953c721ac75266dfee7293f9d0a
David Wang <davidwang@quantatw.com>
2021-04-16 00:25:50 +0800

    Revert "sel: Add OEM SEL Record data structure"
    This reverts commit af378fa3c2ae5b1f7c71f807cf7be2be58d8ec99.

--- storagehandler.cpp
+++ storagehandler.cpp
@@ -782,8 +782,7 @@ ipmi_ret_t getSELEntry(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
                     sizeof(record.nextRecordID));
         std::memcpy(static_cast<uint8_t*>(response) +
                         sizeof(record.nextRecordID),
-                    &record.event.eventRecord.recordID + requestData->offset,
-                    readLength);
+                    &record.recordID + requestData->offset, readLength);
         *data_len = sizeof(record.nextRecordID) + readLength;
     }
 
