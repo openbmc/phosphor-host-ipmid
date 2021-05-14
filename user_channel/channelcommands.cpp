9f00bd07e1ebe6ef487514f89e53f48ca4370c7b
Andrew Geissler <geissonator@yahoo.com>
2021-05-14 14:18:36 -0500

    cast enum to int to satisfy new gcc version
    The latest gcc complains about this code with the following:
      180 |     if ((accessSetMode == doNotSet) || (accessSetMode == reserved))
          |          ~~~~~~~~~~~~~ ^~ ~~~~~~~~
          |          uint2_t {aka boost::multiprecision::number<boost::multiprecision::backends::cpp_int_backend<2, 2, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>}
    ../../../../../../workspace/sources/phosphor-ipmi-host/user_channel/channelcommands.cpp:180:24: note: candidate: 'operator==(int, int)' (built-in)
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Change-Id: I1f05f852f420e8d36dcc9afbde9afdb17462295a

--- user_channel/channelcommands.cpp
+++ user_channel/channelcommands.cpp
@@ -171,13 +171,18 @@ ipmi ::RspType<uint3_t, // access mode,
     ipmiGetChannelAccess(Context::ptr ctx, uint4_t channel, uint4_t reserved1,
                          uint6_t reserved2, uint2_t accessSetMode)
 {
+    // Create EChannelActionType for comparison logic in this function
+    EChannelActionType accessSetModeEChannel =
+        static_cast<EChannelActionType>(accessSetMode);
+
     if (reserved1 || reserved2)
     {
         log<level::DEBUG>("Get channel access - Invalid field in request");
         return responseInvalidFieldRequest();
     }
 
-    if ((accessSetMode == doNotSet) || (accessSetMode == reserved))
+    if ((accessSetModeEChannel == doNotSet) ||
+        (accessSetModeEChannel == reserved))
     {
         log<level::DEBUG>("Get channel access - Invalid Access mode");
         return responseInvalidFieldRequest();
@@ -197,11 +202,11 @@ ipmi ::RspType<uint3_t, // access mode,
 
     Cc compCode;
 
-    if (accessSetMode == nvData)
+    if (accessSetModeEChannel == nvData)
     {
         compCode = getChannelAccessPersistData(chNum, chAccess);
     }
-    else if (accessSetMode == activeData)
+    else if (accessSetModeEChannel == activeData)
     {
         compCode = getChannelAccessData(chNum, chAccess);
     }
