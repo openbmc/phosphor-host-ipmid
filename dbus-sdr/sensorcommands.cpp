597aa850cdd9c28d6353f8246ded9aadade35958
Chalapathi Venkataramashetty <chalapathix.venkataramashetty@intel.com>
2021-06-13 19:51:17 +0000

    sensorcommands: check for reserved sensor number in sensor commands.
    Add support to check for reserved sensor number (0xFF) in sensor
    commands.
    
    Tested:
    Command: Set Sensor Threshold
    ipmitool raw 0x04 0x26 0xff 0x00 0x3a 0xab 0x03 0x02 0xef 0x63
    Response: Unable to send RAW command (channel=0x0 netfn=0x4 lun=0x0
    cmd=0x26 rsp=0xcc): Invalid data field in request
    
    Ported From:
    https://gerrit.openbmc-project.xyz/c/openbmc/intel-ipmi-oem/+/44032
    
    Signed-off-by: Chalapathi Venkataramashetty <chalapathix.venkataramashetty@intel.com>
    Change-Id: Id99f5ef1515263ac3dbf2edec7f94b0b4badb4e3
    Signed-off-by: Willy Tu <wltu@google.com>

--- dbus-sdr/sensorcommands.cpp
+++ dbus-sdr/sensorcommands.cpp
@@ -640,6 +640,11 @@ ipmi::RspType<uint8_t, uint8_t, uint8_t, std::optional<uint8_t>>
     std::string connection;
     std::string path;
 
+    if (sensnum == reservedSensorNumber)
+    {
+        return ipmi::responseInvalidFieldRequest();
+    }
+
     auto status = getSensorConnection(ctx, sensnum, connection, path);
     if (status)
     {
@@ -865,7 +870,9 @@ ipmi::RspType<> ipmiSenSetSensorThresholds(
     uint8_t upperNonCritical, uint8_t upperCritical,
     uint8_t upperNonRecoverable)
 {
-    if (reserved)
+    constexpr uint8_t thresholdMask = 0xFF;
+
+    if (sensorNum == reservedSensorNumber || reserved)
     {
         return ipmi::responseInvalidFieldRequest();
     }
@@ -1087,6 +1094,11 @@ ipmi::RspType<uint8_t, // readable
     std::string connection;
     std::string path;
 
+    if (sensorNumber == reservedSensorNumber)
+    {
+        return ipmi::responseInvalidFieldRequest();
+    }
+
     auto status = getSensorConnection(ctx, sensorNumber, connection, path);
     if (status)
     {
@@ -1174,6 +1186,11 @@ ipmi::RspType<uint8_t, // enabled
     uint8_t assertionEnabledMsb = 0;
     uint8_t deassertionEnabledLsb = 0;
     uint8_t deassertionEnabledMsb = 0;
+
+    if (sensorNum == reservedSensorNumber)
+    {
+        return ipmi::responseInvalidFieldRequest();
+    }
 
     auto status = getSensorConnection(ctx, sensorNum, connection, path);
     if (status)
