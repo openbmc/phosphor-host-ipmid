ca2c3d3738f1a754fe311972314a64aa7c745194
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-04-19 22:13:48 +0800

    transporthandler: Fix get/setGatewayProperty for VLAN interfaces
    Current implementation is unable to get/set default gateway on VLAN
    interfaces as it uses the physical interface name to build object path.
    The logical interface path of LAN channel should be used to support
    operating on VLAN interfaces.
    
    Tested:
    When VLAN is enabled, get/set both IPv4 and IPv6 gateway uses the value
    of VLAN interface instead of its parent interface. The behavior when
    VLAN is not enabled remains unchanged.
    
    Change-Id: I055c1950a74a5c587344e0183563cdd8963c987d
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

--- transporthandler.hpp
+++ transporthandler.hpp
@@ -595,9 +595,8 @@ template <int family>
 std::optional<typename AddrFamily<family>::addr>
     getGatewayProperty(sdbusplus::bus_t& bus, const ChannelParams& params)
 {
-    auto objPath = "/xyz/openbmc_project/network/" + params.ifname;
     auto gatewayStr = std::get<std::string>(
-        getDbusProperty(bus, params.service, objPath, INTF_ETHERNET,
+        getDbusProperty(bus, params.service, params.logicalPath, INTF_ETHERNET,
                         AddrFamily<family>::propertyGateway));
     if (gatewayStr.empty())
     {
@@ -686,8 +685,7 @@ void setGatewayProperty(sdbusplus::bus_t& bus, const ChannelParams& params,
         neighbor = findStaticNeighbor<family>(bus, params, *gateway, neighbors);
     }
 
-    auto objPath = "/xyz/openbmc_project/network/" + params.ifname;
-    setDbusProperty(bus, params.service, objPath, INTF_ETHERNET,
+    setDbusProperty(bus, params.service, params.logicalPath, INTF_ETHERNET,
                     AddrFamily<family>::propertyGateway,
                     addrToString<family>(address));
 
