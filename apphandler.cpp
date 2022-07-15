61880885c96da8db8bad2ef97f837c7398770b18
Brian Ma <chma0@nuvoton.com>
2022-07-15 09:34:07 +0800

    Fix get session info return wrong session handle
    Test:
    ~$ ipmitool -I lanplus -H 10.103.152.117 -U root -P 0penBmc sol activate
    ~$ ipmitool -I lanplus -H 10.103.152.147 -U root -P 0penBmc session  info all
    
    Signed-off-by: Brian Ma <chma0@nuvoton.com>
    Change-Id: I8a6b29783ea26ff92c4f8cd73bbcef1a70e10c14

--- apphandler.cpp
+++ apphandler.cpp
@@ -1275,6 +1275,7 @@ ipmi::RspType<uint8_t, // session handle,
             return ipmi::response(completionCode);
         }
         maybeDetails = std::move(details);
+        break;
     }
 
     if (state == static_cast<uint8_t>(session::State::active) ||
