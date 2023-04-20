9e77eb179814f5d430787266ed68163b3185e9da
John Wedig <johnwedig@google.com>
2023-04-20 16:28:15 -0700

    Fix whitelist generation with multiple files
    The generate_whitelist_create.sh script currently only works with a
    single input file. If you try to pass in multiple files, sed gets
    confused and thinks those multiple filenames are together the name of
    a single file. The end result is the creation of an empty whitelist.
    
    To fix this, we need to use $@ instead of "$*" with sed, so that the
    filenames get passed as separate arguments.
    
    Tested:
    Confirmed that the generated ipmiwhitelist.cpp file is not empty when
    multiple input files are used.
    
    Change-Id: I18204c05c637ae110f41fd5287d9a33d2b2019f2
    Signed-off-by: John Wedig <johnwedig@google.com>

--- generate_whitelist.sh
+++ generate_whitelist.sh
@@ -19,7 +19,7 @@ EOF
 # Sort the list [numerically].
 # Remove any duplicates.
 # Turn "a:b //<NetFn>:<Command>" -> "{ a, b }, //<NetFn>:<Command>"
-sed "s/#.*//" "$*" | sed '/^$/d' | sort -n | uniq | sed "s/^/    { /" | \
+sed "s/#.*//" $@ | sed '/^$/d' | sort -n | uniq | sed "s/^/    { /" | \
     sed "s/\:\(....\)\(.*\)/ , \1 }, \2/"
 
 cat << EOF
