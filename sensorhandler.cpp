408c1fc76072da335003960576c704ec6ddfde07
Jian Zhang <zhangjian.3032@bytedance.com>
2023-12-26 16:58:34 +0800

    sdr: add length validation for request
    Avoid core dump and unexpected behavior when the request length is
    invalid.
    
    Tested:
    Without coredump when use `ipmitool raw 0x0a 0x23` to get sdr
    when the request length is 0.
    
    Change-Id: Ieac68e1ab1f5ee23fcef365204db4138291b285d
    Signed-off-by: Jian Zhang <zhangjian.3032@bytedance.com>

--- sensorhandler.cpp
+++ sensorhandler.cpp
@@ -1335,6 +1335,11 @@ ipmi_ret_t ipmi_sen_get_sdr(ipmi_netfn_t, ipmi_cmd_t, ipmi_request_t request,
     get_sdr::GetSdrReq* req = (get_sdr::GetSdrReq*)request;
     get_sdr::GetSdrResp* resp = (get_sdr::GetSdrResp*)response;
 
+    if (*data_len != sizeof(get_sdr::GetSdrReq))
+    {
+        return IPMI_CC_REQ_DATA_LEN_INVALID;
+    }
+
     // Note: we use an iterator so we can provide the next ID at the end of
     // the call.
     auto sensor = ipmi::sensor::sensors.begin();
