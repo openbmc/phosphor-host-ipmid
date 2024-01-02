3af66b0b9af52834e6fe7d020a339c04bd926566
Alexander Hansen <alexander.hansen@9elements.com>
2024-01-02 10:45:58 +0100

    fix race condition with vector 'restrictedMode'
    There were cases where 'restrictedMode' has been accessed
    at indices where there is no value.
    
    Example 1: (speculative, did not test)
    
    WhitelistFilter::cacheRestrictedMode would assign to an index in
    restrictedMode which would not have existed unless at least 1 previous
    call to WhitelistFilter::cacheRestrictedMode had been successful
    (calling emplace_back)
    
    Example 2: (actually occurs)
    
    WhitelistFilter::filterMessage could access 'restrictedMode' at an index
    without 'cacheRestrictedMode' having been called first.
    
    This can be observed when doing ipmi requests while the bmc is
    rebooting. There is a timespan in which the collection is not
    initialized and the process segfaults.
    
    Proposed solution: Use getter/setter to access 'restrictedMode'.
    
    Tested: during my testing, making requests during initialization did no
    longer result in segfaults.
    
    Change-Id: I9aff346579c557513083b3a3a5b7bc5a96c9315c
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- whitelist-filter.cpp
+++ whitelist-filter.cpp
@@ -9,6 +9,7 @@
 
 #include <algorithm>
 #include <array>
+#include <cstddef>
 
 using namespace phosphor::logging;
 using namespace sdbusplus::error::xyz::openbmc_project::common;
@@ -42,6 +43,9 @@ class AllowlistFilter
         sdbusplus::message_t& m,
         const std::map<std::string, size_t>& deviceList);
     ipmi::Cc filterMessage(ipmi::message::Request::ptr request);
+
+    bool getRestrictedMode(int index);
+    void setRestrictedMode(int index, bool value);
 
     std::vector<bool> restrictedMode;
     std::shared_ptr<sdbusplus::asio::connection> bus;
@@ -101,7 +105,7 @@ void AllowlistFilter::cacheRestrictedMode(
             {
                 log<level::ERR>("Error in RestrictionMode Get");
                 // Fail-safe to true.
-                restrictedMode[index] = true;
+                setRestrictedMode(index, true);
                 return;
             }
 
@@ -111,7 +115,7 @@ void AllowlistFilter::cacheRestrictedMode(
 
             bool restrictMode =
                 (restrictionMode == RestrictionMode::Modes::Whitelist);
-            restrictedMode.emplace_back(restrictMode);
+            setRestrictedMode(index, restrictMode);
 
             log<level::INFO>((restrictMode ? "Set restrictedMode = true"
                                            : "Set restrictedMode = false"));
@@ -158,7 +162,7 @@ void AllowlistFilter::handleRestrictedModeChange(
                     std::get<std::string>(property.second));
             bool restrictMode =
                 (restrictionMode == RestrictionMode::Modes::Whitelist);
-            restrictedMode[hostId] = restrictMode;
+            setRestrictedMode(hostId, restrictMode);
             log<level::INFO>((restrictMode ? "Updated restrictedMode = true"
                                            : "Updated restrictedMode = false"));
         }
@@ -229,7 +233,7 @@ ipmi::Cc AllowlistFilter::filterMessage(ipmi::message::Request::ptr request)
     size_t hostIdx = request->ctx->hostIdx;
 
     if (request->ctx->channel == ipmi::channelSystemIface &&
-        restrictedMode[hostIdx])
+        getRestrictedMode(hostIdx))
     {
         if (!std::binary_search(
                 allowlist.cbegin(), allowlist.cend(),
@@ -243,6 +247,24 @@ ipmi::Cc AllowlistFilter::filterMessage(ipmi::message::Request::ptr request)
         }
     }
     return ipmi::ccSuccess;
+}
+
+bool AllowlistFilter::getRestrictedMode(int index)
+{
+    if (restrictedMode.size() <= (size_t)index)
+    {
+        restrictedMode.resize(index + 1, false);
+    }
+    return restrictedMode[index];
+}
+
+void AllowlistFilter::setRestrictedMode(int index, bool value)
+{
+    if (restrictedMode.size() <= (size_t)index)
+    {
+        restrictedMode.resize(index + 1, false);
+    }
+    restrictedMode[index] = value;
 }
 
 // instantiate the AllowlistFilter when this shared object is loaded
