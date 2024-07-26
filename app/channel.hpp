e660592d357525d7bde728bc7f176aac15268459
George Liu <liuxiwei@ieisystem.com>
2024-07-26 16:58:04 +0800

    app: remove unused commands
    Since the implementations of commands ipmi_app_channel_info,
    ipmi_get_channel_access, and ipmi_set_channel_access have been
    removed in a previous patch [1], this commit removes the
    declarations in hpp.
    
    [1]: https://github.com/openbmc/phosphor-host-ipmid/commit/d2a57decf383eb3faf3587739ad0b3181b7bb2d6
    
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>
    Change-Id: Ice8b824abba4126d373cdfcf63364b6257d045ac

--- app/channel.hpp
+++ app/channel.hpp
@@ -2,57 +2,6 @@
 
 #include <ipmid/api.hpp>
 
-/** @brief The set channel access IPMI command.
- *
- *  @param[in] netfn
- *  @param[in] cmd
- *  @param[in] request
- *  @param[in,out] response
- *  @param[out] data_len
- *  @param[in] context
- *
- *  @return IPMI_CC_OK on success, non-zero otherwise.
- */
-ipmi_ret_t ipmi_set_channel_access(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
-                                   ipmi_request_t request,
-                                   ipmi_response_t response,
-                                   ipmi_data_len_t data_len,
-                                   ipmi_context_t context);
-
-/** @brief The get channel access IPMI command.
- *
- *  @param[in] netfn
- *  @param[in] cmd
- *  @param[in] request
- *  @param[in,out] response
- *  @param[out] data_len
- *  @param[in] context
- *
- *  @return IPMI_CC_OK on success, non-zero otherwise.
- */
-ipmi_ret_t ipmi_get_channel_access(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
-                                   ipmi_request_t request,
-                                   ipmi_response_t response,
-                                   ipmi_data_len_t data_len,
-                                   ipmi_context_t context);
-
-/** @brief The get channel info IPMI command.
- *
- *  @param[in] netfn
- *  @param[in] cmd
- *  @param[in] request
- *  @param[in,out] response
- *  @param[out] data_len
- *  @param[in] context
- *
- *  @return IPMI_CC_OK on success, non-zero otherwise.
- */
-ipmi_ret_t ipmi_app_channel_info(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
-                                 ipmi_request_t request,
-                                 ipmi_response_t response,
-                                 ipmi_data_len_t data_len,
-                                 ipmi_context_t context);
-
 /** @brief this command is used to look up what authentication, integrity,
  *  confidentiality algorithms are supported.
  *
