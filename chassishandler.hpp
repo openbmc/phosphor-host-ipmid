87993931417bcbe234337b6b7b11737118b448d7
George Liu <liuxiwei@ieisystem.com>
2024-12-05 16:57:51 +0800

    chassishandler: remove IPMI_CMD command
    The IPMI_CMD declared in chassishandler.hpp is redundant because
    these commands are already declared in api-types.hpp, so this commit
    removes all IPMI_CMD in chassishandler.hpp.
    
    Change-Id: I2ec33b4b6df7cad1585f3be829386dd25ea6da80
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>

--- chassishandler.hpp
+++ chassishandler.hpp
@@ -4,22 +4,6 @@
 
 #include <cstddef>
 
-// IPMI commands for Chassis net functions.
-enum ipmi_netfn_chassis_cmds
-{
-    IPMI_CMD_GET_CHASSIS_CAP = 0x00,
-    // Chassis Status
-    IPMI_CMD_CHASSIS_STATUS = 0x01,
-    // Chassis Control
-    IPMI_CMD_CHASSIS_CONTROL = 0x02,
-    IPMI_CMD_CHASSIS_IDENTIFY = 0x04,
-    IPMI_CMD_SET_CHASSIS_CAP = 0x05,
-    // Get capability bits
-    IPMI_CMD_SET_SYS_BOOT_OPTIONS = 0x08,
-    IPMI_CMD_GET_SYS_BOOT_OPTIONS = 0x09,
-    IPMI_CMD_GET_POH_COUNTER = 0x0F,
-};
-
 // Command specific completion codes
 enum ipmi_chassis_return_codes
 {
