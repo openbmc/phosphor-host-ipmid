5f9ed924aa40bd65836888992acc61c3f5dd643b
Jayanth Othayoth <ojayanth@gmail.com>
2024-12-08 09:33:58 -0600

    clang-tidy: Fix packed member address warning
    The following error was reported during clang-tidy enablement due to
    taking the address of a packed member
    
    '''
    ../apphandler.cpp:637:31:
      error: taking address of packed member 'recordID' of class or structure 'ipmi::sel::SELEventRecord' may result in an unaligned pointer value [-Werror]
    '''
    
    Fix is to Copy the packed member to a temporary variable before using
    its address. This ensures proper alignment.
    
    Tested: Build and unit testing verified.
    
    Change-Id: Ib547c1764ec4402a18851b8130f06e60cb66dae3
    Signed-off-by: Jayanth Othayoth <ojayanth@gmail.com>

--- storagehandler.cpp
+++ storagehandler.cpp
@@ -21,6 +21,7 @@
 
 #include <algorithm>
 #include <chrono>
+#include <cstdint>
 #include <cstdio>
 #include <cstring>
 #include <filesystem>
@@ -378,9 +379,11 @@ ipmi_ret_t getSELEntry(ipmi_netfn_t, ipmi_cmd_t, ipmi_request_t request,
 
         std::memcpy(response, &record.nextRecordID,
                     sizeof(record.nextRecordID));
+        auto tempRecordID = htole16(record.event.eventRecord.recordID);
         std::memcpy(static_cast<uint8_t*>(response) +
                         sizeof(record.nextRecordID),
-                    &record.event.eventRecord.recordID + requestData->offset,
+                    reinterpret_cast<const uint8_t*>(&tempRecordID) +
+                        requestData->offset,
                     readLength);
         *data_len = sizeof(record.nextRecordID) + readLength;
     }
