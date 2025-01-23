d0886e260912f8612e4e237973d43419dbeed5d5
John Chung <john.chung@arm.com>
2025-01-22 18:07:52 -0600

    SBMR Get/Send Boot Progress Code support
    Implement IPMI commands for boot progress codes by following
    DEN0069E_SBMR_2.1. Add configure option `arm-sbmr` to enable
    SBMR IPMI commands. Boot progress code will update to Redfish
    BootProgress property.
    
    1. Send boot progress code (NetFn 0x2C, Command 0x2)
    2. Get boot progress code  (NetFn 0x2C, Command 0x3)
    
    Test:
    
    1. Send boot progress code
       $> ipmitool raw 0x2C 0x02 0xAE 0x1 0x00 0x00 0x00 \
          0x01 0x10 0x01 0x02 0x00
          ae
    
    2. Get boot progress code
       $> ipmitool raw 0x2C 0x3 0xAE
          ae 01 00 00 00 01 10 01 02 00
    
    3. Redfish BootProgess LastState - /redfish/v1/Systems/system
       {
         ...
         "BootProgress": {
           "LastState": "PCIResourceConfigStarted",
           "LastStateTime": "2024-11-14T19:14:22.432272+00:00"
         }
       }
    
    Signed-off-by: John Chung <john.chung@arm.com>
    Change-Id: I58e6e322006039fceb8d4212c4f9f6a4b4f9e225

--- include/ipmid/api-types.hpp
+++ include/ipmid/api-types.hpp
@@ -31,6 +31,9 @@ constexpr Group groupPICMG = 0x00;
 constexpr Group groupDMTG = 0x01;
 constexpr Group groupSSI = 0x02;
 constexpr Group groupVSO = 0x03;
+#ifdef ARM_SBMR_SUPPORT
+constexpr Group groupSBMR = 0xAE;
+#endif
 constexpr Group groupDCMI = 0xDC;
 
 /*
@@ -326,6 +329,14 @@ constexpr Cmd cmdGetTemperatureReadings = 0x10;
 constexpr Cmd cmdSetDcmiConfigParameters = 0x12;
 constexpr Cmd cmdGetDcmiConfigParameters = 0x13;
 } // namespace dcmi
+
+#ifdef ARM_SBMR_SUPPORT
+namespace sbmr
+{
+constexpr Cmd cmdSendBootProgressCode = 0x02;
+constexpr Cmd cmdGetBootProgressCode = 0x03;
+} // namespace sbmr
+#endif
 
 // These are the command network functions, the response
 // network functions are the function + 1. So to determine
