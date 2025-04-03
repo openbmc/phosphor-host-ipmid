535fc0c925b71a358be1d35a373ca9f508ee71d7
George Liu <liuxiwei@ieisystem.com>
2025-04-03 09:16:27 +0800

    api: remove ipmid_get_sd_event_connection
    The ipmid_get_sd_event_connection method is no longer used, this
    commit removes it.
    
    Change-Id: I6b2fae45407a9d175321f8107c83c1955ac63373
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>

--- include/ipmid/api.h
+++ include/ipmid/api.h
@@ -141,7 +141,6 @@ EInterfaceIndex getInterfaceIndex(void);
 
 sd_bus* ipmid_get_sd_bus_connection(void);
 sd_event* ipmid_get_sd_event_connection(void);
-sd_bus_slot* ipmid_get_sd_bus_slot(void);
 
 // move this from ipmid.hpp, which is now gone
 // this should not be used. Use the channel API to get the channel size
