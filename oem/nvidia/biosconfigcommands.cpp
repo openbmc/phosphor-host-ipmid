0eba36a71fb731acdcd9a677db1e370262b3a1fc
Prithvi Pai <ppai@nvidia.com>
2025-06-05 11:13:34 +0530

    Add in-tree nvidia-ipmi-oem provider
    This patch adds nvidia-ipmi-oem. The current patch adds support
    to 2 commands:
    1) GET BIOS PASSWORD
    2) SET BIOS PASSWORD
    
    Tested:
    1) Tested on patch
    https://gerrit.openbmc.org/c/openbmc/openbmc/+/80748
    
    Change-Id: I48599a1b85a3a614c56e7687b8646f7dfe1e7bb1
    Signed-off-by: Prithvi Pai <ppai@nvidia.com>

---
+++ oem/nvidia/biosconfigcommands.cpp
@@ -0,0 +1,166 @@
+#include "config.h"
+
+#include "oemcommands.hpp"
+
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/sha.h>
+
+#include <ipmid/api.hpp>
+#include <ipmid/types.hpp>
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/lg2.hpp>
+
+#include <array>
+#include <cstdint>
+#include <fstream>
+#include <string>
+#include <vector>
+
+constexpr char biosPasswordFilePath[] =
+    "/var/lib/bios-settings-manager/seedData";
+constexpr int biosPasswordIter = 1000;
+constexpr uint8_t biosPasswordSaltSize = 32;
+constexpr uint8_t biosPasswordMaxHashSize = 64;
+constexpr uint8_t biosPasswordTypeNoChange = 0x00;
+constexpr uint8_t biosPasswordSelectorAdmin = 0x01;
+constexpr uint8_t biosPasswordTypeNoPassowrd = 0x01;
+constexpr uint8_t biosPasswordTypePbkdf2Sha256 = 0x02;
+constexpr uint8_t biosPasswordTypePbkdf2Sha384 = 0x03;
+
+void registerBiosConfigCommands() __attribute__((constructor));
+
+namespace ipmi
+{
+ipmi::RspType<std::vector<uint8_t>> ipmiSetBiosPassword(
+    uint8_t id, uint8_t type, std::array<uint8_t, biosPasswordSaltSize> salt,
+    std::array<uint8_t, biosPasswordMaxHashSize> hash)
+{
+    nlohmann::json json;
+
+    if (id != biosPasswordSelectorAdmin)
+    {
+        return ipmi::responseInvalidFieldRequest();
+    }
+    switch (type)
+    {
+        case biosPasswordTypeNoPassowrd:
+            json["HashAlgo"] = "SHA256";
+            RAND_bytes(salt.data(), salt.size());
+            // password is only Null-terminated character
+            PKCS5_PBKDF2_HMAC("", 1, salt.data(), salt.size(), biosPasswordIter,
+                              EVP_sha256(), SHA256_DIGEST_LENGTH, hash.data());
+            json["Seed"] = salt;
+            json["AdminPwdHash"] = hash;
+            break;
+        case biosPasswordTypePbkdf2Sha256:
+            json["HashAlgo"] = "SHA256";
+            json["Seed"] = salt;
+            json["AdminPwdHash"] = hash;
+            break;
+        case biosPasswordTypePbkdf2Sha384:
+            json["HashAlgo"] = "SHA384";
+            json["Seed"] = salt;
+            json["AdminPwdHash"] = hash;
+            break;
+        default:
+            return ipmi::responseInvalidFieldRequest();
+    }
+    json["IsAdminPwdChanged"] = false;
+    json["IsUserPwdChanged"] = false;
+
+    // initializing with 0 as user password hash field
+    // is not used presently
+    constexpr std::array<uint8_t, biosPasswordMaxHashSize> userPwdHash = {0};
+    json["UserPwdHash"] = userPwdHash;
+
+    try
+    {
+        std::ofstream ofs(biosPasswordFilePath, std::ios::out);
+        const auto& writeData = json.dump();
+        ofs << writeData;
+        ofs.close();
+    }
+    catch (std::exception& e)
+    {
+        lg2::error("Failed to save BIOS Password information: {ERROR}", "ERROR",
+                   e.what());
+        return ipmi::responseUnspecifiedError();
+    }
+    return ipmi::responseSuccess();
+}
+
+ipmi::RspType<uint8_t, std::array<uint8_t, biosPasswordSaltSize>,
+              std::array<uint8_t, biosPasswordMaxHashSize>>
+    ipmiGetBiosPassword(uint8_t id)
+{
+    uint8_t action = biosPasswordTypeNoChange;
+    std::array<uint8_t, biosPasswordSaltSize> salt = {0};
+    std::array<uint8_t, biosPasswordMaxHashSize> hash = {0};
+
+    if (id != biosPasswordSelectorAdmin)
+    {
+        return ipmi::responseParmOutOfRange();
+    }
+
+    std::ifstream ifs(biosPasswordFilePath);
+    if (!ifs.is_open())
+    {
+        // return No change if no file
+        return ipmi::responseSuccess(action, salt, hash);
+    }
+
+    nlohmann::json json = nlohmann::json::parse(ifs, nullptr, false);
+    if (json.is_discarded() || !json.contains("IsAdminPwdChanged") ||
+        !json.contains("HashAlgo") || !json.contains("Seed") ||
+        !json.contains("AdminPwdHash"))
+    {
+        return ipmi::responseResponseError();
+    }
+    bool IsAdminPwdChanged = json["IsAdminPwdChanged"];
+    if (IsAdminPwdChanged == false)
+    {
+        return ipmi::responseSuccess(action, salt, hash);
+    }
+
+    salt = json["Seed"];
+    hash = json["AdminPwdHash"];
+
+    std::string HashAlgo = json["HashAlgo"];
+    auto digest = EVP_sha256();
+    int keylen = SHA256_DIGEST_LENGTH;
+
+    if (HashAlgo == "SHA256")
+    {
+        action = biosPasswordTypePbkdf2Sha256;
+    }
+    else if (HashAlgo == "SHA384")
+    {
+        action = biosPasswordTypePbkdf2Sha384;
+        digest = EVP_sha384();
+        keylen = SHA384_DIGEST_LENGTH;
+    }
+
+    std::array<uint8_t, biosPasswordMaxHashSize> nullHash = {0};
+    PKCS5_PBKDF2_HMAC("", 1, salt.data(), salt.size(), biosPasswordIter, digest,
+                      keylen, nullHash.data());
+    if (hash == nullHash)
+    {
+        action = biosPasswordTypeNoPassowrd;
+        salt.fill(0x00);
+        hash.fill(0x00);
+    }
+
+    return ipmi::responseSuccess(action, salt, hash);
+}
+} // namespace ipmi
+
+void registerBiosConfigCommands()
+{
+    ipmi::registerHandler(ipmi::prioOemBase, ipmi::groupNvidia,
+                          ipmi::bios_password::cmdSetBiosPassword,
+                          ipmi::Privilege::Admin, ipmi::ipmiSetBiosPassword);
+    ipmi::registerHandler(ipmi::prioOemBase, ipmi::groupNvidia,
+                          ipmi::bios_password::cmdGetBiosPassword,
+                          ipmi::Privilege::Admin, ipmi::ipmiGetBiosPassword);
+}
