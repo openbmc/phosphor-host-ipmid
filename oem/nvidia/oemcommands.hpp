ff35515db2f0d990b34e185c7c0575b64bc31769
Prithvi Pai <ppai@nvidia.com>
2025-07-19 13:57:19 +0530

    nvidia-oem: Add USBDescription command
    This command is used by BIOS before creating SMBIOS record
    type 42. BIOS gets USBDescription information from BMC.
    
    Design Document for RHI:
    [1] https://gerrit.openbmc.org/c/openbmc/docs/+/79327
    
    Redfish Host Interface specification:
    [2] https://www.dmtf.org/sites/default/files/standards/documents/DSP0270_1.3.0.pdf
    
    Tested:
    Tested on gb200nvl-obmc platform
    
    Change-Id: I6a325ca3410e2ac2ab0b8c3776944d31f3cac582
    Signed-off-by: Prithvi Pai <ppai@nvidia.com>

--- oem/nvidia/oemcommands.hpp
+++ oem/nvidia/oemcommands.hpp
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #pragma once
 #include <cstdint>
 
@@ -7,9 +13,14 @@ namespace ipmi
 using Group = uint8_t;
 constexpr Group groupNvidia = 0x3C;
 
-namespace bios_password
+namespace bootstrap_credentials_oem
+{
+constexpr auto cmdGetUsbDescription = 0x30;
+} // namespace bootstrap_credentials_oem
+
+namespace bios_password_oem
 {
 constexpr auto cmdSetBiosPassword = 0x36;
 constexpr auto cmdGetBiosPassword = 0x37;
-} // namespace bios_password
+} // namespace bios_password_oem
 } // namespace ipmi
