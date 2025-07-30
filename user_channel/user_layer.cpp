3b35e3f3c36a9342b2a00bcc609a0b861809eb46
Thu Nguyen <thu@os.amperecomputing.com>
2025-07-30 11:42:04 +0000

    user_channel: Add APIs to manage None IPMI group user
    Ipmid does not manage the list of None IPMI group users. But with
    command `GetBootStrapAccount` as [1], it can be used to create the none
    Ipmi group users. Add APIs to manage list of user name with None IPMI
    group.
    
    [1] https://gerrit.openbmc.org/c/openbmc/docs/+/79327
    
    Tested:
    1. Use Ipmi `ipmitool raw 0x2c 0x2 0x52 0xa5` to create 15 bootStrap
    account.
    2. Create the 16th bootStrap account should be failed because system
    only support 15 none IPMI Group account.
    3. Remove one bootStrap account
    4. Create new BootStrap account should be passed.
    5. Update UserGroup of one bootStrap account, add `ipmi` group. Create
    new BootStrap account should be passed.
    
    Change-Id: I7e3f6ab9b6f37c45b55527cd5fa737bc693099b2
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
    Signed-off-by: Prithvi Pai <prithvi24pai@gmail.com>

--- user_channel/user_layer.cpp
+++ user_channel/user_layer.cpp
@@ -250,4 +250,20 @@ Cc ipmiUserSetUserGroups(const uint8_t userId, const uint8_t chNum,
     return getUserAccessObject().setUserGroups(userId, chNum, groupAccess);
 }
 
+Cc ipmiUserAddUserToNonIpmiGroupUsers(const std::string& userName)
+{
+    return getUserAccessObject().addUserToNonIpmiGroupUsers(userName);
+}
+
+Cc ipmiUserRemoveUserFromNoneIpmiGroupUsers(const std::string& userName)
+{
+    return getUserAccessObject().removeUserFromNoneIpmiGroupUsers(userName);
+}
+
+Cc ipmiUserRenameNonIpmiGroupUser(const std::string& userName,
+                                  const std::string& newUserName)
+{
+    return getUserAccessObject().renameNonIpmiGroupUser(userName, newUserName);
+}
+
 } // namespace ipmi
