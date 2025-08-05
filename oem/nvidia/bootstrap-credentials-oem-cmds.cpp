98c6e8f36b220c5c075c51d436af10812e4afbe8
Prithvi Pai <ppai@nvidia.com>
2025-08-05 08:35:21 +0530

    nvidia-oem: Implement GetRedfishHostName
    This command is used by BIOS before creating SMBIOS record
    type 42. BIOS gets USBDescription information from BMC.
    
    Note:
    The implementation in this PR is for RedfishHostName.
    The support for other device descriptors will be added in
    upcoming PRs.
    
    Design Document for RHI:
    [1] https://gerrit.openbmc.org/c/openbmc/docs/+/79327
    
    Redfish Host Interface specification:
    [2] https://www.dmtf.org/sites/default/files/standards/documents/DSP0270_1.3.0.pdf
    
    Tested:
    Tested on gb200nvl-obmc platform:
    
    root@gb200nvl-obmc:~# ipmitool raw 0x3c 0x32
     67 62 32 30 30 6e 76 6c 2d 6f 62 6d 63
    
    Change-Id: Ib70dd1fb8c4bdf6e6b02c4329069751d1059bad7
    Signed-off-by: Prithvi Pai <ppai@nvidia.com>

--- oem/nvidia/bootstrap-credentials-oem-cmds.cpp
+++ oem/nvidia/bootstrap-credentials-oem-cmds.cpp
@@ -8,6 +8,8 @@
 
 #include <ipmid/api.hpp>
 #include <ipmid/types.hpp>
+#include <ipmid/utils.hpp>
+#include <phosphor-logging/lg2.hpp>
 
 #include <cstdint>
 
@@ -44,6 +46,39 @@ ipmi::RspType<std::vector<uint8_t>> ipmiGetUsbSerialNumber()
     return ipmi::responseSuccess(usbSerialNumberVector);
 }
 
+ipmi::RspType<std::vector<uint8_t>> ipmiGetRedfishHostName(
+    ipmi::Context::ptr ctx)
+{
+    std::string service{};
+    constexpr auto networkConfigObj = "/xyz/openbmc_project/network/config";
+    constexpr auto networkConfigIface =
+        "xyz.openbmc_project.Network.SystemConfiguration";
+    boost::system::error_code ec =
+        ipmi::getService(ctx, networkConfigIface, networkConfigObj, service);
+    if (ec)
+    {
+        lg2::error(
+            "ipmiGetRedfishHostName failed to get Network SystemConfiguration object {STATUS}",
+            "STATUS", ec.what());
+        return ipmi::responseResponseError();
+    }
+
+    std::string hostName{};
+    ec = ipmi::getDbusProperty<std::string>(
+        ctx, service, networkConfigObj, networkConfigIface, "HostName",
+        hostName);
+    if (ec)
+    {
+        lg2::error(
+            "ipmiGetRedfishHostName failed to get HostName from Network SystemConfiguration service {STATUS}",
+            "STATUS", ec.what());
+        return ipmi::responseResponseError();
+    }
+    std::vector<uint8_t> hostNameBuffer;
+    std::copy(hostName.begin(), hostName.end(),
+              std::back_inserter(hostNameBuffer));
+    return ipmi::responseSuccess(hostNameBuffer);
+}
 } // namespace ipmi
 
 void registerBootstrapCredentialsOemCommands()
@@ -57,4 +92,9 @@ void registerBootstrapCredentialsOemCommands()
         ipmi::prioOemBase, ipmi::groupNvidia,
         ipmi::bootstrap_credentials_oem::cmdGetUsbSerialNumber,
         ipmi::Privilege::Admin, ipmi::ipmiGetUsbSerialNumber);
+
+    ipmi::registerHandler(
+        ipmi::prioOemBase, ipmi::groupNvidia,
+        ipmi::bootstrap_credentials_oem::cmdGetRedfishHostName,
+        ipmi::Privilege::Admin, ipmi::ipmiGetRedfishHostName);
 }
