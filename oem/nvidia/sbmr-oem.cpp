5d271aaccdd724ab8a16369182ac657c6b3702e9
Prithvi Pai <ppai@nvidia.com>
2025-09-01 17:40:41 +0530

    nvidia-oem: Support Logging of SBMR Boot progress
    Added patch to Log the SBMR Boot Progress.
    
    Tested:
    Tested on gb200nvl-obmc platform
    [1] root@gb200nvl-obmc:~# ipmitool raw 0x3C 0xD1 0x01 0x00 0x00 0x00 0x40 0x01 0x02 0x10 0x20 84 69 83 84 32 67 79 77 80 76 69 84 69 0
    
    Tree view after this command
    ```
    root@gb200nvl-obmc:~# busctl tree xyz.openbmc_project.Logging
    `- /xyz
    `- /xyz/openbmc_project
      `- /xyz/openbmc_project/logging
        |- /xyz/openbmc_project/logging/entry
        | |- /xyz/openbmc_project/logging/entry/1
        | |- /xyz/openbmc_project/logging/entry/2
        | `- /xyz/openbmc_project/logging/entry/3
        `- /xyz/openbmc_project/logging/internal
          `- /xyz/openbmc_project/logging/internal/manager
    ```
    
    Change-Id: I3899a714309230491183545473c9e19d39a3eb07
    Signed-off-by: Prithvi Pai <ppai@nvidia.com>

---
+++ oem/nvidia/sbmr-oem.cpp
@@ -0,0 +1,149 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ipmid/api.hpp>
+#include <ipmid/types.hpp>
+#include <ipmid/utils.hpp>
+#include <phosphor-logging/lg2.hpp>
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+void registerSbmrOemCommands() __attribute__((constructor));
+
+constexpr auto loggingService = "xyz.openbmc_project.Logging";
+constexpr auto loggingObject = "/xyz/openbmc_project/logging";
+constexpr auto loggingInterface = "xyz.openbmc_project.Logging.Create";
+
+namespace ipmi
+{
+
+ipmi::RspType<uint8_t> ipmiOemSbmrSendDescription(
+    ipmi::Context::ptr ctx, uint8_t statusCode, uint8_t, uint8_t,
+    uint8_t severity, uint8_t operation1st, uint8_t operation2nd,
+    uint8_t subClass, uint8_t codeClass, std::vector<uint8_t> description)
+{
+    constexpr size_t maxDescriptionLength = 256;
+    constexpr uint8_t bootProgressCode = 0x01;
+    constexpr uint8_t bootErrorCode = 0x02;
+    constexpr uint8_t bootDebugCode = 0x03;
+
+    // Severity codes
+    constexpr uint8_t minor = 0x40;
+    constexpr uint8_t major = 0x80;
+    constexpr uint8_t unrecovered = 0x90;
+    constexpr uint8_t uncontained = 0xA0;
+
+    if (description.size() > maxDescriptionLength)
+    {
+        return responseReqDataLenExceeded();
+    }
+    if (!description.empty() && description.back() != '\0')
+    {
+        return responseInvalidFieldRequest();
+    }
+
+    std::string messageData;
+    for (uint8_t byte : description)
+    {
+        if (byte > 0x00 && byte < 0x1F)
+        {
+            return responseInvalidFieldRequest();
+        }
+        messageData.push_back(byte);
+    }
+    ipmi::ChannelInfo chInfo;
+    auto cc = ipmi::getChannelInfo(ctx->channel, chInfo);
+    if (cc != ipmi::ccSuccess)
+    {
+        lg2::error("Failed to get Channel Info: {STATUS}", "STATUS", cc);
+        return ipmi::responseUnspecifiedError();
+    }
+    if ((chInfo.mediumType !=
+         static_cast<uint8_t>(ipmi::EChannelMediumType::smbusV20)) &&
+        (chInfo.mediumType !=
+         static_cast<uint8_t>(ipmi::EChannelMediumType::systemInterface)) &&
+        (chInfo.mediumType !=
+         static_cast<uint8_t>(ipmi::EChannelMediumType::oem)))
+
+    {
+        lg2::error("ipmiOemSbmrSendDescription: Error - supported only in SSIF "
+                   "interface");
+        return ipmi::responseCommandNotAvailable();
+    }
+    std::ostringstream hexCode;
+    hexCode << "0x" << std::hex << std::setfill('0') << std::setw(2)
+            << static_cast<int>(codeClass) << std::setw(2)
+            << static_cast<int>(subClass) << std::setw(2)
+            << static_cast<int>(operation2nd) << std::setw(2)
+            << static_cast<int>(operation1st);
+
+    std::string eventMessage;
+    std::string eventSeverity;
+    constexpr std::array<std::pair<uint8_t, std::string_view>, 4> sevMap{
+        {{minor, "Minor"},
+         {major, "Major"},
+         {unrecovered, "Unrecovered"},
+         {uncontained, "Uncontained"}}};
+    switch (statusCode)
+    {
+        case bootProgressCode:
+            eventMessage = "Progress Code ";
+            eventSeverity =
+                "xyz.openbmc_project.Logging.Entry.Level.Informational";
+            break;
+        case bootErrorCode:
+            eventMessage = "Error Code ";
+            if (auto it = std::ranges::find_if(
+                    sevMap,
+                    [severity](auto&& p) { return p.first == severity; });
+                it != sevMap.end())
+            {
+                eventMessage += it->second;
+            }
+            eventSeverity =
+                (severity == minor)
+                    ? "xyz.openbmc_project.Logging.Entry.Level.Warning"
+                    : "xyz.openbmc_project.Logging.Entry.Level.Error";
+            break;
+        case bootDebugCode:
+            eventMessage = "Debug Code ";
+            eventSeverity = "xyz.openbmc_project.Logging.Entry.Level.Debug";
+            break;
+        default:
+            lg2::error("ipmiOemSbmrSendDescription: Invalid Status Code {CODE}",
+                       "CODE", statusCode);
+            return responseInvalidFieldRequest();
+    }
+    eventMessage += hexCode.str();
+    eventMessage += ":" + messageData;
+    std::map<std::string, std::string> additionalData;
+    boost::system::error_code ec = ipmi::callDbusMethod(
+        ctx, loggingService, loggingObject, loggingInterface, "Create",
+        eventMessage, eventSeverity, additionalData);
+    if (ec.value())
+    {
+        lg2::error("Failed to call Create method, Error={ERROR}", "ERROR",
+                   ec.message());
+        return ipmi::responseUnspecifiedError();
+    }
+    return ipmi::responseSuccess();
+}
+} // namespace ipmi
+
+void registerSbmrOemCommands()
+{
+    ipmi::registerHandler(ipmi::prioOemBase, ipmi::groupNvidia,
+                          ipmi::sbmr_oem::cmdSbmrSendDescription,
+                          ipmi::Privilege::Admin,
+                          ipmi::ipmiOemSbmrSendDescription);
+}
