d73eb1ce3c2627645b5adcca6ccc1478fdc67d71
Nikhila Dande <nikhilad@ami.com>
2026-04-02 18:42:09 +0530

    DCMI: Fix for Management Controller Identifier String
    - Currently, the DCMI Set Management Controller
    Identifier String Command supports upto 63 bytes,
    excluding the null terminator.
    
    - As per DCMI Specification v1.5, the maximum length
      of the Management Controller Identifier String is
      64 bytes, including the null terminator.
    
    - Added support to set 64 characters including null
    character
    
    - And return an error if 64th character position
    is non-null character as per DCMI spec.
    
    - Tested and verified all the test cases.
    
    Change-Id: I2f4acd86af687fbe8778f3dc06a7837d3fe53af6
    Signed-off-by: Nikhila Dande <nikhilad@ami.com>

--- dcmihandler.cpp
+++ dcmihandler.cpp
@@ -44,7 +44,7 @@ namespace dcmi
 constexpr auto assetTagMaxOffset = 62;
 constexpr auto assetTagMaxSize = 63;
 constexpr auto maxBytes = 16;
-constexpr size_t maxCtrlIdStrLen = 63;
+constexpr size_t maxCtrlIdStrLen = 64;
 
 constexpr uint8_t parameterRevision = 2;
 constexpr uint8_t specMajorVersion = 1;
@@ -924,6 +924,10 @@ ipmi::RspType<uint8_t> setMgmntCtrlIdStr(ipmi::Context::ptr& ctx,
     {
         // remove the null termination from the data (no need with std::string)
         data.resize(count - 1);
+    }
+    else if ((offset + count) == dcmi::maxCtrlIdStrLen)
+    {
+        return ipmi::responseParmOutOfRange();
     }
 
     static std::string hostname{};
