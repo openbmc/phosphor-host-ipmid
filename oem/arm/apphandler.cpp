4032853c0b71f6b5ef64057c095e1720b3afe7ec
Manoj Kumar Prabakaran <manojkumar.prabakaran@arm.com>
2026-06-09 16:38:25 +0530

    Add in-tree arm OEM provider for SSIF capabilities
    Enable arm as a selectable OEM IPMI provider, add a new in-tree
    arm provider library, and implement Get System Interface
    Capabilities from that provider.
    
    Add ipmi provider that registers Get System Interface Capabilities
    for SSIF, returns the SSIF capability selector response, advertises
    multipart transfer support, PEC support, and the 255-byte input
    message limit and a 252-byte output message size.
    
    Tested:
    $ ipmitool raw 0x06 0x57 0x00
      00 88 ff fc
    
    $ ipmitool raw 0x06 0x57 0x01
      Unable to send RAW command (channel=0x0 netfn=0x6 lun=0x0 cmd=0x57
      rsp=0xc9): Parameter out of range
    
    $ ipmitool raw 0x06 0x57 0x10
      Unable to send RAW command (channel=0x0 netfn=0x6 lun=0x0 cmd=0x57
      rsp=0xcc): Invalid data field in request
    
    decodes as:
    -00 = reserved byte
    -88 = capabilities
    -ff = 255-byte input limit
    -fc = 252-byte output limit
    
    Change-Id: I8cdbb41d9af88f2e7111d5c6ce2971d9e77d8649
    Signed-off-by: Manoj Kumar Prabakaran <manojkumar.prabakaran@arm.com>

---
+++ oem/arm/apphandler.cpp
@@ -0,0 +1,60 @@
+/*
+ * SPDX-FileCopyrightText: Copyright OpenBMC Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ipmid/api.hpp>
+
+#include <cstdint>
+
+void registerArmNetFnAppFunctions() __attribute__((constructor));
+
+namespace
+{
+constexpr uint8_t ssifSystemInterfaceSelector = 0x00;
+constexpr uint8_t reservedResponseByte = 0x00;
+constexpr uint8_t ssifVersion = 0x00;
+constexpr uint8_t ssifVersionMask = 0x07;
+constexpr uint8_t supportsMultiPartReadsWrites = 0x80;
+constexpr uint8_t supportsPec = 0x08;
+constexpr uint8_t supportedInterfaceMask = 0x0F;
+constexpr uint8_t reservedInterfaceBitsMask = 0xF0;
+
+constexpr uint8_t ssifInputMessageSize = 0xFF;
+// IPMI v2.0 section 22.9 reports the SSIF output size as IPMI message bytes.
+// Use 0xFF - 3 to leave room for the response NetFn/LUN, command, and
+// completion code bytes in a 255-byte SSIF transfer.
+constexpr uint8_t ssifOutputMessageSize = 0xFF - 3;
+constexpr uint8_t ssifCapabilities =
+    supportsMultiPartReadsWrites | supportsPec |
+    (ssifVersion & ssifVersionMask);
+} // namespace
+
+ipmi::RspType<uint8_t, uint8_t, uint8_t, uint8_t>
+    ipmiAppGetSystemInterfaceCapabilities(uint8_t interfaceSelector)
+{
+    if ((interfaceSelector & reservedInterfaceBitsMask) != 0)
+    {
+        // Reserved upper bits make the selector field itself invalid.
+        return ipmi::responseInvalidFieldRequest();
+    }
+
+    if ((interfaceSelector & supportedInterfaceMask) !=
+        ssifSystemInterfaceSelector)
+    {
+        // The interface type field is valid, but only SSIF (0x00) is
+        // supported by this provider.
+        return ipmi::responseParmOutOfRange();
+    }
+
+    return ipmi::responseSuccess(reservedResponseByte, ssifCapabilities,
+                                 ssifInputMessageSize, ssifOutputMessageSize);
+}
+
+void registerArmNetFnAppFunctions()
+{
+    ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnApp,
+                          ipmi::app::cmdGetSystemIfCapabilities,
+                          ipmi::Privilege::User,
+                          ipmiAppGetSystemInterfaceCapabilities);
+}
